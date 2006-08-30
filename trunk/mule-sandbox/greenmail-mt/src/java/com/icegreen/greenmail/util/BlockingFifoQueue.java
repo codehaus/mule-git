3ddde4bea2ed877a7b099b61701707e888f1bc04
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2006-08-30 21:24:52 +0000

    a LinkedList is more appropriate here
    git-svn-id: file:///home/projects/mule/scm@2871 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule-sandbox/greenmail-mt/src/java/com/icegreen/greenmail/util/BlockingFifoQueue.java
+++ trunk/mule-sandbox/greenmail-mt/src/java/com/icegreen/greenmail/util/BlockingFifoQueue.java
@@ -4,61 +4,69 @@
  * other way use this code without prior permission
  * of the copyright holder is not permitted.
  */
+
 package com.icegreen.greenmail.util;
 
-import java.util.Vector;
+import java.util.LinkedList;
+
 /**
  * A synchronized blocking FIFO queue.
  * <P>
+ * 
  * @author Wael
  */
 public class BlockingFifoQueue extends Object
 {
- protected Vector m_queue = new Vector(1000,20);
- private boolean m_stopped = false;
- //=============================================
+    protected LinkedList m_queue = new LinkedList();
+    private boolean m_stopped = false;
 
- /**
-  * Constructor
-  */
- public BlockingFifoQueue()
- {
- }
+    // =============================================
 
- public boolean isStopped()
- {
-   return m_stopped;
- }
+    /**
+     * Constructor
+     */
+    public BlockingFifoQueue()
+    {
+        super();
+    }
 
- public synchronized void shutDownQueue()
- {
-   m_stopped = true;
-   notifyAll();
- }
- /*
-  * Add an element to the queue.
-  */
- public synchronized void push(Object obj)
- {
-    m_queue.add(obj);
-    notifyAll();
- }
+    public synchronized boolean isStopped()
+    {
+        return m_stopped;
+    }
 
- /*
-  * Retrives the next element in the queue. If no element in queue this function will block the callers thread until an element gets added or shutDownQueue() is called
-  * @return the next object in the queue.
-  * If shutDownQueue() has been called and the queue is empty null will be returned. Use the public function isStoppedto distinguish this from a null element in the queue
-  */
- public synchronized Object pop() throws InterruptedException
- {
-   while (m_queue.size() == 0)
-   {
-     if (m_stopped)
-     {
-        return null;
-     }
-     wait();
-   }
-   return m_queue.remove(0);
- }
+    public synchronized void shutDownQueue()
+    {
+        m_stopped = true;
+        notifyAll();
+    }
+
+    /*
+     * Add an element to the queue.
+     */
+    public synchronized void push(Object obj)
+    {
+        m_queue.addLast(obj);
+        notifyAll();
+    }
+
+    /*
+     * Retrives the next element in the queue. If no element in queue this function
+     * will block the callers thread until an element gets added or shutDownQueue()
+     * is called @return the next object in the queue. If shutDownQueue() has been
+     * called and the queue is empty null will be returned. Use the public function
+     * isStoppedto distinguish this from a null element in the queue
+     */
+    public synchronized Object pop() throws InterruptedException
+    {
+        while (m_queue.size() == 0)
+        {
+            if (m_stopped)
+            {
+                return null;
+            }
+            wait();
+        }
+        return m_queue.removeFirst();
+    }
 }
