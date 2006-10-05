b0b30cf51a01ef153bbd91ba87ee2129f26634e2
jmoller <jmoller@bf997673-6b11-0410-b953-e057580c5b09>
2006-10-05 20:51:47 +0000

    Branch for experimenting with the IDE
    git-svn-id: file:///home/projects/mule/scm@3434 bf997673-6b11-0410-b953-e057580c5b09

---
+++ branches/mule-ide-experimentation/plugins/org.mule.ide.prototype.editor/src/org/mule/ide/prototype/mulemodel/presentation/util/EMFEditor.java
@@ -0,0 +1,11 @@
+package org.mule.ide.prototype.mulemodel.presentation.util;
+
+import org.eclipse.ui.part.MultiPageEditorPart;
+
+public abstract class EMFEditor extends MultiPageEditorPart {
+
+	public EMFEditor() {
+		super();
+	}
+
+}
