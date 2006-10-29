27eb089db53de08bdb447858a85ee777334820ac
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2006-10-29 16:57:31 +0000

    remove redundant getConnector/getDelegateSession implementations, cleanups
    git-svn-id: file:///home/projects/mule/scm@3728 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-1.4-connectordesign/transports/quartz/src/main/java/org/mule/providers/quartz/QuartzMessageDispatcher.java
+++ branches/mule-1.4-connectordesign/transports/quartz/src/main/java/org/mule/providers/quartz/QuartzMessageDispatcher.java
@@ -10,12 +10,14 @@
 
 package org.mule.providers.quartz;
 
+import java.util.Date;
+import java.util.Iterator;
+
 import org.mule.MuleManager;
 import org.mule.config.i18n.Message;
 import org.mule.providers.AbstractMessageDispatcher;
 import org.mule.providers.quartz.jobs.DelegatingJob;
 import org.mule.umo.UMOEvent;
-import org.mule.umo.UMOException;
 import org.mule.umo.UMOMessage;
 import org.mule.umo.endpoint.UMOImmutableEndpoint;
 import org.mule.umo.provider.DispatchException;
@@ -27,9 +29,6 @@ import org.quartz.JobDetail;
 import org.quartz.Scheduler;
 import org.quartz.SimpleTrigger;
 import org.quartz.Trigger;
-
-import java.util.Date;
-import java.util.Iterator;
 
 /**
  * Can schedule a Job with the Quartz scheduler. The event must contain the Job to
@@ -203,8 +202,4 @@ public class QuartzMessageDispatcher extends AbstractMessageDispatcher
         throw new UnsupportedOperationException("doReceive");
     }
 
-    public Object getDelegateSession() throws UMOException
-    {
-        return null;
-    }
 }
