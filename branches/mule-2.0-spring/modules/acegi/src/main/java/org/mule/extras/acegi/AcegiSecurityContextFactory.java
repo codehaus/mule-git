ed8b18fac9b4c7972309cdaf8c722d6119587f7b
rossmason <rossmason@bf997673-6b11-0410-b953-e057580c5b09>
2006-11-27 20:56:47 +0000

    git-svn-id: file:///home/projects/mule/scm@4054 bf997673-6b11-0410-b953-e057580c5b09

---
+++ branches/mule-2.0-spring/modules/acegi/src/main/java/org/mule/extras/acegi/AcegiSecurityContextFactory.java
@@ -0,0 +1,42 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.extras.acegi;
+
+import org.acegisecurity.context.SecurityContext;
+import org.acegisecurity.context.SecurityContextHolder;
+import org.acegisecurity.context.SecurityContextImpl;
+import org.mule.umo.security.UMOAuthentication;
+import org.mule.umo.security.UMOSecurityContext;
+import org.mule.umo.security.UMOSecurityContextFactory;
+
+/**
+ * <code>AcegiSecurityContextFactory</code> creates an AcegiSecurityContext for an
+ * UMOAuthentication object
+ */
+public class AcegiSecurityContextFactory implements UMOSecurityContextFactory
+{
+    public UMOSecurityContext create(UMOAuthentication authentication)
+    {
+        SecurityContext context = new SecurityContextImpl();
+        context.setAuthentication(((AcegiAuthenticationAdapter)authentication).getDelegate());
+
+        if (authentication.getProperties() != null)
+        {
+            if ((authentication.getProperties().containsKey("securityMode")))
+            {
+                SecurityContextHolder.setStrategyName((String)authentication.getProperties().get(
+                    "securityMode"));
+            }
+        }
+        SecurityContextHolder.setContext(context);
+        return new AcegiSecurityContext(context);
+    }
+}
