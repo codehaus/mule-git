c922409cbf8c0970610a67ded5d1e5d121c847ad
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2006-12-01 22:09:04 +0000

    More updating of send/dispatch methods. Tests continue to run. :)
    git-svn-id: file:///home/projects/mule/scm@4107 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-1.4-connectordesign/transports/jbi/src/main/java/org/mule/providers/jbi/components/MuleDispatcher.java
+++ branches/mule-1.4-connectordesign/transports/jbi/src/main/java/org/mule/providers/jbi/components/MuleDispatcher.java
@@ -20,15 +20,11 @@ import org.mule.providers.AbstractConnector;
 import org.mule.providers.jbi.JbiUtils;
 import org.mule.umo.UMOEvent;
 import org.mule.umo.UMOMessage;
-import org.mule.umo.provider.UMOMessageDispatcher;
 
 /**
- * Is a Jbi component that can dispatch Normalised Messages over a given transport
+ * A JBI component that can dispatch Normalised Messages over a given transport
  * specified by the muleEndpoint property. This component can deliver events over any
- * Mule transport such as jms, ftp, htp, jdbc, ejb, etc
- * 
- * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
- * @version $Revision$
+ * Mule transport such as jms, ftp, htp, jdbc, ejb, etc.
  */
 public class MuleDispatcher extends AbstractEndpointComponent implements MessageExchangeListener
 {
@@ -39,30 +35,33 @@ public class MuleDispatcher extends AbstractEndpointComponent implements Message
         {
             logger.debug("In Mule Dispatcher");
         }
+
         try
         {
-            UMOMessageDispatcher dispatcher = muleEndpoint.getConnector().getDispatcher(muleEndpoint);
             NormalizedMessage out = messageExchange.getMessage(IN);
             UMOMessage message = JbiUtils.createMessage(out);
+
             if (logger.isDebugEnabled())
             {
                 logger.debug("Dispatching Message via Mule: " + message);
             }
+
             MuleSession session = new MuleSession(message,
                 ((AbstractConnector)muleEndpoint.getConnector()).getSessionHandler());
 
             UMOEvent event = new MuleEvent(message, muleEndpoint, session, muleEndpoint.isSynchronous());
+
             if (muleEndpoint.isSynchronous())
             {
                 logger.debug("Dispatching to: " + muleEndpoint.getEndpointURI());
                 logger.debug("Payload is: " + event.getMessageAsString());
 
-                UMOMessage result = dispatcher.send(event);
+                UMOMessage result = muleEndpoint.send(event);
                 // TODO send result back
             }
             else
             {
-                dispatcher.dispatch(event);
+                muleEndpoint.dispatch(event);
             }
         }
         catch (Exception e)
