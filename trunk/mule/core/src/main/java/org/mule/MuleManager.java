34e6dd977ad5dce9a6c3829a899b80e583babc93
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2006-12-02 20:51:41 +0000

    Redesigned agent registration slightly. Fortunately the solution didn't require API changes, thus committing on the trunk directly. This fixes MULE-1101 DefaultJmxSupportAgent fails when combined with any other agent.
    git-svn-id: file:///home/projects/mule/scm@4111 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/core/src/main/java/org/mule/MuleManager.java
+++ trunk/mule/core/src/main/java/org/mule/MuleManager.java
@@ -12,6 +12,7 @@ package org.mule;
 
 import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicBoolean;
 import org.apache.commons.collections.MapUtils;
+import org.apache.commons.collections.list.CursorableLinkedList;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -85,6 +86,7 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Collection;
 import java.util.jar.Manifest;
 
 /**
@@ -1235,13 +1237,30 @@ public class MuleManager implements UMOManager
      */
     protected void initialiseAgents() throws InitialisationException
     {
-        UMOAgent umoAgent;
         logger.info("Initialising agents...");
-        for (Iterator iterator = agents.values().iterator(); iterator.hasNext();)
+
+        // Do not iterate over the map directly, as 'complex' agents
+        // may spawn extra agents during initialisation. This will
+        // cause a ConcurrentModificationException.
+        // Use a cursorable iteration, which supports on-the-fly underlying
+        // data structure changes.
+        Collection agentsSnapshot = agents.values();
+        CursorableLinkedList agents = new CursorableLinkedList(agentsSnapshot);
+        CursorableLinkedList.Cursor cursor = agents.cursor();
+        try
         {
-            umoAgent = (UMOAgent)iterator.next();
-            logger.debug("Initialising agent: " + umoAgent.getName());
-            umoAgent.initialise();
+            while (cursor.hasNext())
+            {
+                UMOAgent umoAgent = (UMOAgent) cursor.next();
+
+                logger.debug("Initialising agent: " + umoAgent.getName());
+                umoAgent.initialise();
+            }
+        }
+        finally
+        {
+            // close the cursor as per JavaDoc
+            cursor.close();
         }
         logger.info("Agents Successfully Initialised");
     }
