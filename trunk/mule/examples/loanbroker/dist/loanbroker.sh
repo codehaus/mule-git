9897d2e88c23a7ebb162e3d6e9a529cdf3bfd2c3
Lajos <Lajos@bf997673-6b11-0410-b953-e057580c5b09>
2006-12-08 17:25:51 +0000

    Updated all example scripts with provision for MULE_BASE
    git-svn-id: file:///home/projects/mule/scm@4205 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/examples/loanbroker/dist/loanbroker.sh
+++ trunk/mule/examples/loanbroker/dist/loanbroker.sh
@@ -6,9 +6,15 @@ if [ -z "$MULE_HOME" ] ; then
   export MULE_HOME
 fi
 
+# If MULE_BASE is not set, make it MULE_HOME
+if [ -z "$MULE_BASE" ] ; then
+  MULE_BASE=$MULE_HOME
+  export MULE_BASE
+fi
+
 # Any changes to the files in ./conf will take precedence over those deployed to $MULE_HOME/lib/user
 MULE_LIB=./conf
 export MULE_LIB
 
-exec $MULE_HOME/bin/mule -main org.mule.samples.loanbroker.LoanConsumer
+exec $MULE_BASE/bin/mule -main org.mule.samples.loanbroker.LoanConsumer
 
