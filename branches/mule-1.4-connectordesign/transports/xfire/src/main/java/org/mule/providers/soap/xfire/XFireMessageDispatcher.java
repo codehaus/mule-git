af41464f05798ee9ae23ee819612233e8d134d7e
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2006-12-14 18:56:08 +0000

    Merged revisions 4257-4270 via svnmerge from https://svn.codehaus.org/mule/trunk/mule
    ........
      r4258 | aperepel | 2006-12-14 03:47:15 +0100 (Do, 14 Dez 2006) | 1 line
      
      Ignore properties set.
    ........
      r4260 | aperepel | 2006-12-14 04:19:09 +0100 (Do, 14 Dez 2006) | 3 lines
      
      Removed merge tracking for "svnmerge" for 
      https://svn.codehaus.org/mule/branches/mule-1.4-agentdesign
    ........
      r4262 | Marie.Rizzo | 2006-12-14 15:07:24 +0100 (Do, 14 Dez 2006) | 1 line
      
      This contains a main method which is used by the GUI installer to be able to save the license... makes use of the LicenseHandler
    ........
      r4263 | Marie.Rizzo | 2006-12-14 15:53:40 +0100 (Do, 14 Dez 2006) | 1 line
      
      Cleaned up the Ws-Security stuff... that is now in a separate module ;)
    ........
      r4264 | Marie.Rizzo | 2006-12-14 16:02:16 +0100 (Do, 14 Dez 2006) | 1 line
      
      Oooops
    ........
      r4265 | Lajos | 2006-12-14 17:06:16 +0100 (Do, 14 Dez 2006) | 1 line
      
      MULE-1152: Updated example build.xml to intelligently pick either MULE_BASE or MULE_HOME
    ........
      r4266 | Lajos | 2006-12-14 17:44:51 +0100 (Do, 14 Dez 2006) | 1 line
      
      Added method per Ross
    ........
      r4267 | Lajos | 2006-12-14 17:45:39 +0100 (Do, 14 Dez 2006) | 1 line
      
      Change per Ross
    ........
      r4269 | Lajos | 2006-12-14 18:37:46 +0100 (Do, 14 Dez 2006) | 1 line
      
      LicenseHandler checks for existence of license.props before building jar file. Removed trap for Exception from GuiLicenseHandler
    ........
    
    
    git-svn-id: file:///home/projects/mule/scm@4271 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-1.4-connectordesign/transports/xfire/src/main/java/org/mule/providers/soap/xfire/XFireMessageDispatcher.java
+++ branches/mule-1.4-connectordesign/transports/xfire/src/main/java/org/mule/providers/soap/xfire/XFireMessageDispatcher.java
@@ -25,8 +25,6 @@ import javax.xml.namespace.QName;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.pool.ObjectPool;
 import org.apache.commons.pool.impl.StackObjectPool;
-import org.apache.ws.security.WSConstants;
-import org.apache.ws.security.handler.WSHandlerConstants;
 import org.codehaus.xfire.XFire;
 import org.codehaus.xfire.client.Client;
 import org.codehaus.xfire.handler.Handler;
@@ -75,61 +73,29 @@ public class XFireMessageDispatcher extends AbstractMessageDispatcher
             {
                 throw new FatalConnectException(new Message("xfire", 8, serviceName), this);
             }
-
-            // Add Security Handlers
-            if (connector.getWsSecurity() != null)
+            
+            List inList = connector.getServerInHandlers();
+            if(inList != null)
             {
-
-                if (connector.getWsSecurity().get("serviceInHandlers") != null)
+                for(int i = 0; i < inList.size(); i++)
                 {
-                    List inList = (List)connector.getWsSecurity().get("serviceInHandlers");
-                    for (int i = 0; i < inList.size(); i++)
-                    {
-                        Class clazz = Class.forName(inList.get(i).toString());
-                        Handler handler = (Handler)clazz.getConstructor(null).newInstance(null);
-                        service.addInHandler(handler);
-                    }
-
-                    if (!inList.contains("org.codehaus.xfire.security.wss4j.WSS4JInHandler"))
-                    {
-                        Class clazz = Class.forName("org.codehaus.xfire.security.wss4j.WSS4JInHandler");
-                        Handler handler = (Handler)clazz.getConstructor(null).newInstance(null);
-                        service.addInHandler(handler);
-                    }
-
-                    if (!inList.contains("org.codehaus.xfire.util.dom.DOMInHandler"))
-                    {
-                        Class clazz = Class.forName("org.codehaus.xfire.util.dom.DOMInHandler");
-                        Handler handler = (Handler)clazz.getConstructor(null).newInstance(null);
-                        service.addInHandler(handler);
-                    }
-                }
-
-                if (connector.getWsSecurity().get("serviceOutHandlers") != null)
-                {
-                    List outList = (List)connector.getWsSecurity().get("serviceOutHandlers");
-                    for (int i = 0; i < outList.size(); i++)
-                    {
-                        Class clazz = Class.forName(outList.get(i).toString());
-                        Handler handler = (Handler)clazz.getConstructor(null).newInstance(null);
-                        service.addOutHandler(handler);
-                    }
-
-                    if (!outList.contains("org.codehaus.xfire.security.wss4j.WSS4JOutHandler"))
-                    {
-                        Class clazz = Class.forName("org.codehaus.xfire.security.wss4j.WSS4JOutHandler");
-                        Handler handler = (Handler)clazz.getConstructor(null).newInstance(null);
-                        service.addOutHandler(handler);
-                    }
-
-                    if (!outList.contains("org.codehaus.xfire.util.dom.DOMOutHandler"))
-                    {
-                        Class clazz = Class.forName("org.codehaus.xfire.util.dom.DOMOutHandler");
-                        Handler handler = (Handler)clazz.getConstructor(null).newInstance(null);
-                        service.addOutHandler(handler);
-                    }
+                    Class clazz = Class.forName(inList.get(i).toString());
+                    Handler handler = (Handler)clazz.getConstructor(null).newInstance(null);
+                    service.addInHandler(handler);
                 }
             }
+            
+            List outList = connector.getServerOutHandlers();
+            if(outList != null)
+            {
+                for(int i = 0; i < outList.size(); i++)
+                {
+                    Class clazz = Class.forName(outList.get(i).toString());
+                    Handler handler = (Handler)clazz.getConstructor(null).newInstance(null);
+                    service.addOutHandler(handler);
+                }
+            }
+
             try
             {
                 clientPool = new StackObjectPool(new XFireClientPoolFactory(endpoint, service, xfire));
@@ -159,22 +125,25 @@ public class XFireMessageDispatcher extends AbstractMessageDispatcher
 
     protected String getMethod(UMOEvent event) throws DispatchException
     {
-        String method = (String)event.getMessage().getProperty(MuleProperties.MULE_METHOD_PROPERTY);
+        String method = (String)event.getMessage().getProperty(MuleProperties.MULE_METHOD_PROPERTY);     
+                
         if (method == null)
         {
             UMOEndpointURI endpointUri = event.getEndpoint().getEndpointURI();
             method = (String)endpointUri.getParams().get(MuleProperties.MULE_METHOD_PROPERTY);
         }
-
+        
         if (method == null)
         {
             method = (String)event.getEndpoint().getProperties().get(MuleProperties.MULE_METHOD_PROPERTY);
-            if (method == null)
-            {
-                throw new DispatchException(new org.mule.config.i18n.Message("soap", 4), event.getMessage(),
-                    event.getEndpoint());
-            }
+        }   
+        
+        if (method == null)
+        {
+            throw new DispatchException(new org.mule.config.i18n.Message("soap", 4), event.getMessage(),
+               event.getEndpoint());
         }
+                
         return method;
     }
 
@@ -212,7 +181,7 @@ public class XFireMessageDispatcher extends AbstractMessageDispatcher
     protected UMOMessage doSend(UMOEvent event) throws Exception
     {
         Client client = null;
-
+                
         try
         {
             client = (Client)clientPool.borrowObject();
@@ -227,32 +196,17 @@ public class XFireMessageDispatcher extends AbstractMessageDispatcher
                 client.setProperty(org.codehaus.xfire.soap.SoapConstants.SOAP_ACTION, soapAction);
             }
 
-            // Ws Security
-            if (event.getMessage().getProperty(WSHandlerConstants.ACTION) != null)
-            {
-                client.setProperty(WSHandlerConstants.ACTION, event.getMessage().getProperty(
-                    WSHandlerConstants.ACTION));
-
-                String passwordType;
-                if (event.getMessage().getProperty(WSConstants.PW_DIGEST) != null)
-                {
-                    passwordType = WSConstants.PW_DIGEST;
+            // Set Custom Headers on the client
+            Object[] arr = event.getMessage().getPropertyNames().toArray();
+            String head;
+            for (int i = 0; i < arr.length; i++){
+                head = "";
+                String test = (String)arr[i];
+                for (int j = 0; j < 4; j++){
+                    head = head + test.charAt(j);
                 }
-                else
-                {
-                    passwordType = WSConstants.PW_TEXT;
-                }
-                client.setProperty(WSHandlerConstants.PASSWORD_TYPE, passwordType);
-                client.setProperty(WSHandlerConstants.USER, event.getMessage().getProperty(
-                    WSHandlerConstants.USER));
-                client.setProperty(WSHandlerConstants.PW_CALLBACK_CLASS, event.getMessage().getProperty(
-                    WSHandlerConstants.PW_CALLBACK_CLASS));
-
-                // if encrypted
-                if (event.getMessage().getProperty(WSHandlerConstants.ENC_PROP_FILE) != null)
-                {
-                    client.setProperty(WSHandlerConstants.ENC_PROP_FILE, event.getMessage().getProperty(
-                        WSHandlerConstants.ENC_PROP_FILE));
+                if ((head != null)&&(!head.equals("MULE"))){
+                    client.setProperty((String)arr[i], event.getMessage().getProperty((String)arr[i]));
                 }
             }
 
