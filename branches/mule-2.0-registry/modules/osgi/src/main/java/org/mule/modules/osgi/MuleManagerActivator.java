041da295591148d0df68bb4397c5ee194483f3da
tcarlson <tcarlson@bf997673-6b11-0410-b953-e057580c5b09>
2006-12-18 12:57:17 +0000

    Cleanup
    git-svn-id: file:///home/projects/mule/scm@4297 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.0-registry/modules/osgi/src/main/java/org/mule/modules/osgi/MuleManagerActivator.java
+++ branches/mule-2.0-registry/modules/osgi/src/main/java/org/mule/modules/osgi/MuleManagerActivator.java
@@ -10,10 +10,8 @@ import org.osgi.framework.BundleContext;
 public class MuleManagerActivator implements BundleActivator {
 	
 	private UMOManager manager;
-	//private ConcurrentHashMap springContexts = new ConcurrentHashMap();
 
 	public void start(BundleContext bc) throws Exception {
-		//bc.addBundleListener(new MuleBundleListener());		
 		manager = MuleManager.getInstance();
 		manager.start();
 	}
@@ -21,25 +19,4 @@ public class MuleManagerActivator implements BundleActivator {
 	public void stop(BundleContext bc) throws Exception {
 		manager.stop();
 	}
-
-//	class MuleBundleListener implements BundleListener {
-//	    public void bundleChanged(BundleEvent event) {
-//	    	if (event.getType() == BundleEvent.STARTING) {
-//	    		System.out.println("Bundle starting: " + event.getBundle().getSymbolicName());
-//	    		//springContexts.put(event.getBundle().getBundleId(), new OsgiBundleXmlApplicationContext(resources));
-//	    	}
-//	    	else if (event.getType() == BundleEvent.STOPPING) {
-//	    		System.out.println("Bundle stopping: " + event.getBundle().getSymbolicName());
-//	    	}
-//	    	else if (event.getType() == BundleEvent.INSTALLED) {
-//	    		System.out.println("Bundle has been installed: " + event.getBundle().getSymbolicName());
-//	    	}
-//	    	else if (event.getType() == BundleEvent.UNINSTALLED) {
-//	    		System.out.println("Bundle has been uninstalled: " + event.getBundle().getSymbolicName());
-//	    	}
-//	    	else if (event.getType() == BundleEvent.UPDATED) {
-//	    		System.out.println("Bundle has been updated: " + event.getBundle().getSymbolicName());
-//	    	}
-//		}
-//	}
 }
