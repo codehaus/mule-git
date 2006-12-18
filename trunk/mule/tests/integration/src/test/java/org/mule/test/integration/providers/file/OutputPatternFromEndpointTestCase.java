c3f95258cdeb71c6b0b72c244501cc0c2f2489f4
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2006-12-18 23:26:27 +0000

    fix svn properties
    git-svn-id: file:///home/projects/mule/scm@4302 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/tests/integration/src/test/java/org/mule/test/integration/providers/file/OutputPatternFromEndpointTestCase.java
+++ trunk/mule/tests/integration/src/test/java/org/mule/test/integration/providers/file/OutputPatternFromEndpointTestCase.java
@@ -1,5 +1,5 @@
 /*
- * $Id: FileRuntimeExceptionStrategyFunctionalTestCase.java 3653 2006-10-24 10:31:33Z holger $
+ * $Id$
  * --------------------------------------------------------------------------------------
  * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
  *
