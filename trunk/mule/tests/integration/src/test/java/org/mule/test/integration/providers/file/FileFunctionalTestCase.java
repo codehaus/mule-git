a3a77f7363221c403dbaaf56f39e3ca2095b57de
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2006-12-20 04:26:32 +0000

    testcase for MULE-1275. fix: 10 seconds, testcase: 4 hours =:(
    git-svn-id: file:///home/projects/mule/scm@4337 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/tests/integration/src/test/java/org/mule/test/integration/providers/file/FileFunctionalTestCase.java
+++ trunk/mule/tests/integration/src/test/java/org/mule/test/integration/providers/file/FileFunctionalTestCase.java
@@ -10,26 +10,88 @@
 
 package org.mule.test.integration.providers.file;
 
+import org.mule.MuleManager;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.tck.functional.FunctionalTestComponent;
+import org.mule.tck.functional.FunctionalTestNotification;
+import org.mule.tck.functional.FunctionalTestNotificationListener;
+import org.mule.umo.UMOEventContext;
+import org.mule.umo.manager.UMOServerNotification;
+import org.mule.util.IOUtils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
 
-/**
- * @author <a href="mailto:gnt@codehaus.org">Guillaume Nodet</a>
- * @version $Revision$
- */
-public class FileFunctionalTestCase extends FunctionalTestCase
+public class FileFunctionalTestCase extends FunctionalTestCase implements FunctionalTestNotificationListener
 {
+    private Object receivedData = null;
+
+    protected void doPostFunctionalSetUp() throws Exception
+    {
+        super.doPostFunctionalSetUp();
+        super.setDisposeManagerPerSuite(true);
+        MuleManager.getInstance().registerListener(this);
+    }
+
+    protected void doFunctionalTearDown() throws Exception
+    {
+        MuleManager.getInstance().unregisterListener(this);
+        super.doFunctionalTearDown();
+    }
+
     protected String getConfigResources()
     {
         return "org/mule/test/integration/providers/file/file-config.xml";
     }
 
-    public void testRelative() throws Exception
+    public void testRelative() throws FileNotFoundException, IOException, InterruptedException
     {
+        byte[] data = new byte[100000];
+        for (int i = 0; i < data.length; i++)
+        {
+            data[i] = (byte)(Math.random() * 128);
+        }
+
         File f = new File("./test/toto.txt");
         f.createNewFile();
-        Thread.sleep(1000);
+        FileOutputStream fos = new FileOutputStream(f);
+        IOUtils.write(data, fos);
+        IOUtils.closeQuietly(fos);
+
+        // give polling a chance
+        Thread.sleep(5000);
+
+        synchronized (this)
+        {
+            assertNotNull(receivedData);
+            assertTrue(receivedData instanceof byte[]);
+            byte[] receivedBytes = (byte[])receivedData;
+            assertEquals(data.length, receivedBytes.length);
+            assertTrue(Arrays.equals(data, receivedBytes));
+        }
+    }
+
+    public void onNotification(UMOServerNotification notification)
+    {
+        synchronized (this)
+        {
+            // save the data for verification
+            this.receivedData = ((FunctionalTestNotification)notification).getReplyMessage();
+        }
+    }
+
+    public static class FileTestComponent extends FunctionalTestComponent
+    {
+        public Object onCall(UMOEventContext context) throws Exception
+        {
+            // there should not be any transformers configured by default, so the
+            // return message should be a byte[]
+            super.setReturnMessage(context.getTransformedMessage());
+            return super.onCall(context);
+        }
     }
 
 }
