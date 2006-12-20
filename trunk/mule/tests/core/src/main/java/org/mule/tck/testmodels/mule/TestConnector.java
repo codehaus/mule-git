c3877121052659a7a8e818e5aa6d8101c9a560ed
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2006-12-20 18:59:50 +0000

    fix test failure that was actually a false negative (test was wrong) :D
    git-svn-id: file:///home/projects/mule/scm@4353 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/tests/core/src/main/java/org/mule/tck/testmodels/mule/TestConnector.java
+++ trunk/mule/tests/core/src/main/java/org/mule/tck/testmodels/mule/TestConnector.java
@@ -10,10 +10,6 @@
 
 package org.mule.tck.testmodels.mule;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
 import org.mule.providers.AbstractConnector;
 import org.mule.providers.AbstractMessageAdapter;
 import org.mule.providers.AbstractMessageDispatcherFactory;
@@ -31,25 +27,15 @@ import org.mule.umo.provider.UMOMessageDispatcher;
 import org.mule.umo.provider.UMOMessageReceiver;
 import org.mule.umo.provider.UMOStreamMessageAdapter;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
  * <code>TestConnector</code> use a mock connector
  */
 public class TestConnector extends AbstractConnector
 {
-    /**
-     * The connector can pool dispatchers based on their endpointUri or can ingnore
-     * the endpointUri altogether and use a ThreadLocal or always create new.
-     * 
-     * @param endpoint the endpoint that can be used to key cached dispatchers
-     * @return the component associated with the endpointUri If there is no component
-     *         for the current thread one will be created
-     * @throws org.mule.umo.UMOException if creation of a component fails
-     */
-    // TODO HH: remove this?
-    public UMOMessageDispatcher getDispatcher(UMOImmutableEndpoint endpoint) throws UMOException
-    {
-        return new TestMessageDispatcher(endpoint);
-    }
 
     public TestConnector()
     {
@@ -63,61 +49,31 @@ public class TestConnector extends AbstractConnector
         });
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.mule.providers.AbstractConnector#doDispose()
-     */
     protected void doDispose()
     {
         // template method
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.mule.providers.AbstractConnector#doInitialise()
-     */
     public void doInitialise() throws InitialisationException
     {
         // template method
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.mule.umo.provider.UMOConnector#getProtocol()
-     */
     public String getProtocol()
     {
         return "test";
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.mule.providers.AbstractConnector#doStart()
-     */
     protected void doStart() throws UMOException
     {
         // template method
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.mule.providers.AbstractConnector#doStop()
-     */
     protected void doStop() throws UMOException
     {
         // template method
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.mule.umo.provider.UMOConnector#getMessageAdapter(java.lang.Object)
-     */
     public UMOMessageAdapter getMessageAdapter(Object message) throws MessagingException
     {
         return new DummyMessageAdapter(message);
@@ -165,32 +121,17 @@ public class TestConnector extends AbstractConnector
             this.message = message;
         }
 
-        /*
-         * (non-Javadoc)
-         * 
-         * @see org.mule.umo.provider.UMOMessageAdapter#getPayload()
-         */
         public Object getPayload()
         {
             return message;
         }
 
-        /*
-         * (non-Javadoc)
-         * 
-         * @see org.mule.umo.provider.UMOMessageAdapter#getPayloadAsBytes()
-         */
         public byte[] getPayloadAsBytes() throws Exception
         {
 
             return message.toString().getBytes();
         }
 
-        /*
-         * (non-Javadoc)
-         * 
-         * @see org.mule.umo.provider.UMOMessageAdapter#getPayloadAsString()
-         */
         public String getPayloadAsString(String encoding) throws Exception
         {
             return message.toString();
