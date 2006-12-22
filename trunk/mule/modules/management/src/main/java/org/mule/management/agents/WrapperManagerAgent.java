3e495faa821cfb64ce2ab6986e20ad86d5855b64
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2006-12-22 02:13:22 +0000

    First cut at MULE-379 (yeah! :) Though already very functional, I just want to check if we need better integration and/or tighter security control. It can be refactored to be fully contained in e.g. JmxAgent later and controlled by a property.
    git-svn-id: file:///home/projects/mule/scm@4371 bf997673-6b11-0410-b953-e057580c5b09

---
+++ trunk/mule/modules/management/src/main/java/org/mule/management/agents/WrapperManagerAgent.java
@@ -0,0 +1,152 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.management.agents;
+
+import org.mule.config.i18n.Message;
+import org.mule.config.i18n.Messages;
+import org.mule.management.support.AutoDiscoveryJmxSupportFactory;
+import org.mule.management.support.JmxSupport;
+import org.mule.management.support.JmxSupportFactory;
+import org.mule.umo.UMOException;
+import org.mule.umo.lifecycle.InitialisationException;
+import org.mule.umo.manager.UMOAgent;
+
+import java.util.List;
+
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanRegistrationException;
+import javax.management.MBeanServer;
+import javax.management.MBeanServerFactory;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.tanukisoftware.wrapper.jmx.WrapperManager;
+
+/**
+ *
+ */
+public class WrapperManagerAgent implements UMOAgent {
+    public static final String WRAPPER_OBJECT_NAME = "name=WrapperManager";
+
+    private static final Log logger = LogFactory.getLog(WrapperManagerAgent.class);
+
+    private String name = "Wrapper Manager";
+    private MBeanServer mBeanServer;
+    private ObjectName wrapperName;
+
+    private JmxSupportFactory jmxSupportFactory = new AutoDiscoveryJmxSupportFactory();
+    private JmxSupport jmxSupport;
+    private WrapperManager wrapperManager;
+
+    /* @see org.mule.umo.lifecycle.Initialisable#initialise() */
+    public void initialise() throws InitialisationException {
+        jmxSupport = jmxSupportFactory.newJmxSupport();
+        final List servers = MBeanServerFactory.findMBeanServer(null);
+        if (servers.isEmpty())
+        {
+            // TODO construct proper exception
+            throw new RuntimeException("no mbean servers found");
+        }
+
+        try
+        {
+            mBeanServer = (MBeanServer) servers.get(0);
+
+            wrapperName = jmxSupport.getObjectName(jmxSupport.getDomainName() + ":" + WRAPPER_OBJECT_NAME);
+
+            unregisterMBeansIfNecessary();
+            wrapperManager = new WrapperManager();
+            mBeanServer.registerMBean(wrapperManager, wrapperName);
+        }
+
+        catch (Exception e)
+        {
+            throw new InitialisationException(new Message(Messages.FAILED_TO_START_X, "wrapper agent"), e, this);
+        }
+    }
+
+    /* @see org.mule.umo.lifecycle.Startable#start() */
+    public void start() throws UMOException {
+        // no-op
+    }
+
+    /* @see org.mule.umo.lifecycle.Stoppable#stop() */
+    public void stop() throws UMOException
+    {
+        // no-op
+    }
+
+    /**
+     * Unregister all Mx4j MBeans if there are any left over the old deployment
+     */
+    protected void unregisterMBeansIfNecessary()
+        throws MalformedObjectNameException, InstanceNotFoundException, MBeanRegistrationException {
+        if (mBeanServer == null)
+        {
+            return;
+        }
+        if (mBeanServer.isRegistered(wrapperName))
+        {
+            mBeanServer.unregisterMBean(wrapperName);
+        }
+    }
+
+    /* @see org.mule.umo.lifecycle.Disposable#dispose() */
+    public void dispose()
+    {
+        try
+        {
+            unregisterMBeansIfNecessary();
+        }
+        catch (Exception e)
+        {
+            logger.error("Couldn't unregister MBean: "
+                         + (wrapperName != null ? wrapperName.getCanonicalName() : "null"), e);
+        }
+    }
+
+    /* @see org.mule.umo.manager.UMOAgent#registered() */
+    public void registered()
+    {
+        // nothing to do
+    }
+
+    /* @see org.mule.umo.manager.UMOAgent#unregistered() */
+    public void unregistered()
+    {
+        // nothing to do
+    }
+
+    // /////////////////////////////////////////////////////////////////////////
+    // Getters and setters
+    // /////////////////////////////////////////////////////////////////////////
+
+    /* @see org.mule.umo.manager.UMOAgent#getDescription() */
+    public String getDescription()
+    {
+        return "Wrapper Manager: Mule PID #" + wrapperManager.getJavaPID() +
+               ", Wrapper PID #" + wrapperManager.getWrapperPID();
+    }
+
+    /* @see org.mule.umo.manager.UMOAgent#getName() */
+    public String getName()
+    {
+        return this.name;
+    }
+
+    /* @see org.mule.umo.manager.UMOAgent#setName(java.lang.String) */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
