f0122c2c5ede5a34edc16c8f758c5aa60d8aee6a
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2007-01-07 18:52:29 +0000

    just use the snapshot instead of shuffling the same array around multiple times for no good reason
    git-svn-id: file:///home/projects/mule/scm@4578 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/core/src/main/java/org/mule/routing/inbound/AbstractEventResequencer.java
+++ trunk/mule/core/src/main/java/org/mule/routing/inbound/AbstractEventResequencer.java
@@ -13,35 +13,32 @@ package org.mule.routing.inbound;
 import org.mule.umo.MessagingException;
 import org.mule.umo.UMOEvent;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import org.apache.commons.collections.IteratorUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 /**
  * <code>AbstractEventResequencer</code> is used to receive a set of events,
  * resequence them and forward them on to their destination
- * 
- * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
- * @version $Revision$
  */
 
 public abstract class AbstractEventResequencer extends SelectiveConsumer
 {
     protected static final String NO_CORRELATION_ID = "no-id";
 
-    /**
-     * logger used by this class
-     */
-    protected static Log logger = LogFactory.getLog(AbstractEventResequencer.class);
+    private volatile Comparator comparator;
+    private final Map eventGroups = new HashMap();
 
-    private Comparator comparator;
-    private Map eventGroups = new HashMap();
+    public AbstractEventResequencer()
+    {
+        super();
+    }
+
+    public void setComparator(Comparator eventComparator)
+    {
+        this.comparator = eventComparator;
+    }
 
     public UMOEvent[] process(UMOEvent event) throws MessagingException
     {
@@ -50,11 +47,8 @@ public abstract class AbstractEventResequencer extends SelectiveConsumer
             EventGroup eg = addEvent(event);
             if (shouldResequence(eg))
             {
-                List resequencedEvents = resequenceEvents(eg);
                 removeGroup(eg.getGroupId());
-                UMOEvent[] returnEvents = new UMOEvent[resequencedEvents.size()];
-                resequencedEvents.toArray(returnEvents);
-                return returnEvents;
+                return resequenceEvents(eg);
             }
         }
         return null;
@@ -63,10 +57,13 @@ public abstract class AbstractEventResequencer extends SelectiveConsumer
     protected EventGroup addEvent(UMOEvent event)
     {
         String cId = event.getMessage().getCorrelationId();
+
         if (cId == null)
         {
             cId = NO_CORRELATION_ID;
         }
+
+        // TODO HH: not atomic
         EventGroup eg = (EventGroup)eventGroups.get(cId);
         if (eg == null)
         {
@@ -78,6 +75,7 @@ public abstract class AbstractEventResequencer extends SelectiveConsumer
         {
             eg.addEvent(event);
         }
+
         return eg;
     }
 
@@ -86,29 +84,22 @@ public abstract class AbstractEventResequencer extends SelectiveConsumer
         eventGroups.remove(id);
     }
 
-    protected List resequenceEvents(EventGroup events)
+    protected UMOEvent[] resequenceEvents(EventGroup events)
     {
-        List result = IteratorUtils.toList(events.iterator(), events.size());
+        UMOEvent[] result = events.toArray();
+
         if (comparator != null)
         {
-            Collections.sort(result, comparator);
+            Arrays.sort(result, comparator);
         }
         else
         {
             logger.warn("Event comparator is null, events were not reordered");
         }
+
         return result;
     }
 
-    public Comparator getComparator()
-    {
-        return comparator;
-    }
-
-    public void setComparator(Comparator comparator)
-    {
-        this.comparator = comparator;
-    }
-
     protected abstract boolean shouldResequence(EventGroup events);
+
 }
