7fa9f966d0d6e7a28d8dfcabc8f8407a266102d9
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2007-01-07 17:03:26 +0000

    some work for Ross ;)
    git-svn-id: file:///home/projects/mule/scm@4549 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/tests/integration/src/test/java/org/mule/test/integration/providers/jms/activemq/MultipleConnectorsAndTransactionsTestCase.java
+++ trunk/mule/tests/integration/src/test/java/org/mule/test/integration/providers/jms/activemq/MultipleConnectorsAndTransactionsTestCase.java
@@ -21,6 +21,7 @@ public class MultipleConnectorsAndTransactionsTestCase extends FunctionalTestCas
 
     public void testDispatchingToSeparateEndpoints() throws Exception
     {
-
+        // TODO RM: something missing here?
     }
+
 }
