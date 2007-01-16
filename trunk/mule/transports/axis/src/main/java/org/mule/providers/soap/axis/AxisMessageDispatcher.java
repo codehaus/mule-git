1b00919dd2950c28d8bcd4dc43fa5478503c2132
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2007-01-16 10:35:36 +0000

    one more fix for the occasional Axis NPE hiccup
    git-svn-id: file:///home/projects/mule/scm@4704 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/transports/axis/src/main/java/org/mule/providers/soap/axis/AxisMessageDispatcher.java
+++ trunk/mule/transports/axis/src/main/java/org/mule/providers/soap/axis/AxisMessageDispatcher.java
@@ -249,13 +249,17 @@ public class AxisMessageDispatcher extends AbstractMessageDispatcher
         // set Mule event here so that handlers can extract info
         call.setProperty(MuleProperties.MULE_EVENT_PROPERTY, event);
         call.setProperty(MuleProperties.MULE_ENDPOINT_PROPERTY, event.getEndpoint());
-        
+
         // set custom properties
-        Object head;
-        for (Iterator iter = event.getMessage().getPropertyNames().iterator(); iter.hasNext();){           
-            if ((head = iter.next()) != null){
-                if ((!((String)head).startsWith(MuleProperties.PROPERTY_PREFIX)) && (!(event.getMessage().getProperty((String)head) == null))){
-                    call.setProperty((String)head, event.getMessage().getProperty((String)head));
+        for (Iterator iter = event.getMessage().getPropertyNames().iterator(); iter.hasNext();)
+        {
+            String key = (String)iter.next();
+            if (!(key.startsWith(MuleProperties.PROPERTY_PREFIX)))
+            {
+                Object value = event.getMessage().getProperty(key);
+                if (value != null)
+                {
+                    call.setProperty(key, value);
                 }
             }
         }
