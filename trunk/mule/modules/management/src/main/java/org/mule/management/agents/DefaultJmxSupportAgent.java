e0353659e96e7f1840a1d63a2c0d351ec3c4b771
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2007-02-08 00:56:11 +0000

    Make credentials configurable on the default support agent, related to MULE-1335
    git-svn-id: file:///home/projects/mule/scm@5043 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/modules/management/src/main/java/org/mule/management/agents/DefaultJmxSupportAgent.java
+++ trunk/mule/modules/management/src/main/java/org/mule/management/agents/DefaultJmxSupportAgent.java
@@ -17,6 +17,8 @@ import org.mule.umo.manager.UMOAgent;
 import org.mule.util.StringUtils;
 
 import java.text.MessageFormat;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * TODO document.
@@ -32,6 +34,12 @@ public class DefaultJmxSupportAgent implements UMOAgent
     private boolean loadMx4jAgent = false;
     private String port;
     private String host;
+
+    /**
+     * Username/password combinations for JMX Remoting
+     * authentication.
+     */
+    private Map credentials = new HashMap();
 
     /**
      * Gets the name of this agent
@@ -63,21 +71,25 @@ public class DefaultJmxSupportAgent implements UMOAgent
         return "Default Jmx Agent Support";
     }
 
+    /** {@inheritDoc} */
     public void registered()
     {
         // nothing to do
     }
 
+    /** {@inheritDoc} */
     public void unregistered()
     {
         // nothing to do
     }
 
+    /** {@inheritDoc} */
     public void start() throws UMOException
     {
         // nothing to do
     }
 
+    /** {@inheritDoc} */
     public void stop() throws UMOException
     {
         // nothing to do
@@ -172,6 +184,10 @@ public class DefaultJmxSupportAgent implements UMOAgent
                                          new Object[] {StringUtils.defaultString(host, DEFAULT_HOST),
                                                        StringUtils.defaultString(port, DEFAULT_PORT)});
         }
+        if (credentials != null && !credentials.isEmpty())
+        {
+            agent.setCredentials(credentials);
+        }
         agent.setConnectorServerUrl(remotingUri);
         return agent;
     }
@@ -209,44 +225,95 @@ public class DefaultJmxSupportAgent implements UMOAgent
         return MuleManager.getInstance().lookupAgent(agent.getName()) != null;
     }
 
+    /**
+     * Getter for property 'loadJdmkAgent'.
+     *
+     * @return Value for property 'loadJdmkAgent'.
+     */
     public boolean isLoadJdmkAgent()
     {
         return loadJdmkAgent;
     }
 
+    /**
+     * Setter for property 'loadJdmkAgent'.
+     *
+     * @param loadJdmkAgent Value to set for property 'loadJdmkAgent'.
+     */
     public void setLoadJdmkAgent(boolean loadJdmkAgent)
     {
         this.loadJdmkAgent = loadJdmkAgent;
     }
 
+    /**
+     * Getter for property 'loadMx4jAgent'.
+     *
+     * @return Value for property 'loadMx4jAgent'.
+     */
     public boolean isLoadMx4jAgent()
     {
         return loadMx4jAgent;
     }
 
+    /**
+     * Setter for property 'loadMx4jAgent'.
+     *
+     * @param loadMx4jAgent Value to set for property 'loadMx4jAgent'.
+     */
     public void setLoadMx4jAgent(boolean loadMx4jAgent)
     {
         this.loadMx4jAgent = loadMx4jAgent;
     }
 
 
+    /**
+     * Getter for property 'port'.
+     *
+     * @return Value for property 'port'.
+     */
     public String getPort()
     {
         return port;
     }
 
+    /**
+     * Setter for property 'port'.
+     *
+     * @param port Value to set for property 'port'.
+     */
     public void setPort(final String port)
     {
         this.port = port;
     }
 
+    /**
+     * Getter for property 'host'.
+     *
+     * @return Value for property 'host'.
+     */
     public String getHost()
     {
         return host;
     }
 
+    /**
+     * Setter for property 'host'.
+     *
+     * @param host Value to set for property 'host'.
+     */
     public void setHost(final String host)
     {
         this.host = host;
+    }
+
+
+    /**
+     * Setter for property 'credentials'.
+     *
+     * @param credentials Value to set for property 'credentials'.
+     */
+    public void setCredentials(final Map credentials)
+    {
+        this.credentials = credentials;
     }
 }
