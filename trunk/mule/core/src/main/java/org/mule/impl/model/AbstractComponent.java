0162dff791edbff09c90652f6ef8aff140b25052
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2007-02-21 15:01:54 +0000

    fix b0rken description
    git-svn-id: file:///home/projects/mule/scm@5254 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/core/src/main/java/org/mule/impl/model/AbstractComponent.java
+++ trunk/mule/core/src/main/java/org/mule/impl/model/AbstractComponent.java
@@ -517,15 +517,6 @@ public abstract class AbstractComponent implements UMOComponent
 
     protected abstract void doDispatch(UMOEvent event) throws UMOException;
 
-    /**
-     * Gets the underlying instance form this component Where the Component
-     * implmentation provides pooling this is no 1-2-1 mapping between UMOComponent
-     * and instance, so this method will return the object in initial state. <p/> If
-     * the underlying component is Container managed in Spring or another IoC
-     * container then the object instance in the IoC container will be returned
-     * 
-     * @return the underlying instance form this component
-     */
     public Object getInstance() throws UMOException
     {
         return lookupComponent();
