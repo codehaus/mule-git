66c8debe2adffd1f2ecba38fa73401d41f4b0824
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2007-02-21 13:40:15 +0000

    MULE-904: fixes for handling of InterruptedException, better concurrency, clarify behaviour when number of registrations != deregistrations (they must match)
    git-svn-id: file:///home/projects/mule/scm@5252 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/core/src/main/java/org/mule/impl/internal/notifications/ServerNotificationManager.java
+++ trunk/mule/core/src/main/java/org/mule/impl/internal/notifications/ServerNotificationManager.java
@@ -19,16 +19,17 @@ import org.mule.umo.lifecycle.LifecycleException;
 import org.mule.umo.manager.UMOServerNotification;
 import org.mule.umo.manager.UMOServerNotificationListener;
 import org.mule.umo.manager.UMOWorkManager;
+import org.mule.util.concurrent.ConcurrentHashSet;
 
 import edu.emory.mathcs.backport.java.util.concurrent.BlockingDeque;
 import edu.emory.mathcs.backport.java.util.concurrent.ConcurrentHashMap;
-import edu.emory.mathcs.backport.java.util.concurrent.CopyOnWriteArrayList;
+import edu.emory.mathcs.backport.java.util.concurrent.ConcurrentMap;
 import edu.emory.mathcs.backport.java.util.concurrent.LinkedBlockingDeque;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 import javax.resource.spi.work.Work;
 import javax.resource.spi.work.WorkException;
@@ -51,18 +52,17 @@ public class ServerNotificationManager implements Work, Disposable
 
     public static final String NULL_SUBSCRIPTION = "NULL";
 
-    private Map eventsMap;
+    private ConcurrentMap eventsMap;
     private BlockingDeque eventQueue;
-    private List listeners;
+    private Set listeners;
     private WorkListener workListener;
     private volatile boolean disposed = false;
 
     public ServerNotificationManager()
     {
-        // listenersMap = new ConcurrentHashMap();
         eventsMap = new ConcurrentHashMap();
         eventQueue = new LinkedBlockingDeque();
-        listeners = new CopyOnWriteArrayList();
+        listeners = new ConcurrentHashSet();
         workListener = MuleManager.getConfiguration().getWorkListener();
     }
 
@@ -82,10 +82,13 @@ public class ServerNotificationManager implements Work, Disposable
     {
         if (UMOServerNotification.class.isAssignableFrom(eventType))
         {
-            if (!eventsMap.containsKey(listenerType))
+            Class previousEventType = (Class)eventsMap.putIfAbsent(listenerType, eventType);
+            if (previousEventType != null)
             {
-                // listenersMap.put(eventType, new TreeMap(comparator));
-                eventsMap.put(listenerType, eventType);
+                eventType = previousEventType;
+            }
+            else
+            {
                 if (logger.isDebugEnabled())
                 {
                     logger.debug("Registered event type: " + eventType);
@@ -104,7 +107,7 @@ public class ServerNotificationManager implements Work, Disposable
 
     public void registerListener(UMOServerNotificationListener listener) throws NotificationException
     {
-        registerListener(listener, null);
+        this.registerListener(listener, null);
     }
 
     public void registerListener(UMOServerNotificationListener listener, String subscription)
@@ -135,7 +138,7 @@ public class ServerNotificationManager implements Work, Disposable
 
         if (notification instanceof BlockingServerEvent)
         {
-            notifyListeners(notification);
+            this.notifyListeners(notification);
         }
         else
         {
@@ -145,7 +148,10 @@ public class ServerNotificationManager implements Work, Disposable
             }
             catch (InterruptedException e)
             {
-                logger.error("Failed to queue notification: " + notification, e);
+                if (!disposed)
+                {
+                    logger.error("Failed to queue notification: " + notification, e);
+                }
             }
         }
     }
@@ -153,7 +159,6 @@ public class ServerNotificationManager implements Work, Disposable
     public void dispose()
     {
         disposed = true;
-        // listenersMap.clear();
         eventsMap.clear();
         eventQueue.clear();
         listeners.clear();
@@ -184,7 +189,7 @@ public class ServerNotificationManager implements Work, Disposable
 
     public void release()
     {
-        dispose();
+        this.dispose();
     }
 
     /**
@@ -205,15 +210,14 @@ public class ServerNotificationManager implements Work, Disposable
                 UMOServerNotification notification = (UMOServerNotification)eventQueue.take();
                 if (notification != null)
                 {
-                    notifyListeners(notification);
+                    this.notifyListeners(notification);
                 }
             }
             catch (InterruptedException e)
             {
                 if (!disposed)
                 {
-                    logger.error("Failed to take notificationication from server notificationication queue",
-                        e);
+                    logger.error("Failed to take notification from queue", e);
                 }
             }
         }
@@ -263,7 +267,7 @@ public class ServerNotificationManager implements Work, Disposable
 
         public boolean matches(UMOServerNotification notification)
         {
-            if (subscriptionMatches(notification))
+            if (this.subscriptionMatches(notification))
             {
                 for (Iterator iterator = notificationClasses.iterator(); iterator.hasNext();)
                 {
