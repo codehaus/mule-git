bb0ad71a5bdb1b56b228bf31aa948e432186e768
rossmason <rossmason@bf997673-6b11-0410-b953-e057580c5b09>
2007-03-01 11:09:32 +0000

    reworked the existing Http functional tests to use Xml configuration and the MuleClient to make requests.
    git-svn-id: file:///home/projects/mule/scm@5370 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/transports/http/src/test/java/org/mule/providers/http/functional/JettyHttpFunctionalTestCase.java
+++ trunk/mule/transports/http/src/test/java/org/mule/providers/http/functional/JettyHttpFunctionalTestCase.java
@@ -10,79 +10,12 @@
 
 package org.mule.providers.http.functional;
 
-import java.net.URI;
-
-import org.apache.commons.httpclient.HttpConnection;
-import org.apache.commons.httpclient.HttpState;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.methods.StringRequestEntity;
-import org.mule.impl.endpoint.MuleEndpointURI;
-import org.mule.providers.http.jetty.JettyConnector;
-import org.mule.umo.endpoint.MalformedEndpointException;
-import org.mule.umo.endpoint.UMOEndpointURI;
-import org.mule.umo.provider.UMOConnector;
-
-/**
- * @author <a href="mailto:ross.mason@symphonysoft.com">Ross Mason</a>
- * @version $Revision$
- */
 public class JettyHttpFunctionalTestCase extends HttpFunctionalTestCase
 {
-    protected static final String TEST_MESSAGE = "Test Http Request";
 
-    private HttpConnection cnn;
-
-    protected UMOEndpointURI getInDest()
+    //@Override
+    protected String getConfigResources()
     {
-        try
-        {
-            return new MuleEndpointURI("jetty:http://localhost:60198");
-        }
-        catch (MalformedEndpointException e)
-        {
-            fail(e.getMessage());
-            return null;
-        }
-    }
-
-    protected UMOEndpointURI getOutDest()
-    {
-        return null;
-    }
-
-    protected UMOConnector createConnector() throws Exception
-    {
-        JettyConnector connector = new JettyConnector();
-        connector.setName("testJettyHttp");
-
-        return connector;
-    }
-
-    protected void sendTestData(int iterations) throws Exception
-    {
-        URI uri = getInDest().getUri();
-        PostMethod postMethod = new PostMethod(uri.toString());
-        postMethod.setRequestEntity(new StringRequestEntity(TEST_MESSAGE));
-        cnn = new HttpConnection(uri.getHost(), uri.getPort());
-        cnn.open();
-        postMethod.execute(new HttpState(), cnn);
-    }
-
-    //
-    protected void receiveAndTestResults() throws Exception
-    {
-        Thread.sleep(1000);
-        byte[] buf = new byte[1024 * 4];
-
-        int len = cnn.getResponseInputStream().read(buf);
-        if (len < 1)
-        {
-            fail("Nothing was sent back in the response");
-        }
-        String msg = new String(buf, 0, len);
-
-        assertNotNull(msg);
-        // Todo regression assertTrue(msg.endsWith(TEST_MESSAGE + " Received"));
-        assertTrue(msg.indexOf(TEST_MESSAGE + " Received") > -1);
+        return "jetty-http-functional-test.xml";
     }
 }
