351e716b5ef6cb3e21748b88dd09b55aff66c9d9
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2007-03-05 12:24:20 +0000

    Fix "connection closed" errors: in this case they were caused by immediate shutdown of the JMS connector after the end of the test method. Since dispatch is async, the messages could not make it through in time and rightfully complain that their passage had been cut off.
    git-svn-id: file:///home/projects/mule/scm@5415 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/transports/jms/src/test/java/org/mule/test/integration/providers/jms/MultipleJmsConnectorsTestCase.java
+++ trunk/mule/transports/jms/src/test/java/org/mule/test/integration/providers/jms/MultipleJmsConnectorsTestCase.java
@@ -19,8 +19,8 @@ public class MultipleJmsConnectorsTestCase extends AbstractMuleTestCase
 {
     public void testMultipleJmsClientConnections() throws Exception
     {
-        ActiveMQConnectionFactory factory =
-                new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false&broker.useJmx=false");
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(
+            "vm://localhost?broker.persistent=false&broker.useJmx=false");
 
         MuleClient client = new MuleClient();
         client.setProperty("jms.connectionFactory", factory);
@@ -28,6 +28,9 @@ public class MultipleJmsConnectorsTestCase extends AbstractMuleTestCase
         client.getManager().start();
         client.dispatch("jms://admin:admin@admin.queue?createConnector=ALWAYS", "testing", null);
         client.dispatch("jms://ross:ross@ross.queue?createConnector=ALWAYS", "testing", null);
+
+        // wait a bit to let the messages go on their way
+        Thread.sleep(3000);
 
         assertEquals(2, client.getManager().getConnectors().size());
     }
