7ee21fdb158069752603ce57b3bab15edf307a20
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2007-03-16 12:31:22 +0000

    misc. warning fixes, cleanups, @author removals
    git-svn-id: file:///home/projects/mule/scm@5643 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/transports/http/src/test/java/org/mule/providers/http/HttpsConnectorTestCase.java
+++ trunk/mule/transports/http/src/test/java/org/mule/providers/http/HttpsConnectorTestCase.java
@@ -77,8 +77,10 @@ public class HttpsConnectorTestCase extends AbstractConnectorTestCase
             fail("cannot register with null endpointUri");
         }
         catch (Exception e)
-        { /* expected */
+        {
+            /* expected */
         }
+
         endpoint.setEndpointURI(null);
         try
         {
@@ -86,7 +88,8 @@ public class HttpsConnectorTestCase extends AbstractConnectorTestCase
             fail("cannot register with empty endpointUri");
         }
         catch (Exception e)
-        { /* expected */
+        {
+            /* expected */
         }
 
         endpoint.setEndpointURI(new MuleEndpointURI("https://localhost:0"));
@@ -97,8 +100,10 @@ public class HttpsConnectorTestCase extends AbstractConnectorTestCase
             fail("cannot register on the same endpointUri");
         }
         catch (Exception e)
-        { /* expected */
+        {
+            /* expected */
         }
+
         connector.dispose();
     }
 
@@ -106,10 +111,10 @@ public class HttpsConnectorTestCase extends AbstractConnectorTestCase
     {
         HttpsConnector c = (HttpsConnector)getConnector();
 
-        c.setBufferSize(1024);
-        assertEquals(1024, c.getBufferSize());
-        c.setBufferSize(0);
-        assertEquals(TcpConnector.DEFAULT_BUFFER_SIZE, c.getBufferSize());
+        c.setSendBufferSize(1024);
+        assertEquals(1024, c.getSendBufferSize());
+        c.setSendBufferSize(0);
+        assertEquals(TcpConnector.DEFAULT_BUFFER_SIZE, c.getSendBufferSize());
 
         c.dispose();
         // all kinds of timeouts are tested in TcpConnectorTestCase now
