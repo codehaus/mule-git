3681318d4137c4e5c8bdc89ceda548f445677dc7
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2007-04-03 21:16:26 +0000

    Give it a more generic name (can be used both inside and outside the container). The other one would be a native non-JMS connector for WMQ.
    git-svn-id: file:///home/projects/mule/scm@5841 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/transports/jms/src/main/java/org/mule/providers/jms/websphere/WebsphereJmsConnector.java
+++ trunk/mule/transports/jms/src/main/java/org/mule/providers/jms/websphere/WebsphereJmsConnector.java
@@ -13,12 +13,12 @@ package org.mule.providers.jms.websphere;
 import org.mule.providers.jms.JmsConnector;
 
 /**
- * Websphere-specific JMS connector for the Default (embedded) provider.
+ * Websphere-specific JMS connector.
  */
-public class WebsphereEmbededJmsConnector extends JmsConnector
+public class WebsphereJmsConnector extends JmsConnector
 {
-    /** Constructs a new WebsphereEmbededJmsConnector. */
-    public WebsphereEmbededJmsConnector()
+    /** Constructs a new WebsphereJmsConnector. */
+    public WebsphereJmsConnector()
     {
         setRecoverJmsConnections(false);
     }
