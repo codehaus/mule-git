598c65ea59f9b3172c1af507c42d4b4de783b810
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2007-04-04 00:52:43 +0000

    Removed 'funny' characters. They add nothing to the test, but properly managing those would require filtering source files through native2ascii, then back in various IDEs on different platforms, etc, you get it...
    There is still an issue with a handshake exception thrown, which wasn't there before:
    
    javax.net.ssl.SSLException: Received close_notify during handshake
    
    git-svn-id: file:///home/projects/mule/scm@5843 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/transports/ssl/src/test/java/org/mule/providers/ssl/SslFunctionalTestCase.java
+++ trunk/mule/transports/ssl/src/test/java/org/mule/providers/ssl/SslFunctionalTestCase.java
@@ -1,3 +1,13 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.providers.ssl;
 
 import org.mule.tck.FunctionalTestCase;
@@ -9,7 +19,7 @@ import java.util.HashMap;
 
 public class SslFunctionalTestCase extends FunctionalTestCase {
 
-    protected static String TEST_MESSAGE = "Test SSL Request (R�dgr�d), 57 = \u06f7\u06f5 in Arabic";
+    protected static String TEST_MESSAGE = "Test SSL Request";
 
     public SslFunctionalTestCase()
     {
