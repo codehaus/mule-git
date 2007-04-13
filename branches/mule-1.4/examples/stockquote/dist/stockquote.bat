82bc00a1a5a1d0ac96d6088e239fb26ba8221b8e
tcarlson <tcarlson@bf997673-6b11-0410-b953-e057580c5b09>
2007-04-13 20:44:44 +0000

    Disable WSDL until MULE-1613 is fixed
    git-svn-id: file:///home/projects/mule/scm@6022 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-1.4/examples/stockquote/dist/stockquote.bat
+++ branches/mule-1.4/examples/stockquote/dist/stockquote.bat
@@ -8,12 +8,13 @@ SET MULE_LIB=.\conf
 
 ECHO (make sure you have configured your HTTP proxy if behind a firewall - see README.txt)
 ECHO.
-ECHO The Stock Quote example is available in three variations:
+REM ECHO The Stock Quote example is available in three variations:
+ECHO The Stock Quote example is available in two variations:
 ECHO   1. REST
 ECHO   2. SOAP
-ECHO   3. WSDL
+REM ECHO   3. WSDL
 SET /P Choice=Select the one you wish to execute and press Enter...
 
 IF '%Choice%'=='1' call %MULE_BASE%\bin\mule.bat -config .\conf\rest-config.xml
 IF '%Choice%'=='2' call %MULE_BASE%\bin\mule.bat -config .\conf\soap-config.xml
-IF '%Choice%'=='3' call %MULE_BASE%\bin\mule.bat -config .\conf\wsdl-config.xml
+REM IF '%Choice%'=='3' call %MULE_BASE%\bin\mule.bat -config .\conf\wsdl-config.xml
