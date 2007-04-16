68ca78f816502dfafa089645563dde41ee124871
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2007-04-16 15:14:21 +0000

    fixed warnings and simplified code
    git-svn-id: file:///home/projects/mule/scm@6031 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/transports/jms/src/test/java/org/mule/tck/testmodels/services/TestJMSComponent.java
+++ trunk/mule/transports/jms/src/test/java/org/mule/tck/testmodels/services/TestJMSComponent.java
@@ -10,20 +10,17 @@
 
 package org.mule.tck.testmodels.services;
 
-import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicInteger;
-
 import javax.jms.TextMessage;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 public class TestJMSComponent extends TestComponent
 {
     public String receiveJms(TextMessage message) throws Exception
     {
-        logger.info("Received: " + message.getText() + " Number: " + inc() + " in thread: "
-                    + Thread.currentThread().getName());
-        return "Received: " + message.getText();
+        String answer = "Received: " + message.getText();
+
+        logger.info(answer + " Number: " + inc() + " in thread: " + Thread.currentThread().getName());
+
+        return answer;
     }
 
 }
--- trunk/mule/transports/jms/src/test/java/org/mule/tck/testmodels/services/TestJMSReceiver.java
+++ trunk/mule/transports/jms/src/test/java/org/mule/tck/testmodels/services/TestJMSReceiver.java
@@ -10,27 +10,20 @@
 
 package org.mule.tck.testmodels.services;
 
-import org.mule.impl.RequestContext;
-import org.mule.util.StringMessageUtils;
-
-import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicInteger;
-
 import javax.jms.TextMessage;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 public class TestJMSReceiver extends TestReceiver
 {
     public String receive(TextMessage message) throws Exception
     {
+        String answer = "Received: " + message.getText();
+
         if (logger.isDebugEnabled())
         {
-            logger.debug("Received: " + message.getText() + " Number: " + inc() + " in thread: "
-                         + Thread.currentThread().getName());
+            logger.debug(answer + " Number: " + inc() + " in thread: " + Thread.currentThread().getName());
             logger.debug("Message ID is: " + message.getJMSMessageID());
         }
 
-        return "Received: " + message.getText();
+        return answer;
     }
 }
