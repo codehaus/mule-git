9d86c2754f773c2fc31932537f4323a41ddbb606
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2007-04-30 13:38:41 +0000

    javadoc fixes
    git-svn-id: file:///home/projects/mule/scm@6220 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/core/src/main/java/org/mule/config/PoolingProfile.java
+++ trunk/mule/core/src/main/java/org/mule/config/PoolingProfile.java
@@ -28,17 +28,20 @@ public class PoolingProfile
 {
 
     /**
-     * Tells the object pool not to initialise any components on startup
+     * Tells the object pool not to initialise any components on startup. The
+     * configuration alias for this value is "INITIALISE_NONE".
      */
     public static final int POOL_INITIALISE_NO_COMPONENTS = 0;
 
     /**
-     * Tells the object pool only to initialise one component on startup
+     * Tells the object pool only to initialise one component on startup. The
+     * configuration alias for this value is "INITIALISE_ONE".
      */
     public static final int POOL_INITIALISE_ONE_COMPONENT = 1;
 
     /**
-     * Tells the object pool not to initialise any components on startup
+     * Tells the object pool not to initialise any components on startup. The
+     * configuration alias for this value is "INITIALISE_ALL".
      */
     public static final int POOL_INITIALISE_ALL_COMPONENTS = 2;
 
@@ -61,33 +64,36 @@ public class PoolingProfile
     public static final int DEFAULT_MAX_POOL_IDLE = ObjectPool.DEFAULT_MAX_SIZE;
 
     /**
-     * When the threadPoolExhaustedAction is setto 2 (WHEN_EXHAUSTED_BLOCK) this can
+     * When the threadPoolExhaustedAction is set to 2 (WHEN_EXHAUSTED_BLOCK) this can
      * specify the maximum milliseconds the pool should block before throwing a
      * NoSuchElementException
      */
     public static final long DEFAULT_MAX_POOL_WAIT = ObjectPool.DEFAULT_MAX_WAIT;
 
     /**
-     * Specifies the behaviour of the Mule UMO pool when the pool is exhausted: <p/>
-     * 0 (WHEN_EXHAUSTED_FAIL) : will throw a NoSuchElementException 1
-     * (WHEN_EXHAUSTED_BLOCK): will block (invoke Object.wait(long) until a new or
-     * idle object is available. 2 (WHEN_EXHAUSTED_GROW) : will create a new Mule and
-     * return it(essentially making maxActive meaningless.) <p/> If a positive
-     * maxWait value is supplied, it will block for at most that many milliseconds,
-     * after which a NoSuchElementException will be thrown. If maxThraedWait is
-     * non-positive, it will block indefinitely.
+     * Specifies the behaviour of the Mule UMO pool when the pool is exhausted:
+     * <ul>
+     * <li>0 (WHEN_EXHAUSTED_FAIL) : will throw a NoSuchElementException</li>
+     * <li>1 (WHEN_EXHAUSTED_BLOCK): will block (invoke Object.wait(long) until a
+     * new or idle object is available.</li>
+     * <li>2 (WHEN_EXHAUSTED_GROW) : will create a new Mule and return it
+     * (essentially making maxActive meaningless).</li>
+     * </ul>
+     * If a positive maxWait value is supplied, it will block for at most that many
+     * milliseconds, after which a NoSuchElementException will be thrown. If maxWait
+     * is non-positive, it will block indefinitely.
      */
     public static final int DEFAULT_POOL_EXHAUSTED_ACTION = ObjectPool.WHEN_EXHAUSTED_GROW;
 
     /**
-     * Determines how components in a pool should be initialised. the possible values
-     * are -
+     * Determines how components in a pool should be initialised. The possible values
+     * are:
      * <ul>
-     * <li>0 (POOL_INIT_NO_COMPONENTs) : Will not load any components in the pool on
+     * <li>0 (POOL_INITIALISE_NO_COMPONENTS) : Will not load any components in the pool on
      * startup</li>
-     * <li>1 (POOL_INIT_ONE_COMPONENT) : Will load only the first component in the
+     * <li>1 (POOL_INITIALISE_ONE_COMPONENT) : Will load only the first component in the
      * pool on startup</li>
-     * <li>2 (POOL_INIT_ALL_COMPONENTS) : Will load all components in the pool on
+     * <li>2 (POOL_INITIALISE_ALL_COMPONENTS) : Will load all components in the pool on
      * startup</li>
      * </ul>
      */
