4285675cb37707d847ded1d5ce651b56a093852e
acooke <acooke@bf997673-6b11-0410-b953-e057580c5b09>
2007-05-02 13:12:51 +0000

    MULE-1686 - Added test for repated TCP synch send (fails, excluded from POM).
    git-svn-id: file:///home/projects/mule/scm@6256 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/transports/tcp/src/test/java/org/mule/providers/tcp/issues/MultiSynchMule1692TestCase.java
+++ trunk/mule/transports/tcp/src/test/java/org/mule/providers/tcp/issues/MultiSynchMule1692TestCase.java
@@ -8,40 +8,35 @@
  * LICENSE.txt file.
  */
 
-package org.mule.providers.tcp.integration;
+package org.mule.providers.tcp.issues;
 
-import org.mule.MuleManager;
-import org.mule.extras.client.MuleClient;
-import org.mule.impl.model.streaming.StreamingComponent;
 import org.mule.tck.FunctionalTestCase;
 import org.mule.tck.functional.EventCallback;
 import org.mule.tck.functional.FunctionalStreamingTestComponent;
 import org.mule.umo.UMOEventContext;
 import org.mule.umo.UMOSession;
 import org.mule.umo.model.UMOModel;
+import org.mule.extras.client.MuleClient;
+import org.mule.MuleManager;
+import org.mule.impl.model.streaming.StreamingComponent;
 
 import java.util.HashMap;
 
-import edu.emory.mathcs.backport.java.util.concurrent.CountDownLatch;
-import edu.emory.mathcs.backport.java.util.concurrent.TimeUnit;
-import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicInteger;
-import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicReference;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import edu.emory.mathcs.backport.java.util.concurrent.CountDownLatch;
+import edu.emory.mathcs.backport.java.util.concurrent.TimeUnit;
+import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicReference;
 
-/**
- * This test is more about testing the streaming model than the TCP provider, really.
- * While the test passes, it shows some problems (multiple calls, errors).
- */
-public class StreamingTestCase extends FunctionalTestCase
+public class MultiSynchMule1692TestCase extends FunctionalTestCase
 {
 
-    private static final Log logger = LogFactory.getLog(StreamingTestCase.class);
+    private static final Log logger = LogFactory.getLog(MultiSynchMule1692TestCase.class);
     public static final int TIMEOUT = 3000;
     public static final String TEST_MESSAGE = "Test TCP Request";
     public static final String RESULT = "Received stream; length: 16; 'Test...uest'";
 
-    public StreamingTestCase()
+    public MultiSynchMule1692TestCase()
     {
         setDisposeManagerPerSuite(true);
     }
@@ -51,19 +46,14 @@ public class StreamingTestCase extends FunctionalTestCase
         return "tcp-streaming-test.xml";
     }
 
-    public void testSend() throws Exception
+    private EventCallback newCallback(final CountDownLatch latch, final AtomicReference message)
     {
-        final CountDownLatch latch = new CountDownLatch(1);
-        final AtomicReference message = new AtomicReference();
-        final AtomicInteger loopCount = new AtomicInteger(0);
-
-        EventCallback callback = new EventCallback()
+        return new EventCallback()
         {
             public synchronized void eventReceived(UMOEventContext context, Object component)
             {
                 try
                 {
-                    logger.info("called " + loopCount.incrementAndGet() + " times");
                     FunctionalStreamingTestComponent ftc = (FunctionalStreamingTestComponent) component;
                     // without this we may have problems with the many repeats
                     if (1 == latch.getCount())
@@ -79,36 +69,31 @@ public class StreamingTestCase extends FunctionalTestCase
                 }
             }
         };
-
+    }
+    
+    public void testSend() throws Exception
+    {
         MuleClient client = new MuleClient();
-
-        // this just creates another class - not the one used
-//        FunctionalStreamingTestComponent ftc =
-//                (FunctionalStreamingTestComponent) MuleManager.getInstance()
-//                        .getContainerContext().getComponent(
-//                        new ContainerKeyPair("mule", "testComponent"));
-
-        // this creates a new instance too
-//        UMOModel model = (UMOModel) MuleManager.getInstance().getModels().get("echo");
-//        FunctionalStreamingTestComponent ftc =
-//                (FunctionalStreamingTestComponent) model.getComponent("testComponent").getInstance();
 
         UMOModel model = (UMOModel) MuleManager.getInstance().getModels().get("echo");
         UMOSession session = model.getComponentSession("testComponent");
         StreamingComponent component = (StreamingComponent) session.getComponent();
         FunctionalStreamingTestComponent ftc = (FunctionalStreamingTestComponent) component.getComponent();
 
-        ftc.setEventCallback(callback, TEST_MESSAGE.length());
-
+        final CountDownLatch latch = new CountDownLatch(1);
+        final AtomicReference message = new AtomicReference();
+        ftc.setEventCallback(newCallback(latch, message), TEST_MESSAGE.length());
         client.dispatch("tcp://localhost:65432", TEST_MESSAGE, new HashMap());
-
-        // we could have called client.dispatchStream instead, it makes no difference,
-        // since everything gets put on the socket output stream anyway.  but that is
-        // the "stream provider" functionality, rather than the "streaming model"
-        // functionality...
-
         latch.await(10, TimeUnit.SECONDS);
         assertEquals(RESULT, message.get());
+
+        final CountDownLatch latch2 = new CountDownLatch(1);
+        final AtomicReference message2 = new AtomicReference();
+        ftc.setEventCallback(newCallback(latch2, message2), TEST_MESSAGE.length());
+        client.dispatch("tcp://localhost:65432", TEST_MESSAGE, new HashMap());
+        latch2.await(10, TimeUnit.SECONDS);
+        assertEquals(RESULT, message2.get());
     }
 
 }
+
