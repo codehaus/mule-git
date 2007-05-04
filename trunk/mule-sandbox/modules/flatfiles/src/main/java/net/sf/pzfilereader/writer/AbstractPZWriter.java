40dc8e42b8c161a6d565697348e2437bc89d7b16
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2007-05-04 16:31:41 +0000

    adding a write(char[]) for completeness
    git-svn-id: file:///home/projects/mule/scm@6326 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule-sandbox/modules/flatfiles/src/main/java/net/sf/pzfilereader/writer/AbstractPZWriter.java
+++ trunk/mule-sandbox/modules/flatfiles/src/main/java/net/sf/pzfilereader/writer/AbstractPZWriter.java
@@ -30,26 +30,28 @@ public abstract class AbstractPZWriter extends Object implements PZWriter
         super();
         writer = new BufferedWriter(new OutputStreamWriter(output));
     }
-    
+
     public void addRecordEntry(String columnName, Object value)
     {
         if (rowMap == null)
         {
             rowMap = new HashMap();
         }
-        
+
         if (this.validateColumnTitle(columnName) == false)
         {
             throw new IllegalArgumentException("unknown column: \"" + columnName + "\"");
         }
         rowMap.put(columnName, value);
     }
-    
+
     /**
-     * Subclasses must implement this method to perform validation of <code>columnTitle</code>.
+     * Subclasses must implement this method to perform validation of
+     * <code>columnTitle</code>.
      * 
      * @param columnTitle title of the column to be filled
-     * @return <code>true</code> if the column title is valid else return <code>false</code>.
+     * @return <code>true</code> if the column title is valid else return
+     *         <code>false</code>.
      */
     protected abstract boolean validateColumnTitle(String columnTitle);
 
@@ -57,16 +59,17 @@ public abstract class AbstractPZWriter extends Object implements PZWriter
      * Writes a newline to the output and discards the <code>rowMap</code>.
      * <p>
      * This method must be overridden by subclasses to write out the record data
-     * stored in <code>rowMap</code>. Overriders <b>must</b> call <code>super.nextRecord()</code>
-     * as the last call in their implementation.
+     * stored in <code>rowMap</code>. Overriders <b>must</b> call
+     * <code>super.nextRecord()</code> as the last call in their implementation.
      */
     public void nextRecord() throws IOException
     {
-        // the row should have been written out by the subclass so it's safe to discard it here
+        // the row should have been written out by the subclass so it's safe to
+        // discard it here
         rowMap = null;
         writer.newLine();
     }
-    
+
     protected void write(Object value) throws IOException
     {
         if (value == null)
@@ -76,12 +79,17 @@ public abstract class AbstractPZWriter extends Object implements PZWriter
         // TODO DO: converter/formatter for converting value to string?
         writer.write(value.toString());
     }
-    
+
     protected void write(char character) throws IOException
     {
         writer.write(character);
     }
-    
+
+    protected void write(char[] characters) throws IOException
+    {
+        writer.write(characters);
+    }
+
     public void flush() throws IOException
     {
         writer.flush();
@@ -92,11 +100,9 @@ public abstract class AbstractPZWriter extends Object implements PZWriter
         writer.flush();
         writer.close();
     }
-    
+
     protected Map getRowMap()
     {
         return rowMap;
     }
 }
-
-
