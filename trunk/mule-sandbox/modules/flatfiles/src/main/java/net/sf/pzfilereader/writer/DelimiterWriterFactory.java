afce803b2e2a1d9878416d97c3a19d75c31ba784
dirk.olmes <dirk.olmes@bf997673-6b11-0410-b953-e057580c5b09>
2007-05-09 10:27:51 +0000

    removed mule license headers as this source will be donated to the pzfilereader project
    git-svn-id: file:///home/projects/mule/scm@6425 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule-sandbox/modules/flatfiles/src/main/java/net/sf/pzfilereader/writer/DelimiterWriterFactory.java
+++ trunk/mule-sandbox/modules/flatfiles/src/main/java/net/sf/pzfilereader/writer/DelimiterWriterFactory.java
@@ -1,13 +1,3 @@
-/*
- * $Id: MappedDelimiterWriterFactory.java 6291 2007-05-03 12:03:50Z dirk.olmes $
- * --------------------------------------------------------------------------------------
- * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
- *
- * The software in this package is published under the terms of the MuleSource MPL
- * license, a copy of which has been included with this distribution in the
- * LICENSE.txt file.
- */
-
 package net.sf.pzfilereader.writer;
 
 import java.io.IOException;
--- trunk/mule-sandbox/modules/flatfiles/src/test/java/net/sf/pzfilereader/writer/AllTests.java
+++ trunk/mule-sandbox/modules/flatfiles/src/test/java/net/sf/pzfilereader/writer/AllTests.java
@@ -1,13 +1,3 @@
-/*
- * $Id$
- * --------------------------------------------------------------------------------------
- * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
- *
- * The software in this package is published under the terms of the MuleSource MPL
- * license, a copy of which has been included with this distribution in the
- * LICENSE.txt file.
- */
-
 package net.sf.pzfilereader.writer;
 
 import junit.framework.Test;
