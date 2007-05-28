951fe84829c126626426e504ea2445209b759973
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2007-05-28 22:15:23 +0000

    MULE-1606 Deadlock on ErrorHandler (ActiveMQ) example shutdown. Reflectively cleaning up connection resource (via a proprietary call) before closing the connection fixes the deadlock.
    git-svn-id: file:///home/projects/mule/scm@6752 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/transports/jms/src/main/java/org/mule/providers/jms/activemq/ActiveMqJmsConnector.java
+++ trunk/mule/transports/jms/src/main/java/org/mule/providers/jms/activemq/ActiveMqJmsConnector.java
@@ -10,17 +10,59 @@
 
 package org.mule.providers.jms.activemq;
 
+import org.mule.providers.ConnectException;
 import org.mule.providers.jms.JmsConnector;
+
+import java.lang.reflect.Method;
+
+import javax.jms.Connection;
 
 /**
  * ActiveMQ 4.x-specific JMS connector.
  */
 public class ActiveMqJmsConnector extends JmsConnector
 {
-    /** Constructs a new ActiveMqJmsConnector. */
-    public ActiveMqJmsConnector ()
+    /**
+     * Constructs a new ActiveMqJmsConnector.
+     */
+    public ActiveMqJmsConnector()
     {
         setEagerConsumer(false);
         // TODO MULE-1409 better support for ActiveMQ 4.x temp destinations
+    }
+
+    /**
+     * Will additionally try to cleanup the ActiveMq connection, otherwise there's a deadlock on shutdown.
+     */
+    protected void doDisconnect() throws ConnectException
+    {
+        try
+        {
+            final Connection conn = getConnection();
+            if (conn != null)
+            {
+                final Class clazz = conn.getClass();
+                final Method cleanupMethod = clazz.getMethod("cleanup", null);
+                if (cleanupMethod != null)
+                {
+                    try
+                    {
+                        cleanupMethod.invoke(conn, null);
+                    }
+                    finally
+                    {
+                        conn.close();
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            throw new ConnectException(e, this);
+        }
+        finally
+        {
+            setConnection(null);
+        }
     }
 }
