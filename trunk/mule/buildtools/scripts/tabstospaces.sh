9ec8b0915fe09770c18bf91c40db77fd37ffa674
tcarlson <tcarlson@bf997673-6b11-0410-b953-e057580c5b09>
2007-06-14 13:26:34 +0000

    Script for converting tabs to spaces
    git-svn-id: file:///home/projects/mule/scm@7103 bf997673-6b11-0410-b953-e057580c5b09

---
+++ trunk/mule/buildtools/scripts/tabstospaces.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Usage: tabstospaces.sh directory"
+
+if [ $# -lt 1 ]
+then
+    echo "Usage: $0 directory"
+    exit 1
+fi
+
+for file in `find $1`
+do
+    if [ -f $file ]
+    then
+        echo "Editing file $file"
+        sed -e 's/\t/    /g' $file > $file.tmp
+        mv -f $file.tmp $file
+    fi
+done
