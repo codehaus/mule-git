589505d43350708a9474a129652843c7e5265ab0
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2007-07-12 20:00:48 +0000

    Reformatted, no code changes.
    git-svn-id: file:///home/projects/mule/scm@7483 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/modules/spring/src/main/java/org/mule/extras/spring/config/MuleBeanDefinitionDocumentReader.java
+++ trunk/mule/modules/spring/src/main/java/org/mule/extras/spring/config/MuleBeanDefinitionDocumentReader.java
@@ -22,22 +22,24 @@ import org.springframework.util.SystemPropertyUtils;
 import org.w3c.dom.Element;
 
 /**
- * By default, an <import resource="file.xml"/> statement will assume the imported resource's location is relative 
+ * By default, an <import resource="file.xml"/> statement will assume the imported resource's location is relative
  * to the current file.  Override to try loading the resource from the classpath as well.
  */
 public class MuleBeanDefinitionDocumentReader extends DefaultBeanDefinitionDocumentReader
 {
     /**
-     * By default, an <import resource="file.xml"/> statement will assume the imported resource's location is relative 
+     * By default, an <import resource="file.xml"/> statement will assume the imported resource's location is relative
      * to the current file.  Override this method to try loading the resource from the classpath as well.
-     * 
-     * Note: only the section labelled "Customized for Mule" below has been changed, the rest is copy-pasted from the 
+     * <p/>
+     * Note: only the section labelled "Customized for Mule" below has been changed, the rest is copy-pasted from the
      * parent (Spring) class.
      */
     //@Override
-    protected void importBeanDefinitionResource(Element ele) {
+    protected void importBeanDefinitionResource(Element ele)
+    {
         String location = ele.getAttribute(RESOURCE_ATTRIBUTE);
-        if (!StringUtils.hasText(location)) {
+        if (!StringUtils.hasText(location))
+        {
             getReaderContext().error("Resource location must not be empty", ele);
             return;
         }
@@ -45,49 +47,60 @@ public class MuleBeanDefinitionDocumentReader extends DefaultBeanDefinitionDocum
         // Resolve system properties: e.g. "${user.dir}"
         location = SystemPropertyUtils.resolvePlaceholders(location);
 
-        if (ResourcePatternUtils.isUrl(location)) {
-            try {
+        if (ResourcePatternUtils.isUrl(location))
+        {
+            try
+            {
                 int importCount = getReaderContext().getReader().loadBeanDefinitions(location);
-                if (logger.isDebugEnabled()) {
+                if (logger.isDebugEnabled())
+                {
                     logger.debug("Imported " + importCount + " bean definitions from URL location [" + location + "]");
                 }
             }
-            catch (BeanDefinitionStoreException ex) {
+            catch (BeanDefinitionStoreException ex)
+            {
                 getReaderContext().error(
                         "Failed to import bean definitions from URL location [" + location + "]", ele, ex);
             }
         }
-        else {
+        else
+        {
             // No URL -> considering resource location as relative to the current file.
-            try {
+            try
+            {
                 Resource relativeResource = getReaderContext().getResource().createRelative(location);
                 int importCount = getReaderContext().getReader().loadBeanDefinitions(relativeResource);
-                if (logger.isDebugEnabled()) {
+                if (logger.isDebugEnabled())
+                {
                     logger.debug("Imported " + importCount + " bean definitions from relative location [" + location + "]");
                 }
             }
             ////////////////////////////////////////////////////////////////////////////////////////////
             // Customized for Mule
             ////////////////////////////////////////////////////////////////////////////////////////////
-            catch (IOException ex) 
+            catch (IOException ex)
             {
-                if (logger.isDebugEnabled()) {
+                if (logger.isDebugEnabled())
+                {
                     logger.debug("Invalid relative resource location [" + location + "] to import bean definitions from, will try loading from classpath");
                 }
                 Resource classpathResource = new ClassPathResource(location);
                 int importCount = getReaderContext().getReader().loadBeanDefinitions(classpathResource);
-                if (logger.isDebugEnabled()) {
+                if (logger.isDebugEnabled())
+                {
                     logger.debug("Imported " + importCount + " bean definitions from classpath resource [" + location + "]");
                 }
             }
-            catch (BeanDefinitionStoreException ex) 
+            catch (BeanDefinitionStoreException ex)
             {
-                if (logger.isDebugEnabled()) {
+                if (logger.isDebugEnabled())
+                {
                     logger.debug("Failed to import bean definitions from relative location [" + location + "], will try loading from classpath");
                 }
                 Resource classpathResource = new ClassPathResource(location);
                 int importCount = getReaderContext().getReader().loadBeanDefinitions(classpathResource);
-                if (logger.isDebugEnabled()) {
+                if (logger.isDebugEnabled())
+                {
                     logger.debug("Imported " + importCount + " bean definitions from classpath resource [" + location + "]");
                 }
             }
