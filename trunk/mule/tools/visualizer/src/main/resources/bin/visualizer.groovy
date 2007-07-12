631728f66148c6783e632c5f389050bb3d185167
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2007-07-12 17:30:22 +0000

    This is now working :) Some TODOs to improve usability, but functionally already ok.
    git-svn-id: file:///home/projects/mule/scm@7471 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/tools/visualizer/src/main/resources/bin/visualizer.groovy
+++ trunk/mule/tools/visualizer/src/main/resources/bin/visualizer.groovy
@@ -1,27 +1,52 @@
 import org.mule.util.SystemUtils
 import org.mule.tools.visualizer.MuleVisualizer
+import org.apache.commons.cli.Options
+import org.apache.commons.cli.BasicParser
+import org.apache.commons.cli.CommandLine
 
-assert args.size() : "No cmd parameters passed in"
+// TODO Add a nice usage message, try to reuse cli options setup with descriptions
+// TODO Add verbose/debug output option, supress Velocity output by default
 
-def output = 'visualizer-output'
+// setup allowed cmd options
+def o = new Options()
+// true for hasArgs
+o.addOption('files', true, 'A comma-separated list of config files to visualize')
+o.addOption('exec', true, 'Full path to DOT executable')
+o.addOption('outputdir', true, 'Visualizer output directory (can add to existing documentation)')
 
-def nativeLibPath = '../lib/native/visualizer'
-def exec = SystemUtils.IS_OS_WINDOWS ? "$nativeLibPath/dot.exe" : "$nativeLibPath/dot"
+CommandLine cmdLine =  new BasicParser().parse(o, args, true)
 
-// for unix flavor check local path and fallback to a globally installed package
-if (SystemUtils.IS_OS_UNIX) {
-    if (!new File(exec).exists()) {
-        // check global path
-        exec = 'which dot'.execute().getText()
-    }
+// files is mandatory
+if (!cmdLine.hasOption('files')) {
+    println '\n\nMandatory parameter missing: files\n\n'
+    System.exit(-1)
+}
+def files = cmdLine.getOptionValue('files')
+
+// outputdir with fallback to the defaults
+def outputdir = cmdLine.getOptionValue('outputdir','visualizer-output')
+
+// dot executable
+def exec = '../lib/native/visualizer/dot'
+if (SystemUtils.IS_OS_WINDOWS) {
+    // need a real file name for windows
+    exec += '.exe'
 }
 
-// add more cmd parameters
-def newArgs = args.toList()
-newArgs << '-exec' << exec
+// for unix flavor check local path and fallback to a globally installed package if none found
+if (SystemUtils.IS_OS_UNIX && !new File(exec).exists()) {
+    def Process process = 'which dot'.execute()
+    def out = process.text
+    def err = process.err.text
+    if (err.contains('Command not found')) {
+        println '\n\nGraph Visualization package not found, please see http://www.graphviz.org\n\n'
+        System.exit(-1)
+    }
+    exec = out // found in global path
+}
 
-newArgs << '-outputdir'
-newArgs << output 
+// construct new (possibly modified) args
+def newArgs = []
+newArgs << '-exec' << exec << '-files' << files << '-outputdir' << outputdir
 
 MuleVisualizer.main(newArgs as String[])
-
