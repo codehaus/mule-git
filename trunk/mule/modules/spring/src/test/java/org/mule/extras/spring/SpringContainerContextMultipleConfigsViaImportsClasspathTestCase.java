090c0a5f55355e2a4533ffb7049902786fa41a18
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2007-07-12 19:55:36 +0000

    Fixed JavaDocs.
    git-svn-id: file:///home/projects/mule/scm@7481 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/modules/spring/src/test/java/org/mule/extras/spring/SpringContainerContextMultipleConfigsViaImportsClasspathTestCase.java
+++ trunk/mule/modules/spring/src/test/java/org/mule/extras/spring/SpringContainerContextMultipleConfigsViaImportsClasspathTestCase.java
@@ -13,7 +13,8 @@ package org.mule.extras.spring;
 
 
 /**
- * Tests the Spring container where more than one config file are loaded via <import resource="classpath:config.xml"/>
+ * Tests the Spring container where more than one config file are loaded via
+ * <code>&lt;import resource="classpath:config.xml"/&gt;</code>
  * "classpath:" is a Spring pseudo-URL which clarifies that the resource is to be loaded from the classpath, not the file system. 
  */
 public class SpringContainerContextMultipleConfigsViaImportsClasspathTestCase extends SpringContainerContextMultipleConfigsViaImportsTestCase
