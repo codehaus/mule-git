e5b5df46e86f403c19a6c986b070a34bc2c56848
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2007-07-12 19:53:35 +0000

    This test is also broken.
    git-svn-id: file:///home/projects/mule/scm@7480 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/modules/spring/src/test/java/org/mule/extras/spring/config/MuleConfigOnFileSystemWithSpringImportsTestCase.java
+++ trunk/mule/modules/spring/src/test/java/org/mule/extras/spring/config/MuleConfigOnFileSystemWithSpringImportsTestCase.java
@@ -13,13 +13,14 @@ package org.mule.extras.spring.config;
 
 /**
  * Tests a Mule config from the file system (i.e., not from the classpath) which loads Spring configs 
- * via <import resource="file.xml"/> statements.  The Spring configs will be loaded from the same directory
+ * via <code>&lt;import resource="file.xml"/&gt;</code> statements.  The Spring configs will be loaded from the same directory
  * as the Mule config (Spring uses relative path by default).
  */
 public class MuleConfigOnFileSystemWithSpringImportsTestCase extends MuleConfigWithSpringImportsTestCase
 {
     public String getConfigResources()
     {
+        // TODO TC this is guaranteed to fail, when e.g. running in a reactor build fromt the root
         return "./src/test/resources/mule-config-with-imports.xml";
     }
 }
