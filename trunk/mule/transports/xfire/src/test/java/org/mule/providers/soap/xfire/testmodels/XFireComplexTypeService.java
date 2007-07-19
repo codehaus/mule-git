1b770d48d213e1cba67d01c42be9a82a461065e3
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2007-07-19 01:24:58 +0000

    Finished renaming.
    git-svn-id: file:///home/projects/mule/scm@7581 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/transports/xfire/src/test/java/org/mule/providers/soap/xfire/testmodels/XFireComplexTypeService.java
+++ trunk/mule/transports/xfire/src/test/java/org/mule/providers/soap/xfire/testmodels/XFireComplexTypeService.java
@@ -2,7 +2,7 @@ package org.mule.providers.soap.xfire.testmodels;
 
 import org.mule.tck.testmodels.services.Person;
 
-public class XfireComplexTypeService
+public class XFireComplexTypeService
 {
 
     public boolean addPersonWithConfirmation(Person person)
