c27d49a4e5c050152cfb493cde55f41d2862a985
acooke <acooke@bf997673-6b11-0410-b953-e057580c5b09>
2007-07-23 21:51:16 +0000

    This is a bit baroque, but gives vaguely seful results.  Typically, everything just works.  With too small a wait period some messages were missed (presumably at the start), which were caught by the "clean up" output.  With a longer pause "clean up" goes to 0 but still, sometimes, the test fails for quite a few iterations (ie until hitting batch sizes of anywhere between 13 and 250).  I have no idea why.
    git-svn-id: file:///home/projects/mule/scm@7630 bf997673-6b11-0410-b953-e057580c5b09

--- branches/trunk-fix-for-MULE-1564/transports/udp/src/test/java/org/mule/providers/udp/functional/UdpConnectorFunctionalTestCase.java
+++ branches/trunk-fix-for-MULE-1564/transports/udp/src/test/java/org/mule/providers/udp/functional/UdpConnectorFunctionalTestCase.java
@@ -10,63 +10,122 @@
 
 package org.mule.providers.udp.functional;
 
-import org.mule.extras.client.MuleClient;
 import org.mule.tck.FunctionalTestCase;
+import org.mule.extras.client.MuleClient;
 import org.mule.umo.UMOMessage;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.HashSet;
 
 public class UdpConnectorFunctionalTestCase extends FunctionalTestCase
 {
 
     public static final String MESSAGE = "hello";
-
+    public static final int TOTAL_MESSAGE_COUNT = 1000;
+    public static final int MAX_NUMBER_OF_BATCHES = 128;
+    public static final long MAX_PAUSE_PERIOD = 2000;
+    public static final long MIN_PAUSE_PERIOD = 10;
+    public static final long BETWEEN_BATCH_PAUSE = 5000;
 
     protected String getConfigResources()
     {
         return "udp-functional-test.xml";
     }
 
-    public void testSendTestData() throws Exception
+    /**
+     * We try progressively smaller batches to see if there are issues with internal
+     * buffers.  If we don't get 100% success eventually, we fail.
+     */
+    public void testMany() throws Exception
     {
-        // no idea what number to use here.
-        // i have seen as low as 97%
-        double okPercentage = 90.0; // UDP is unreliable...
-        final int numberOfMessages = 10000;
+        int numberOfBatches = 0;
+        boolean ok = false;
+        while (!ok && numberOfBatches < MAX_NUMBER_OF_BATCHES)
+        {
+            numberOfBatches = 0 == numberOfBatches ? 1 : numberOfBatches * 2;
+            ok = doTestSome(TOTAL_MESSAGE_COUNT, MAX_PAUSE_PERIOD, TOTAL_MESSAGE_COUNT / numberOfBatches);
+            if (!ok)
+            {
+                logger.warn("UDP failed to send " + TOTAL_MESSAGE_COUNT + " messages in " + numberOfBatches + " batches");
+
+                // clean out the system
+                try
+                {
+                    synchronized(this)
+                    {
+                        this.wait(BETWEEN_BATCH_PAUSE);
+                    }
+                }
+                catch (InterruptedException e)
+                {
+                    // ignore
+                }
+                MuleClient client = new MuleClient();
+                int dropped = 0;
+                while (null != client.receive("vm://foo", MAX_PAUSE_PERIOD))
+                {
+                    // discard old messages
+                    dropped++;
+                }
+                logger.info("Cleaned out " + dropped + " messages");
+            }
+        }
+
+        if (!ok)
+        {
+            fail("Couldn't get UDP to 100% with a batch size of " + TOTAL_MESSAGE_COUNT / numberOfBatches);
+        }
+        else
+        {
+            logger.info("Required " + numberOfBatches + " batches before UDP 100% OK ("
+                    + TOTAL_MESSAGE_COUNT + " messages)");
+        }
+    }
+
+    /**
+     * @param numberOfMessages Total number of tests
+     * @param period Wait period collecting messages
+     * @param burst Number of mesages to send between wait periods
+     * @return true if all messages received
+     * @throws Exception
+     */
+    protected boolean doTestSome(int numberOfMessages, long period, int burst) throws Exception
+    {
+        logger.info("Trying " + numberOfMessages + " messages in batches of " + burst);
         MuleClient client = new MuleClient();
 
+        int burstCount = 0;
+        Set receivedMessages = new HashSet(numberOfMessages);
         for (int sentPackets = 0; sentPackets < numberOfMessages; sentPackets++)
         {
+            burstCount++;
             String msg = MESSAGE + sentPackets;
             client.dispatch("serverEndpoint", msg, null);
-        }
 
-        Set receivedMessages = new HashSet(numberOfMessages);
-
-        for (int i = 0; i < numberOfMessages; i++)
-        {
-            UMOMessage message = client.receive("vm://foo", 2000);
-            if (null != message)
+            if (burst == burstCount || sentPackets == numberOfMessages-1)
             {
-                receivedMessages.add(message.getPayloadAsString());
+                long pause = period;
+                for (int i = 0; i < burstCount; i++)
+                {
+                    UMOMessage message = client.receive("vm://foo", pause);
+                    // reduce waiting time once we have a bunch of messages coming in
+                    // (without this, we can end up waiting for very long times....)
+                    pause = Math.max(MIN_PAUSE_PERIOD, pause / 2);
+                    if (null != message)
+                    {
+                        receivedMessages.add(message.getPayloadAsString());
+                    }
+                }
+                burstCount = 0;
             }
         }
-        assertTrue("Received only " + receivedMessages.size() + " messages",
-                receivedMessages.size() > numberOfMessages * okPercentage / 100.0 );
 
-        int errCount = 0;
-        for (int i = 0; i < numberOfMessages; i++)
+        boolean ok = receivedMessages.size() == numberOfMessages;
+        if (!ok)
         {
-            String message = MESSAGE + i + " Received";
-            if (! receivedMessages.contains(message))
-            {
-                logger.warn("Message " + i + " dropped");
-                errCount++;
-            }
+            logger.info("Received " + receivedMessages.size() + " messages");
         }
-        assertTrue("Dropped " + errCount + " messages",
-                errCount < numberOfMessages * (1.0 - (okPercentage / 100.0 )));
-
+        return ok;
     }
+
 }
