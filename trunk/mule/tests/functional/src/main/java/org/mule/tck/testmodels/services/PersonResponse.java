c544319530b4c6851d0e41d6a757bfd171f11cdb
marie.rizzo <marie.rizzo@bf997673-6b11-0410-b953-e057580c5b09>
2007-07-25 08:47:57 +0000

    This is one of my commit blunders of last week. It was missing the header.
    git-svn-id: file:///home/projects/mule/scm@7645 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/tests/functional/src/main/java/org/mule/tck/testmodels/services/PersonResponse.java
+++ trunk/mule/tests/functional/src/main/java/org/mule/tck/testmodels/services/PersonResponse.java
@@ -1,6 +1,17 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.tck.testmodels.services;
 
-public class PersonResponse {
+public class PersonResponse
+{
     private Person person;
     private long time;
 
@@ -9,24 +20,29 @@ public class PersonResponse {
         // empty constructor
     }
 
-    public PersonResponse(Person person){
+    public PersonResponse(Person person)
+    {
         this.person = person;
         this.time = System.currentTimeMillis();
     }
 
-    public Person getPerson() {
+    public Person getPerson()
+    {
         return person;
     }
 
-    public void setPerson(Person person) {
+    public void setPerson(Person person)
+    {
         this.person = person;
     }
 
-    public long getTime() {
+    public long getTime()
+    {
         return time;
     }
 
-    public void setTime(long time) {
+    public void setTime(long time)
+    {
         this.time = time;
     }
 }
