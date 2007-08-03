460ec68ac127cf91461a9967d605cba01aced06a
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2007-08-03 16:06:17 +0000

    Apply a possible fix for MULE-1937 (fix 1-second polling for JMS XA), the same change as for VM (MULE-1688)
    git-svn-id: file:///home/projects/mule/scm@7743 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/transports/jms/src/main/java/org/mule/providers/jms/XaTransactedJmsMessageReceiver.java
+++ trunk/mule/transports/jms/src/main/java/org/mule/providers/jms/XaTransactedJmsMessageReceiver.java
@@ -22,8 +22,8 @@ import org.mule.umo.endpoint.UMOEndpoint;
 import org.mule.umo.lifecycle.InitialisationException;
 import org.mule.umo.provider.UMOConnector;
 import org.mule.umo.provider.UMOMessageAdapter;
-import org.mule.util.MapUtils;
 import org.mule.util.ClassUtils;
+import org.mule.util.MapUtils;
 
 import java.util.List;
 
@@ -33,8 +33,13 @@ import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.Session;
 
+import edu.emory.mathcs.backport.java.util.concurrent.TimeUnit;
+
 public class XaTransactedJmsMessageReceiver extends TransactedPollingMessageReceiver
 {
+    public static final long DEFAULT_VM_POLL_FREQUENCY = 1;
+    public static final TimeUnit DEFAULT_VM_POLL_TIMEUNIT = TimeUnit.NANOSECONDS;
+    
     protected final JmsConnector connector;
     protected boolean reuseConsumer;
     protected boolean reuseSession;
@@ -70,9 +75,11 @@ public class XaTransactedJmsMessageReceiver extends TransactedPollingMessageRece
     public XaTransactedJmsMessageReceiver(UMOConnector umoConnector, UMOComponent component, UMOEndpoint endpoint)
         throws InitialisationException
     {
+        super(umoConnector, component, endpoint);
         // TODO AP: find appropriate value for polling frequency with the scheduler;
         // see setFrequency/setTimeUnit & VMMessageReceiver for more
-        super(umoConnector, component, endpoint);
+        this.setFrequency(DEFAULT_VM_POLL_FREQUENCY);
+        this.setTimeUnit(DEFAULT_VM_POLL_TIMEUNIT);
         this.connector = (JmsConnector) umoConnector;
         this.timeout = endpoint.getTransactionConfig().getTimeout();
 
@@ -122,12 +129,11 @@ public class XaTransactedJmsMessageReceiver extends TransactedPollingMessageRece
     {
         if (connector.isConnected() && connector.isEagerConsumer())
         {
-            // TODO Fix Bug
+            createConsumer();
             // creating this consumer now would prevent from the actual worker
             // consumer
             // to receive the message!
             //Antoine Borg 08 Dec 2006 - Uncommented for MULE-1150
-            createConsumer();
             // if we comment this line, if one tries to restart the service through
             // JMX,
             // this will fail...
