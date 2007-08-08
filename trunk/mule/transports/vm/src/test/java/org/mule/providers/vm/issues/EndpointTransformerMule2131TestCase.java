832bb46e8929ab428be3d86f037448e650b98626
acooke <acooke@bf997673-6b11-0410-b953-e057580c5b09>
2007-08-08 21:02:04 +0000

    MULE-2131: Similar behaviour to 2.0 (DO NOT MERGE WITH 2.0!)
    git-svn-id: file:///home/projects/mule/scm@7807 bf997673-6b11-0410-b953-e057580c5b09

---
+++ trunk/mule/transports/vm/src/test/java/org/mule/providers/vm/issues/EndpointTransformerMule2131TestCase.java
@@ -0,0 +1,77 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.providers.vm.issues;
+
+import org.mule.tck.FunctionalTestCase;
+import org.mule.extras.client.MuleClient;
+import org.mule.umo.UMOException;
+import org.mule.umo.UMOMessage;
+
+public class EndpointTransformerMule2131TestCase extends FunctionalTestCase
+{
+
+    public static final long TIMEOUT = 1000L;
+    public static final String MESSAGE = "a message";
+
+    protected String getConfigResources()
+    {
+        return "endpoint-transformer-mule-2131-test.xml";
+    }
+
+    protected MuleClient send() throws UMOException
+    {
+        MuleClient client = new MuleClient();
+        client.dispatch("in", MESSAGE, null);
+        return client;
+    }
+
+    public void testDirect() throws Exception
+    {
+        String response = receive(send(), "direct");
+        assertEquals(MESSAGE, response);
+    }
+
+    public void testGlobalNameGlobalTransformer() throws Exception
+    {
+        doTestTransformed("global-name-global-transformer");
+    }
+
+    public void testGlobalNameLocalTransformer() throws Exception
+    {
+        doTestTransformed("global-name-local-transformer");
+    }
+
+    public void testNoNameLocalTransformer() throws Exception
+    {
+        doTestTransformed("vm://no-name-local-transformer?connector=queue");
+    }
+
+    // not possible before 2.0?
+//    public void testLocalNameLocalTransformer() throws Exception
+//    {
+//        doTestTransformed("local-name-local-transformer");
+//    }
+
+    protected void doTestTransformed(String endpoint) throws Exception
+    {
+        String response = receive(send(), endpoint);
+        assertEquals(MESSAGE + StringAppendTransformer.TRANSFORMED, response);
+    }
+
+    protected String receive(MuleClient client, String endpoint) throws Exception
+    {
+        UMOMessage message = client.receive(endpoint, TIMEOUT);
+        assertNotNull(message);
+        assertNotNull(message.getPayloadAsString());
+        return message.getPayloadAsString();
+    }
+
+}
