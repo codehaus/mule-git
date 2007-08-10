cc0d35327c94ca1d589d9e14fc05e4023ac5abd1
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2007-08-10 19:13:19 +0000

    Activate scheduler/polling thread decoupling & reset scheduling times to sane values. AP, please give XA-JMS a workout.
    git-svn-id: file:///home/projects/mule/scm@7840 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/core/src/main/java/org/mule/providers/AbstractPollingMessageReceiver.java
+++ trunk/mule/core/src/main/java/org/mule/providers/AbstractPollingMessageReceiver.java
@@ -22,8 +22,6 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-import javax.resource.spi.work.Work;
-
 import edu.emory.mathcs.backport.java.util.concurrent.ScheduledFuture;
 import edu.emory.mathcs.backport.java.util.concurrent.TimeUnit;
 
@@ -63,8 +61,9 @@ public abstract class AbstractPollingMessageReceiver extends AbstractMessageRece
                 // polling takes longer than the specified frequency, e.g. when the
                 // polled database or network is slow or returns large amounts of
                 // data.
-                ScheduledFuture schedule = connector.getScheduler().scheduleWithFixedDelay(this.createWork(),
-                    DEFAULT_STARTUP_DELAY, this.getFrequency(), this.getTimeUnit());
+                ScheduledFuture schedule = connector.getScheduler().scheduleWithFixedDelay(
+                    new PollingReceiverWorkerSchedule(this.createWork()), DEFAULT_STARTUP_DELAY,
+                    this.getFrequency(), this.getTimeUnit());
                 schedules.add(schedule);
 
                 if (logger.isDebugEnabled())
@@ -103,7 +102,7 @@ public abstract class AbstractPollingMessageReceiver extends AbstractMessageRece
         }
     }
 
-    protected Work createWork()
+    protected PollingReceiverWorker createWork()
     {
         return new PollingReceiverWorker(this);
     }
