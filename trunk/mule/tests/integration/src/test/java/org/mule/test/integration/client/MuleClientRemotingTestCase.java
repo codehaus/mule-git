966229dad7e48249a5e53e551599151130dce6e8
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2007-08-16 12:36:26 +0000

    One of these tests actually works fine when fixed.
    git-svn-id: file:///home/projects/mule/scm@7898 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/tests/integration/src/test/java/org/mule/test/integration/client/MuleClientRemotingTestCase.java
+++ trunk/mule/tests/integration/src/test/java/org/mule/test/integration/client/MuleClientRemotingTestCase.java
@@ -29,18 +29,18 @@ public class MuleClientRemotingTestCase extends FunctionalTestCase
         return "tcp://localhost:60504";
     }
 
-    // public void testClientSendToRemoteComponent() throws Exception
-    // {
-    // // Will connect to the server using tcp://localhost:60504
-    // MuleClient client = new MuleClient();
-    // MuleManager.getConfiguration().setSynchronous(true);
-    //
-    // RemoteDispatcher dispatcher = client.getRemoteDispatcher(getServerUrl());
-    // UMOMessage message = dispatcher.sendToRemoteComponent("TestReceiverUMO", "Test
-    // Client Send message", null);
-    // assertNotNull(message);
-    // assertEquals("Received: Test Client Send message", message.getPayload());
-    // }
+    public void testClientSendToRemoteComponent() throws Exception
+    {
+        // Will connect to the server using tcp://localhost:60504
+        MuleClient client = new MuleClient();
+        MuleManager.getConfiguration().setSynchronous(true);
+
+        RemoteDispatcher dispatcher = client.getRemoteDispatcher(getServerUrl());
+        UMOMessage message = dispatcher.sendToRemoteComponent("TestReceiverUMO", "Test Client Send message",
+            null);
+        assertNotNull(message);
+        assertEquals("Test Client Send message Received", message.getPayload());
+    }
 
     public void testClientRequestResponseOnEndpoint() throws Exception
     {
@@ -55,24 +55,29 @@ public class MuleClientRemotingTestCase extends FunctionalTestCase
         assertEquals("received from remote", message.getPayloadAsString());
     }
 
-    // public void testClientSendAndReceiveRemote() throws Exception
-    // {
-    // String remoteEndpoint = "vm://remote.queue";
-    // // Will connect to the server using tcp://localhost:60504
-    // MuleClient client = new MuleClient();
-    // MuleManager.getConfiguration().setSynchronous(true);
-    //
-    // RemoteDispatcher dispatcher = client.getRemoteDispatcher(getServerUrl());
-    // UMOMessage message = dispatcher.receiveRemote(remoteEndpoint, 1000);
-    // assertNull(message);
-    //
-    // // Dispatch a message
-    // dispatcher.dispatchRemote(remoteEndpoint, "Test Remote Message 2", null);
-    //
-    // // Receive the message
-    // message = dispatcher.receiveRemote(remoteEndpoint, 2000);
-    // assertNotNull(message);
-    // assertEquals("Test Remote Message 2", message.getPayload());
-    //
-    // }
+    /*
+     * This test exposes at least two problems:
+     * - the VM connector must be specified explicitly; this is intended
+     * - when a VM connector is specified, Mule tries to serialize the VM connector?!
+     */
+    public void _testClientSendAndReceiveRemote() throws Exception
+    {
+        String remoteEndpoint = "vm://remote.queue";
+        // Will connect to the server using tcp://localhost:60504
+        MuleClient client = new MuleClient();
+        MuleManager.getConfiguration().setSynchronous(true);
+
+        RemoteDispatcher dispatcher = client.getRemoteDispatcher(getServerUrl());
+        UMOMessage message = dispatcher.receiveRemote(remoteEndpoint, 1000);
+        assertNull(message);
+
+        // Dispatch a message
+        dispatcher.dispatchRemote(remoteEndpoint, "Test Remote Message 2", null);
+
+        // Receive the message
+        message = dispatcher.receiveRemote(remoteEndpoint, 2000);
+        assertNotNull(message);
+        assertEquals("Test Remote Message 2", message.getPayload());
+    }
+
 }
