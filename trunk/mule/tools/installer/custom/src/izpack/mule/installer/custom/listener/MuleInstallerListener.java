6969a8a0b2433dda3947ecb022c47d3350027143
dirk.olmes <dirk.olmes@bf997673-6b11-0410-b953-e057580c5b09>
2007-08-16 10:33:16 +0000

    git-svn-id: file:///home/projects/mule/scm@7892 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/tools/installer/custom/src/izpack/mule/installer/custom/listener/MuleInstallerListener.java
+++ trunk/mule/tools/installer/custom/src/izpack/mule/installer/custom/listener/MuleInstallerListener.java
@@ -20,7 +20,6 @@ import java.util.List;
 
 public class MuleInstallerListener extends SimpleInstallerListener
 {
-
     /**
      * Default constructor
      */
@@ -32,37 +31,47 @@ public class MuleInstallerListener extends SimpleInstallerListener
     public void beforePacks(AutomatedInstallData idata, Integer npacks, AbstractUIProgressHandler handler) throws Exception
     {
         List availablePacks = idata.availablePacks;
-        for (int i = 0; i < availablePacks.size(); i++){
-            String packagePath = "/"+ ((Pack)availablePacks.get(i)).name;
-            File dir = new File(idata.getInstallPath()+packagePath);
-            if (dir.exists()){
+        for (int i = 0; i < availablePacks.size(); i++)
+        {
+            String packagePath = "/" + ((Pack)availablePacks.get(i)).name;
+            File dir = new File(idata.getInstallPath() + packagePath);
+            if (dir.exists())
+            {
                 deleteDir(dir);
             }
         }
     }
 
-    public void afterPacks(AutomatedInstallData idata, AbstractUIProgressHandler handler) throws Exception{
-        if (!idata.installSuccess){
+    public void afterPacks(AutomatedInstallData idata, AbstractUIProgressHandler handler) throws Exception
+    {
+        if (!idata.installSuccess)
+        {
             List availablePacks = idata.availablePacks;
-            for (int i = 0; i < availablePacks.size(); i++){
-                String packagePath = "/"+ ((Pack)availablePacks.get(i)).name;
-                File dir = new File(idata.getInstallPath()+packagePath);
-                if (dir.exists()){
+            for (int i = 0; i < availablePacks.size(); i++)
+            {
+                String packagePath = "/" + ((Pack)availablePacks.get(i)).name;
+                File dir = new File(idata.getInstallPath() + packagePath);
+                if (dir.exists())
+                {
                     deleteDir(dir);
                 }
             }
         }
     }
 
-    //  Deletes all files and subdirectories under dir.
+    // Deletes all files and subdirectories under dir.
     // Returns true if all deletions were successful.
     // If a deletion fails, the method stops attempting to delete and returns false.
-    public static boolean deleteDir(File dir) {
-        if (dir.isDirectory()) {
+    public static boolean deleteDir(File dir)
+    {
+        if (dir.isDirectory())
+        {
             String[] children = dir.list();
-            for (int i=0; i<children.length; i++) {
+            for (int i = 0; i < children.length; i++)
+            {
                 boolean success = deleteDir(new File(dir, children[i]));
-                if (!success) {
+                if (!success)
+                {
                     return false;
                 }
             }
