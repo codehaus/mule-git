754c9271f239551e583c3afb1eb484c3d4633940
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2007-08-20 21:21:44 +0000

    Tabs -> spaces. No code changes.
    git-svn-id: file:///home/projects/mule/scm@7953 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/transports/http/src/test/java/org/mule/providers/http/functional/HttpContentLengthPropagationTestCase.java
+++ trunk/mule/transports/http/src/test/java/org/mule/providers/http/functional/HttpContentLengthPropagationTestCase.java
@@ -12,24 +12,24 @@ import java.io.IOException;
 
 public class HttpContentLengthPropagationTestCase extends FunctionalTestCase
 {
-	private static final String payload_path = "target/test-classes/test-xml-payload.xml";
-	private static final String stylesheet_path = "target/test-classes/stylesheet.xsl";
-	private byte[] fileContents;
-	
-	public HttpContentLengthPropagationTestCase()
-	{
-	    super();
-	    this.setDisposeManagerPerSuite(true);
-	}
+    private static final String payload_path = "target/test-classes/test-xml-payload.xml";
+    private static final String stylesheet_path = "target/test-classes/stylesheet.xsl";
+    private byte[] fileContents;
 
-	protected String getConfigResources() 
-	{
-		return "http-content-length-propagation-conf.xml";
-	}
-	
-	private void readFile(String path) throws IOException
-	{
-	    FileInputStream stream = new FileInputStream(path);
+    public HttpContentLengthPropagationTestCase()
+    {
+        super();
+        this.setDisposeManagerPerSuite(true);
+    }
+
+    protected String getConfigResources()
+    {
+        return "http-content-length-propagation-conf.xml";
+    }
+
+    private void readFile(String path) throws IOException
+    {
+        FileInputStream stream = new FileInputStream(path);
         File file = new File(path);
         fileContents = new byte[(int)file.length()];
         int bytesRead;
@@ -40,25 +40,25 @@ public class HttpContentLengthPropagationTestCase extends FunctionalTestCase
             bytesRead = stream.read(fileContents, totalbytesRead, (int)file.length());
             totalbytesRead += bytesRead;
         }
-	}
-	
-	public byte[] localTransform() throws Exception
-	{
-	    XsltTransformer trans = new XsltTransformer();
-	    trans.setXslFile(stylesheet_path);
+    }
+
+    public byte[] localTransform() throws Exception
+    {
+        XsltTransformer trans = new XsltTransformer();
+        trans.setXslFile(stylesheet_path);
 
         return (byte[])trans.doTransform(fileContents, "UTF-8");
-	}
-	
-	public void testContentLengthPropagation() throws Exception
-	{
-		readFile(payload_path);
-		
-		MuleClient client = new MuleClient();
-		UMOMessage result = client.send("http://localhost:8085", new MuleMessage(fileContents));
-		
-		assertEquals(new String(localTransform()), result.getPayloadAsString());
+    }
 
-	}
+    public void testContentLengthPropagation() throws Exception
+    {
+        readFile(payload_path);
+
+        MuleClient client = new MuleClient();
+        UMOMessage result = client.send("http://localhost:8085", new MuleMessage(fileContents));
+
+        assertEquals(new String(localTransform()), result.getPayloadAsString());
+
+    }
 
 }
