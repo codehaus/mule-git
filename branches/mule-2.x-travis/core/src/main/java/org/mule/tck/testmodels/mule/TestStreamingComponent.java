5cb974532109dc90bd592ff785ca3eb36aaa512f
tcarlson <tcarlson@bf997673-6b11-0410-b953-e057580c5b09>
2007-10-05 19:16:37 +0000

    API changes for MULE-2287, MULE-2137
    git-svn-id: file:///home/projects/mule/scm@8896 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.x-travis/core/src/main/java/org/mule/tck/testmodels/mule/TestStreamingComponent.java
+++ branches/mule-2.x-travis/core/src/main/java/org/mule/tck/testmodels/mule/TestStreamingComponent.java
@@ -3,23 +3,22 @@
  * --------------------------------------------------------------------------------------
  * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
  *
- * The software in this package is published under the terms of the CPAL v1.0
+ * The software in this package is published under the terms of the MuleSource MPL
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
 
 package org.mule.tck.testmodels.mule;
 
-import org.mule.impl.model.seda.SedaComponent;
+import org.mule.impl.model.streaming.StreamingComponent;
 import org.mule.umo.UMOException;
 
-
 /**
- * Exposes some internals of the SedaComponent useful for unit testing.
+ * Exposes some internals of the StreamingComponent useful for unit testing.
  */
-public class TestSedaComponent extends SedaComponent
+public class TestStreamingComponent extends StreamingComponent
 {
-    public TestSedaComponent()
+    public TestStreamingComponent()
     {
         super();
     }
