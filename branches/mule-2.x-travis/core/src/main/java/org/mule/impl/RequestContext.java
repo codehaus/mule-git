57b37d1277a816671f0e3eb7e608bc65d0703452
tcarlson <tcarlson@bf997673-6b11-0410-b953-e057580c5b09>
2007-10-08 13:10:33 +0000

    Merged revisions 8984 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.x
    ........
      r8984 | tcarlson | 2007-10-08 10:07:27 -0300 (Mon, 08 Oct 2007) | 1 line
      
      Turn off misleading stack traces in log file
    ........
    
    
    git-svn-id: file:///home/projects/mule/scm@8985 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.x-travis/core/src/main/java/org/mule/impl/RequestContext.java
+++ branches/mule-2.x-travis/core/src/main/java/org/mule/impl/RequestContext.java
@@ -10,7 +10,6 @@
 
 package org.mule.impl;
 
-import org.mule.config.MuleManifest;
 import org.mule.config.MuleProperties;
 import org.mule.umo.UMOEvent;
 import org.mule.umo.UMOEventContext;
@@ -194,7 +193,7 @@ public final class RequestContext
     {
         if (logger.isDebugEnabled())
         {
-            //logger.debug("copying " + type, new Exception());
+            logger.debug("copying " + type);//, new Exception());
         }
     }
 
