5199fab0c0ddc0c26e35e9bb60b7504a11b90ecb
tcarlson <tcarlson@bf997673-6b11-0410-b953-e057580c5b09>
2007-10-10 21:32:47 +0000

    Disable broken WSDL variant (MULE-2526)
    git-svn-id: file:///home/projects/mule/scm@9057 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.0.0-M2/examples/stockquote/dist/stockquote.bat
+++ branches/mule-2.0.0-M2/examples/stockquote/dist/stockquote.bat
@@ -11,10 +11,9 @@ SET MULE_LIB=.\conf
 
 ECHO (make sure you have configured your HTTP proxy if behind a firewall - see README.txt)
 ECHO.
-ECHO The Stock Quote example is available in three variations:
+ECHO The Stock Quote example is available in two variations:
 ECHO   1. REST
-ECHO   2. WSDL
-ECHO   3. SOAP
+ECHO   2. SOAP
 SET /P Choice=Select the one you wish to execute and press Enter...
 
 REM Use the SpringConfigurationBuilder because the MuleXmlConfigurationBuilder (Commons Digester) has issues 
@@ -23,5 +22,4 @@ REM Note: We can't use it for stockquote-soap-config.xml, because Spring chokes 
 REM   <property name="soapAction" value="${methodNamespace}${method}"/>
 
 IF '%Choice%'=='1' call "%MULE_BASE%\bin\mule.bat" -config "stdio-config.xml,stockquote-rest-config.xml"
-IF '%Choice%'=='2' call "%MULE_BASE%\bin\mule.bat" -config "stdio-config.xml,stockquote-wsdl-config.xml"
-IF '%Choice%'=='3' call "%MULE_BASE%\bin\mule.bat" -config "stdio-config.xml,stockquote-soap-config.xml"
+IF '%Choice%'=='2' call "%MULE_BASE%\bin\mule.bat" -config "stdio-config.xml,stockquote-soap-config.xml"
