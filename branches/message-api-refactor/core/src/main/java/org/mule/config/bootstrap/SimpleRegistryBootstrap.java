11bc8f302956889e4ecf1e58edf8f27e00cf8db2
rossmason <rossmason@bf997673-6b11-0410-b953-e057580c5b09>
2007-10-26 23:42:37 +0000

    updated the javadoc
    git-svn-id: file:///home/projects/mule/scm@9420 bf997673-6b11-0410-b953-e057580c5b09

--- branches/message-api-refactor/core/src/main/java/org/mule/config/bootstrap/SimpleRegistryBootstrap.java
+++ branches/message-api-refactor/core/src/main/java/org/mule/config/bootstrap/SimpleRegistryBootstrap.java
@@ -7,7 +7,7 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.config.bootstrap;
+    package org.mule.config.bootstrap;
 
 import org.mule.impl.ManagementContextAware;
 import org.mule.registry.Registry;
