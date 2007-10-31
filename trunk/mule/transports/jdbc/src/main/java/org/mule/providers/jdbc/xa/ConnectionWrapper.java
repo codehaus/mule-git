a82d448ab7581672a8507c0ddede7172388610ad
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2007-10-31 13:22:03 +0000

    Use a dedicated IllegalTransactionStateException instead of a more generic IllegalStateException.
    git-svn-id: file:///home/projects/mule/scm@9508 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/transports/jdbc/src/main/java/org/mule/providers/jdbc/xa/ConnectionWrapper.java
+++ trunk/mule/transports/jdbc/src/main/java/org/mule/providers/jdbc/xa/ConnectionWrapper.java
@@ -11,6 +11,7 @@
 package org.mule.providers.jdbc.xa;
 
 import org.mule.config.i18n.CoreMessages;
+import org.mule.transaction.IllegalTransactionStateException;
 import org.mule.transaction.TransactionCoordination;
 import org.mule.transaction.XaTransaction;
 import org.mule.umo.UMOTransaction;
@@ -458,11 +459,11 @@ public class ConnectionWrapper implements Connection
         UMOTransaction transaction = TransactionCoordination.getInstance().getTransaction();
         if (transaction == null)
         {
-            throw new IllegalStateException(CoreMessages.noMuleTransactionAvailable().toString());
+            throw new IllegalTransactionStateException(CoreMessages.noMuleTransactionAvailable());
         }
         if (!(transaction instanceof XaTransaction))
         {
-            throw new IllegalStateException(CoreMessages.notMuleXaTransaction(transaction).toString());
+            throw new IllegalTransactionStateException(CoreMessages.notMuleXaTransaction(transaction));
         }
         if (!isEnlisted())
         {
