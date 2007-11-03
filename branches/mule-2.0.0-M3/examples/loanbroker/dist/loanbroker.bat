a9420423e51d97adeb3b51c1d27f4e9e1348a408
tcarlson <tcarlson@bf997673-6b11-0410-b953-e057580c5b09>
2007-11-03 12:35:58 +0000

    Exclude broken examples
    git-svn-id: file:///home/projects/mule/scm@9585 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.0.0-M3/examples/loanbroker/dist/loanbroker.bat
+++ branches/mule-2.0.0-M3/examples/loanbroker/dist/loanbroker.bat
@@ -8,22 +8,9 @@ REM This extends the classpath to include the configuration directory
 REM Any changes to the files in .\conf will take precedence over those deployed to %MULE_HOME%\lib\user
 SET MULE_LIB=.\conf
 
-REM 3rd party libraries for LoanBroker ESB
-SET ACTIVEMQ=activemq-core-4.1.0-incubator.jar
-SET OPENEJB=openejb-core-1.0.jar
 REM 3rd party libraries for LoanBroker BPM
 SET BSH=bsh-1.3.0.jar
 SET DERBYDB=derby-10.3.1.4.jar
-
-:testactivemq
-if exist "%MULE_BASE%\lib\user\%ACTIVEMQ%" goto :testopenejb
-if exist "%MULE_HOME%\lib\user\%ACTIVEMQ%" goto :testopenejb
-goto :missinglibs
-
-:testopenejb
-if exist "%MULE_BASE%\lib\user\%OPENEJB%" goto :testbsh
-if exist "%MULE_HOME%\lib\user\%OPENEJB%" goto :testbsh
-goto :missinglibs
 
 :testbsh
 if exist "%MULE_BASE%\lib\user\%BSH%" goto :testderbydb
@@ -39,12 +26,11 @@ echo This example requires additional libraries which need to be downloaded by t
 goto :eof
 
 :mule
-ECHO The Loan Broker example is available in three flavors:
-ECHO   1. Loan Broker ESB
-ECHO   2. Loan Broker ESN
-ECHO   3. Loan Broker BPM
+ECHO The Loan Broker example is available in two flavors:
+ECHO   1. Loan Broker ESN
+ECHO   2. Loan Broker BPM
 SET /P Choice=Select the one you wish to execute and press Enter...
 
-IF '%Choice%'=='1' call "%MULE_BASE%\bin\mule.bat" -main org.mule.examples.loanbroker.esb.LoanBrokerApp
-IF '%Choice%'=='2' call "%MULE_BASE%\bin\mule.bat" -main org.mule.examples.loanbroker.esn.LoanBrokerApp
-IF '%Choice%'=='3' call "%MULE_BASE%\bin\mule.bat" -main org.mule.examples.loanbroker.bpm.LoanBrokerApp
+IF '%Choice%'=='1' call "%MULE_BASE%\bin\mule.bat" -main org.mule.examples.loanbroker.esn.LoanBrokerApp
+IF '%Choice%'=='2' call "%MULE_BASE%\bin\mule.bat" -main org.mule.examples.loanbroker.bpm.LoanBrokerApp
+
--- branches/mule-2.0.0-M3/examples/stockquote/dist/stockquote.bat
+++ branches/mule-2.0.0-M3/examples/stockquote/dist/stockquote.bat
@@ -11,12 +11,10 @@ SET MULE_LIB=.\conf
 
 ECHO (make sure you have configured your HTTP proxy if behind a firewall - see README.txt)
 ECHO.
-ECHO The Stock Quote example is available in three variations:
+ECHO The Stock Quote example is available in two variations:
 ECHO   1. REST
-ECHO   2. WSDL
-ECHO   3. SOAP
+ECHO   2. SOAP
 SET /P Choice=Select the one you wish to execute and press Enter...
 
 IF '%Choice%'=='1' call "%MULE_BASE%\bin\mule.bat" -config "stdio-config.xml,stockquote-rest-config.xml"
-IF '%Choice%'=='2' call "%MULE_BASE%\bin\mule.bat" -config "stdio-config.xml,stockquote-wsdl-config.xml"
-IF '%Choice%'=='3' call "%MULE_BASE%\bin\mule.bat" -config "stdio-config.xml,stockquote-soap-config.xml"
+IF '%Choice%'=='2' call "%MULE_BASE%\bin\mule.bat" -config "stdio-config.xml,stockquote-soap-config.xml"
