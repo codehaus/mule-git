e11b92e530512f9fabeb480ac2f4528350958e72
aguenther <aguenther@bf997673-6b11-0410-b953-e057580c5b09>
2007-12-03 23:13:14 +0000

    MULE-2635, MULE-2707, MULE-2670 - Re-introduce mule startup using custom main function
    git-svn-id: file:///home/projects/mule/scm@9969 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/modules/boot/src/main/java/org/mule/modules/boot/MuleBootstrap.java
+++ trunk/mule/modules/boot/src/main/java/org/mule/modules/boot/MuleBootstrap.java
@@ -10,8 +10,19 @@
 
 package org.mule.modules.boot;
 
-import java.io.File;
+import org.mule.util.ClassUtils;
 
+import java.io.File;
+import java.net.URL;
+import java.util.Date;
+import java.util.Properties;
+
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.tanukisoftware.wrapper.WrapperListener;
+import org.tanukisoftware.wrapper.WrapperManager;
 import org.tanukisoftware.wrapper.WrapperSimpleApp;
 
 /**
@@ -27,14 +38,49 @@ import org.tanukisoftware.wrapper.WrapperSimpleApp;
  */
 public final class MuleBootstrap
 {
-    public static final String MAIN_CLASS_MULE_SERVER = "org.mule.modules.boot.MuleServerWrapper";
-
+    private static final String MULE_MODULE_BOOT_POM_FILE_PATH = "META-INF/maven/org.mule.modules/mule-module-boot/pom.properties";
+    
+    public static final String MULE_SERVER_WRAPPER = "org.mule.modules.boot.MuleServerWrapper";
+    public static final String MULE_VERSION_WRAPPER = "org.mule.modules.boot.VersionWrapper";
+    
+    public static final String CLI_OPTIONS[][] = {
+        {"main", "true", "Main Class"},
+        {"version", "false", "Show product and version information"}
+    };
+    
     private MuleBootstrap()
     {
         // utility class only
     }
 
     public static void main(String args[]) throws Exception
+    {
+        prepareBootstrapPhase();
+        
+        CommandLine commandLine = parseCommandLine(args);
+        String[] remainingArgs = commandLine.getArgs();
+        
+        String mainClassName = commandLine.getOptionValue("main");
+        if (commandLine.hasOption("version"))
+        {
+            mainClassName = MULE_VERSION_WRAPPER;
+        }
+        else if (mainClassName == null)
+        {
+            mainClassName = MULE_SERVER_WRAPPER;
+        }
+        
+        if (mainClassName.equals(MULE_SERVER_WRAPPER) || mainClassName.equals(MULE_VERSION_WRAPPER))
+        {
+            WrapperManager.start((WrapperListener) Class.forName(mainClassName).newInstance(), remainingArgs);
+        }
+        else
+        {
+            wrapperMain(mainClassName, args);
+        }    
+    }
+    
+    private static void prepareBootstrapPhase() throws Exception
     {
         File muleHome = lookupMuleHome();
         File muleBase = lookupMuleBase();
@@ -46,7 +92,9 @@ public final class MuleBootstrap
 
         MuleBootstrapUtils.addLocalJarFilesToClasspath(muleHome, muleBase);
         MuleBootstrapUtils.addExternalJarFilesToClasspath(muleHome, null);
-        wrapperMain(MAIN_CLASS_MULE_SERVER, args);
+        
+        setSystemMuleVersion();
+        requestLicenseAcceptance();           
     }
     
     private static File lookupMuleHome() throws Exception
@@ -77,12 +125,48 @@ public final class MuleBootstrap
         }
         return muleBase;
     }
+
+    private static void requestLicenseAcceptance() throws Exception
+    {
+        if (!LicenseHandler.isLicenseAccepted() && !LicenseHandler.getAcceptance())
+        {
+            WrapperManager.stop(-1);
+        }        
+    }
     
-    private static void wrapperMain(String mainClass, String[] args) throws Exception
+    private static void setSystemMuleVersion()
+    {
+        try
+        {
+            URL mavenPropertiesUrl = ClassUtils.getResource(MULE_MODULE_BOOT_POM_FILE_PATH, MuleServerWrapper.class);
+            Properties mavenProperties = new Properties();
+            mavenProperties.load(mavenPropertiesUrl.openStream());
+            
+            System.setProperty("mule.version", mavenProperties.getProperty("version"));
+            System.setProperty("mule.reference.version", mavenProperties.getProperty("version") + '-' + (new Date()).getTime());
+        }
+        catch (Exception ignore)
+        {
+            // ignore
+        }
+    }    
+    
+    private static CommandLine parseCommandLine(String[] args) throws ParseException
+    {
+        Options options = new Options();
+        for (int i = 0; i < CLI_OPTIONS.length; i++)
+        {
+            options.addOption(CLI_OPTIONS[i][0], "true".equalsIgnoreCase(CLI_OPTIONS[i][1]), CLI_OPTIONS[i][2]);
+        }
+        return new BasicParser().parse(options, args, true);
+    }
+    
+    private static void wrapperMain(String mainClassName, String[] args) throws Exception
     {
         String[] appArgs = new String[args.length + 1];
-        appArgs[0] = mainClass;
+        appArgs[0] = mainClassName;
         System.arraycopy(args, 0, appArgs, 1, args.length);
+        System.out.println("Starting class " + mainClassName + "...");
         WrapperSimpleApp.main(appArgs);
     }    
 }
--- trunk/mule/modules/boot/src/main/java/org/mule/modules/boot/MuleServerWrapper.java
+++ trunk/mule/modules/boot/src/main/java/org/mule/modules/boot/MuleServerWrapper.java
@@ -11,57 +11,94 @@
 package org.mule.modules.boot;
 
 import org.mule.MuleServer;
-import org.mule.util.ClassUtils;
 
-import java.net.URL;
-import java.util.Date;
-import java.util.Properties;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
+import org.tanukisoftware.wrapper.WrapperListener;
 import org.tanukisoftware.wrapper.WrapperManager;
 
-public final class MuleServerWrapper
+public class MuleServerWrapper implements WrapperListener
 {
-    private static final Log logger = LogFactory.getLog(MuleServerWrapper.class);
-    
-    private static final String MULE_MODULE_BOOT_POM_FILE_PATH = "META-INF/maven/org.mule.modules/mule-module-boot/pom.properties";
-    
-    private MuleServerWrapper()
+    private MuleServer mule;
+
+    /*---------------------------------------------------------------
+     * Constructors
+     *-------------------------------------------------------------*/
+    public MuleServerWrapper()
     {
-        // utility class only
+        super();
     }
 
-    public static void main(String args[]) throws Exception
-    {
-        setSystemMuleVersion();
-        requestLicenseAcceptance();
-        MuleServer.main(args);
-    }
-
-    private static void requestLicenseAcceptance() throws Exception
-    {
-        if (!LicenseHandler.isLicenseAccepted() && !LicenseHandler.getAcceptance())
-        {
-            WrapperManager.stop(-1);
-        }        
-    }
-    
-    private static void setSystemMuleVersion()
+    /*---------------------------------------------------------------
+     * WrapperListener Methods
+     *-------------------------------------------------------------*/
+    /**
+     * The start method is called when the WrapperManager is signaled by the native
+     * wrapper code that it can start its application. This method call is expected
+     * to return, so a new thread should be launched if necessary.
+     * 
+     * @param args List of arguments used to initialize the application.
+     * @return Any error code if the application should exit on completion of the
+     *         start method. If there were no problems then this method should return
+     *         null.
+     */
+    public Integer start(String[] args)
     {
         try
         {
-            URL mavenPropertiesUrl = ClassUtils.getResource(MULE_MODULE_BOOT_POM_FILE_PATH, MuleServerWrapper.class);
-            Properties mavenProperties = new Properties();
-            mavenProperties.load(mavenPropertiesUrl.openStream());
-            
-            System.setProperty("mule.version", mavenProperties.getProperty("version"));
-            System.setProperty("mule.reference.version", mavenProperties.getProperty("version") + '-' + (new Date()).getTime());
+            MuleServer.main(args);
+            return null;
         }
-        catch (Exception ignore)
+        catch (Exception e)
         {
-            logger.debug(ignore);
+            e.printStackTrace();
+            return new Integer(1);
+        }
+    }
+
+    /**
+     * Called when the application is shutting down. The Wrapper assumes that this
+     * method will return fairly quickly. If the shutdown code code could potentially
+     * take a long time, then WrapperManager.signalStopping() should be called to
+     * extend the timeout period. If for some reason, the stop method can not return,
+     * then it must call WrapperManager.stopped() to avoid warning messages from the
+     * Wrapper.
+     * 
+     * @param exitCode The suggested exit code that will be returned to the OS when
+     *            the JVM exits.
+     * @return The exit code to actually return to the OS. In most cases, this should
+     *         just be the value of exitCode, however the user code has the option of
+     *         changing the exit code if there are any problems during shutdown.
+     */
+    public int stop(int exitCode)
+    {
+        mule.shutdown();
+        return exitCode;
+    }
+
+    /**
+     * Called whenever the native wrapper code traps a system control signal against
+     * the Java process. It is up to the callback to take any actions necessary.
+     * Possible values are: WrapperManager.WRAPPER_CTRL_C_EVENT,
+     * WRAPPER_CTRL_CLOSE_EVENT, WRAPPER_CTRL_LOGOFF_EVENT, or
+     * WRAPPER_CTRL_SHUTDOWN_EVENT
+     * 
+     * @param event The system control signal.
+     */
+    public void controlEvent(int event)
+    {
+        if (WrapperManager.isControlledByNativeWrapper())
+        {
+            // The Wrapper will take care of this event
+        }
+        else
+        {
+            // We are not being controlled by the Wrapper, so
+            // handle the event ourselves.
+            if ((event == WrapperManager.WRAPPER_CTRL_C_EVENT)
+                || (event == WrapperManager.WRAPPER_CTRL_CLOSE_EVENT)
+                || (event == WrapperManager.WRAPPER_CTRL_SHUTDOWN_EVENT))
+            {
+                WrapperManager.stop(0);
+            }
         }
     }
 }
