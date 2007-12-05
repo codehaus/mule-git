c051d6a2704935606be9b87dd7b513311557dc73
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2007-12-05 18:00:51 +0000

    Error text now matches attempted action.
    git-svn-id: file:///home/projects/mule/scm@10003 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/modules/management/src/main/java/org/mule/management/mbeans/MuleService.java
+++ trunk/mule/modules/management/src/main/java/org/mule/management/mbeans/MuleService.java
@@ -197,7 +197,7 @@ public class MuleService implements MuleServiceMBean
             }
             catch (IOException e)
             {
-                logger.warn("Failed to load LICENSE.txt", e);
+                logger.warn("Failed to load MULE_LICENSE.txt", e);
             }
             if (license == null)
             {
