c66fef3807d71390523132e12dc1fa3c2fce9560
dfeist <dfeist@bf997673-6b11-0410-b953-e057580c5b09>
2008-01-08 11:42:43 +0000

    Merged revisions 10250-10251 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.x
    ................
      r10250 | dirk.olmes | 2008-01-08 04:50:14 -0200 (Tue, 08 Jan 2008) | 9 lines
      
      Merged revisions 10249 via svnmerge from 
      https://svn.codehaus.org/mule/trunk/mule
      
      ........
        r10249 | dirk.olmes | 2008-01-08 07:48:29 +0100 (Tue, 08 Jan 2008) | 1 line
        
        MULE-2838 (NPE when using client.sendDirect to a nonexisting UMO): also make UMOException null-safe.
      ........
    ................
      r10251 | acooke | 2008-01-08 08:42:03 -0200 (Tue, 08 Jan 2008) | 3 lines
      
      Removing spring:entry: http, email, axis (last incomplete).
      Expected this to commit overnight, but there was a svn conflict - seems to have merged OK now.
    ................
    
    
    git-svn-id: file:///home/projects/mule/scm@10254 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.x-mgmtcontext-spring/transports/http/src/main/java/org/mule/providers/http/config/HttpNamespaceHandler.java
+++ branches/mule-2.x-mgmtcontext-spring/transports/http/src/main/java/org/mule/providers/http/config/HttpNamespaceHandler.java
@@ -9,8 +9,9 @@
  */
 package org.mule.providers.http.config;
 
+
 import org.mule.config.spring.handlers.AbstractMuleNamespaceHandler;
-import org.mule.config.spring.parsers.collection.ChildListDefinitionParser;
+import org.mule.config.spring.parsers.collection.ChildListEntryDefinitionParser;
 import org.mule.config.spring.parsers.collection.ChildMapEntryDefinitionParser;
 import org.mule.config.spring.parsers.generic.MuleOrphanDefinitionParser;
 import org.mule.config.spring.parsers.specific.ComplexComponentDefinitionParser;
@@ -35,9 +36,9 @@ public class HttpNamespaceHandler extends AbstractMuleNamespaceHandler
         registerBeanDefinitionParser("connector", new MuleOrphanDefinitionParser(HttpConnector.class, true));
 
         registerBeanDefinitionParser("rest-service-component", new ComplexComponentDefinitionParser(RestServiceWrapper.class));
-        registerBeanDefinitionParser("payloadParameterNames", new ChildListDefinitionParser("payloadParameterNames"));
-        registerBeanDefinitionParser("requiredParams", new ChildMapEntryDefinitionParser("requiredParams","key","value"));
-        registerBeanDefinitionParser("optionalParams", new ChildMapEntryDefinitionParser("optionalParams","key","value"));
+        registerBeanDefinitionParser("payloadParameterName", new ChildListEntryDefinitionParser("payloadParameterNames", ChildMapEntryDefinitionParser.VALUE));
+        registerBeanDefinitionParser("requiredParameter", new ChildMapEntryDefinitionParser("requiredParams"));
+        registerBeanDefinitionParser("optionalParameter", new ChildMapEntryDefinitionParser("optionalParams"));
         
         registerBeanDefinitionParser("http-client-response-to-object", new MuleOrphanDefinitionParser(HttpClientMethodResponseToObject.class, false));
         registerBeanDefinitionParser("http-response-to-string", new MuleOrphanDefinitionParser(HttpResponseToString.class, false));
