ea6dfb40dd21fcf2423090012df28cc14f40b2f9
ashaposhnikov <ashaposhnikov@bf997673-6b11-0410-b953-e057580c5b09>
2008-01-21 13:27:15 +0000

    Checkstyle
    git-svn-id: file:///home/projects/mule/scm@10421 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/transports/jdbc/src/test/java/org/mule/providers/jdbc/DerbyStoredProc.java
+++ trunk/mule/transports/jdbc/src/test/java/org/mule/providers/jdbc/DerbyStoredProc.java
@@ -1,3 +1,11 @@
+/**
+* Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+*
+* The software in this package is published under the terms of the CPAL v1.0
+* license, a copy of which has been included with this distribution in the
+* LICENSE.txt file.
+*/
+
 package org.mule.providers.jdbc;
 
 import java.sql.Connection;
@@ -6,19 +14,22 @@ import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-public class DerbyStoredProc {
-
-    public static void plus(int a, int[] b, double[] c, String[] str) {
-        try {
+public class DerbyStoredProc
+{
+    public static void plus(int a, int[] b, double[] c, String[] str)
+    {
+        try
+        {
             Connection conn = DriverManager.getConnection("jdbc:default:connection");
             PreparedStatement statement = conn.prepareStatement("SELECT * FROM TEST");
             ResultSet rset = statement.executeQuery();
-        } catch (SQLException ex) {
+        }
+        catch (SQLException ex)
+        {
 
         }
         c[0] = a + b[0] + 0.3;
         b[0] = 10;
         str[0] = "test";
-        //return result;
     }
 }
