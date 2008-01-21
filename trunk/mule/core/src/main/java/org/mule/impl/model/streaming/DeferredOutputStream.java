537a2b9bd26804258e337f91c50cf7525cd46a1e
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2008-01-21 14:07:06 +0000

    Typo.
    git-svn-id: file:///home/projects/mule/scm@10422 bf997673-6b11-0410-b953-e057580c5b09

--- trunk/mule/core/src/main/java/org/mule/impl/model/streaming/DeferredOutputStream.java
+++ trunk/mule/core/src/main/java/org/mule/impl/model/streaming/DeferredOutputStream.java
@@ -32,7 +32,7 @@ import org.apache.commons.logging.LogFactory;
  * This outputStream allows a stream to be choosen after this has been created.  It is used by
  * Mule when using streaming to pass to the Streaming component. The actual output stream is
  * choosen at the point where the first byte is written. At this point an outbound router can be
- * choosen and values set on the event context can affect which stream is choosen.
+ * chosen and values set on the event context can affect which stream is choosen.
  */
 public class DeferredOutputStream extends OutputStream
 {
