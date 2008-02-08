a52ca2f15d623d2ba74285bef4838d07af48b6f5
dirk.olmes <dirk.olmes@bf997673-6b11-0410-b953-e057580c5b09>
2008-02-08 03:16:21 +0000

    Merged revisions 10744-10745 via svnmerge from https://svn.codehaus.org/mule/trunk/mule
    ........
      r10744 | holger | 2008-02-07 17:33:10 +0100 (Thu, 07 Feb 2008) | 1 line
      
      no warnings please
    ........
      r10745 | holger | 2008-02-07 17:33:42 +0100 (Thu, 07 Feb 2008) | 1 line
      
      MULE-2972: FTP message dispatcher fails to specify encoding for data types other than UMOStreamMessageAdapter and byte[]
    ........
    
    
    git-svn-id: file:///home/projects/mule/scm@10747 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-1.4.x/transports/ftp/src/main/java/org/mule/providers/ftp/FtpMessageDispatcher.java
+++ branches/mule-1.4.x/transports/ftp/src/main/java/org/mule/providers/ftp/FtpMessageDispatcher.java
@@ -71,7 +71,7 @@ public class FtpMessageDispatcher extends AbstractMessageDispatcher
                 }
                 else
                 {
-                    dataBytes = data.toString().getBytes();
+                    dataBytes = data.toString().getBytes(event.getEncoding());
                 }
                 IOUtils.write(dataBytes, out);
             }
