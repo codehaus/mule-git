543b478abc82c7b507fec512222f6c8e70bfd223
holger <holger@bf997673-6b11-0410-b953-e057580c5b09>
2008-02-18 12:20:34 +0000

    MULE-2676 step 1: lookup() has to go
    git-svn-id: file:///home/projects/mule/scm@10842 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.0.x/modules/spring-config/src/main/java/org/mule/util/object/SpringBeanLookup.java
+++ branches/mule-2.0.x/modules/spring-config/src/main/java/org/mule/util/object/SpringBeanLookup.java
@@ -81,16 +81,6 @@ public class SpringBeanLookup implements ObjectFactory, ApplicationContextAware
         return applicationContext.getBean(bean);
     }
 
-    /**
-     * Unsupported: Calling applicationContext.getBean() will instantiate a bean if it is lazy-init or
-     * a non-singleton, which would not be a pure lookup.
-     */
-    // TODO HH: remove
-    public Object lookup(String id) throws Exception
-    {
-        throw new UnsupportedOperationException();
-    }
-
     public void release(Object object) throws Exception
     {
         // Not implemented for Spring Beans
