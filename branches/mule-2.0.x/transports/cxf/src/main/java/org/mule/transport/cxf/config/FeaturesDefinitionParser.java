20bd1cc2f2dd9430e4a7a5252ad7e8f0377f756d
acooke <acooke@bf997673-6b11-0410-b953-e057580c5b09>
2008-02-26 12:26:16 +0000

    Documentation for Dan D.
    git-svn-id: file:///home/projects/mule/scm@11023 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.0.x/transports/cxf/src/main/java/org/mule/transport/cxf/config/FeaturesDefinitionParser.java
+++ branches/mule-2.0.x/transports/cxf/src/main/java/org/mule/transport/cxf/config/FeaturesDefinitionParser.java
@@ -19,6 +19,11 @@ import org.mule.config.spring.parsers.assembly.configuration.SimplePropertyConfi
 import org.mule.config.spring.parsers.assembly.configuration.PropertyConfiguration;
 import org.mule.config.spring.MuleHierarchicalBeanDefinitionParserDelegate;
 
+/**
+ * This parser allows us to inter-operate with third party (Apache CXF) bean definition parsers.
+ * It "calls out" to the parsers for the child elements and sets them as collection entries on the
+ * property "features".
+ */
 public class FeaturesDefinitionParser extends ChildSingletonMapDefinitionParser
 {
 
