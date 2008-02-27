f4594a46f2dc70b81eb99796f741317da711ad71
dfeist <dfeist@bf997673-6b11-0410-b953-e057580c5b09>
2008-02-27 05:08:01 +0000

    MULE-3004 DefaultRemoteSync unused MULE-2161 Registry should not contain ManagementContext
    git-svn-id: file:///home/projects/mule/scm@11060 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.0.x/tests/integration/src/test/java/org/mule/test/integration/client/MuleClientJmsTestCase.java
+++ branches/mule-2.0.x/tests/integration/src/test/java/org/mule/test/integration/client/MuleClientJmsTestCase.java
@@ -25,7 +25,7 @@ public class MuleClientJmsTestCase extends FunctionalTestCase
 
     public MuleClientJmsTestCase()
     {
-        setDisposeManagerPerSuite(true);
+        setDisposeManagerPerSuite(false);
     }
 
     protected String getConfigResources()
