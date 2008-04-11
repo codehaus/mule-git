8bb3023a42342d8ecc2aef1d20c12245b788e6fd
tcarlson <tcarlson@bf997673-6b11-0410-b953-e057580c5b09>
2008-04-11 12:15:16 +0000

    MULE-3212 Upgrade jBPM to 3.2.2 and spring-modules-jbpm31 to 0.9 Removed workaround for MOD-266 (Spring Modules bug) Fixed MULE-3200 jBPM test cases throw a NPE on shutdown Fixed a bug which resulted from the patch for MULE-2836 Added a <bpm:outbound-router> element as a shortcut Started on MULE-3205 Implement <bpm:component> type
    git-svn-id: file:///home/projects/mule/scm@11566 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.0.x/examples/loanbroker/bpm/src/test/java/org/mule/example/loanbroker/bpm/JBpmFunctionalTestCase.java
+++ branches/mule-2.0.x/examples/loanbroker/bpm/src/test/java/org/mule/example/loanbroker/bpm/JBpmFunctionalTestCase.java
@@ -10,10 +10,8 @@
 
 package org.mule.example.loanbroker.bpm;
 
-import org.mule.api.config.ConfigurationBuilder;
 import org.mule.api.config.ConfigurationException;
 import org.mule.config.i18n.MessageFactory;
-import org.mule.config.spring.SpringXmlConfigurationBuilder;
 import org.mule.example.loanbroker.tests.AbstractAsynchronousLoanBrokerTestCase;
 import org.mule.transport.bpm.BPMS;
 import org.mule.transport.bpm.ProcessConnector;
@@ -25,6 +23,12 @@ public class JBpmFunctionalTestCase extends AbstractAsynchronousLoanBrokerTestCa
     /** For unit tests, we assume a virgin database, therefore the process ID is assumed to be = 1 */
     public static final long PROCESS_ID = 1;
 
+    public JBpmFunctionalTestCase()
+    {
+        super();
+        setDisposeManagerPerSuite(true);        
+    }
+    
     @Override
     protected void suitePreSetUp() throws Exception
     {
@@ -37,11 +41,6 @@ public class JBpmFunctionalTestCase extends AbstractAsynchronousLoanBrokerTestCa
     }
 
     @Override
-    protected ConfigurationBuilder getBuilder() throws Exception 
-    {
-        return new SpringXmlConfigurationBuilder(getConfigResources());
-    }
-    
     protected String getConfigResources()
     {
         return "loan-broker-bpm-mule-config.xml";
@@ -49,7 +48,7 @@ public class JBpmFunctionalTestCase extends AbstractAsynchronousLoanBrokerTestCa
 
     @Override
     public void testSingleLoanRequest() throws Exception
-    {
+    {        
         super.testSingleLoanRequest();
         
         ProcessConnector connector =
@@ -63,16 +62,5 @@ public class JBpmFunctionalTestCase extends AbstractAsynchronousLoanBrokerTestCa
         //   org.hibernate.LazyInitializationException: could not initialize proxy - the owning MuleSession was closed
         // See http://forum.springframework.org/archive/index.php/t-24800.html
         //assertEquals("loanApproved", bpms.getState(bpms.lookupProcess(new Long(PROCESS_ID))));
-    }
-    
-    @Override
-    public void testLotsOfLoanRequests() throws Exception
-    {
-        super.testLotsOfLoanRequests();
-        
-        //without this sleep, the test still succeeds but throws a series of exceptions
-        //probably Spring would not have enough time to close db connections before
-        //database itself is shut down while jvm start disposing
-        Thread.sleep(100);
     }
 }
