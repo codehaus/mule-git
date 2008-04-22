ee502593762c1b93ab6ccf9dea6cd1d069537d12
aguenther <aguenther@bf997673-6b11-0410-b953-e057580c5b09>
2008-04-22 23:12:40 +0000

    MULE-2536 - This initial commit only corrects assert usage and improves failure output used for further issue resolution.
    git-svn-id: file:///home/projects/mule/scm@11632 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.0.x/core/src/test/java/org/mule/util/queue/AbstractTransactionQueueManagerTestCase.java
+++ branches/mule-2.0.x/core/src/test/java/org/mule/util/queue/AbstractTransactionQueueManagerTestCase.java
@@ -39,15 +39,15 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
         QueueSession s = mgr.getQueueSession();
         Queue q = s.getQueue("queue1");
 
-        assertEquals(0, q.size());
+        assertEquals("Queue size", 0, q.size());
         // Object o = q.take();
         // assertNull(o);
         q.put("String1");
-        assertEquals(1, q.size());
+        assertEquals("Queue size", 1, q.size());
         Object o = q.take();
         assertNotNull(o);
-        assertEquals("String1", o);
-        assertEquals(0, q.size());
+        assertEquals("Queue content", "String1", o);
+        assertEquals("Queue size", 0, q.size());
 
         mgr.stop(AbstractResourceManager.SHUTDOWN_MODE_NORMAL);
     }
@@ -69,7 +69,7 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
                     Thread.sleep(200);
                     QueueSession s = mgr.getQueueSession();
                     Queue q = s.getQueue("queue1");
-                    assertEquals(0, q.size());
+                    assertEquals("Queue size", 0, q.size());
                     q.put("String1");
                 }
                 catch (Exception e)
@@ -83,13 +83,13 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
         long t0 = System.currentTimeMillis();
         QueueSession s = mgr.getQueueSession();
         Queue q = s.getQueue("queue1");
-        assertEquals(0, q.size());
+        assertEquals("Queue size", 0, q.size());
         Object o = q.take();
         long t1 = System.currentTimeMillis();
         t.join();
         assertNotNull(o);
-        assertEquals("String1", o);
-        assertEquals(0, q.size());
+        assertEquals("Queue content", "String1", o);
+        assertEquals("Queue size", 0, q.size());
         assertTrue(t1 - t0 > 100);
 
         mgr.stop(AbstractResourceManager.SHUTDOWN_MODE_NORMAL);
@@ -112,7 +112,7 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
                     Thread.sleep(200);
                     QueueSession s = mgr.getQueueSession();
                     Queue q = s.getQueue("queue1");
-                    assertEquals(0, q.size());
+                    assertEquals("Queue size", 0, q.size());
                     s.begin();
                     q.put("String1");
                     s.rollback();
@@ -131,13 +131,13 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
         long t0 = System.currentTimeMillis();
         QueueSession s = mgr.getQueueSession();
         Queue q = s.getQueue("queue1");
-        assertEquals(0, q.size());
+        assertEquals("Queue size", 0, q.size());
         Object o = q.take();
         long t1 = System.currentTimeMillis();
         t.join();
         assertNotNull(o);
-        assertEquals("String2", o);
-        assertEquals(0, q.size());
+        assertEquals("Queue content", "String2", o);
+        assertEquals("Queue size", 0, q.size());
         assertTrue(t1 - t0 > 100);
 
         mgr.stop(AbstractResourceManager.SHUTDOWN_MODE_NORMAL);
@@ -162,7 +162,7 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
                     QueueSession s = mgr.getQueueSession();
                     Queue q = s.getQueue("queue1");
                     Object o = q.take();
-                    assertEquals("String1", o);
+                    assertEquals("Queue content", "String1", o);
                 }
                 catch (Exception e)
                 {
@@ -173,7 +173,7 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
         t.start();
         QueueSession s = mgr.getQueueSession();
         Queue q = s.getQueue("queue1");
-        assertEquals(0, q.size());
+        assertEquals("Queue size", 0, q.size());
         q.put("String1");
         q.put("String2");
         latch.countDown();
@@ -181,7 +181,7 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
         q.put("String3");
         long t1 = System.currentTimeMillis();
         t.join();
-        assertEquals(2, q.size());
+        assertEquals("Queue size", 2, q.size());
         assertTrue(t1 - t0 > 100);
 
         mgr.stop(AbstractResourceManager.SHUTDOWN_MODE_NORMAL);
@@ -200,10 +200,10 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
                 QueueSession s = mgr.getQueueSession();
                 Queue q = s.getQueue("queue1");
                 q.put("String1");
-                assertEquals(1, q.size());
+                assertEquals("Queue size", 1, q.size());
 
                 q = s.getQueue("queue1");
-                assertEquals(1, q.size());
+                assertEquals("Queue size", 1, q.size());
             }
             finally
             {
@@ -216,7 +216,7 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
                 mgr.start();
                 QueueSession s = mgr.getQueueSession();
                 Queue q = s.getQueue("queue1");
-                assertEquals(1, q.size());
+                assertEquals("Queue size", 1, q.size());
             }
             finally
             {
@@ -243,13 +243,13 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
                 Queue q = s.getQueue("queue1");
                 s.begin();
                 q.put("String1");
-                assertEquals(1, q.size());
+                assertEquals("Queue size", 1, q.size());
                 s.commit();
-                assertEquals(1, q.size());
+                assertEquals("Queue size", 1, q.size());
 
                 s = mgr.getQueueSession();
                 q = s.getQueue("queue1");
-                assertEquals(1, q.size());
+                assertEquals("Queue size", 1, q.size());
 
                 mgr.stop(AbstractResourceManager.SHUTDOWN_MODE_NORMAL);
 
@@ -257,7 +257,7 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
                 mgr.start();
                 s = mgr.getQueueSession();
                 q = s.getQueue("queue1");
-                assertEquals(1, q.size());
+                assertEquals("Queue size", 1, q.size());
             }
             finally
             {
@@ -284,13 +284,13 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
                 Queue q = s.getQueue("queue1");
                 s.begin();
                 q.put("String1");
-                assertEquals(1, q.size());
+                assertEquals("Queue size", 1, q.size());
                 s.rollback();
-                assertEquals(0, q.size());
+                assertEquals("Queue size", 0, q.size());
 
                 s = mgr.getQueueSession();
                 q = s.getQueue("queue1");
-                assertEquals(0, q.size());
+                assertEquals("Queue size", 0, q.size());
 
                 mgr.stop(AbstractResourceManager.SHUTDOWN_MODE_NORMAL);
 
@@ -298,7 +298,7 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
                 mgr.start();
                 s = mgr.getQueueSession();
                 q = s.getQueue("queue1");
-                assertEquals(0, q.size());
+                assertEquals("Queue size", 0, q.size());
 
             }
             finally
@@ -332,50 +332,50 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
             Queue q2s2 = s2.getQueue("queue2");
 
             q1s1.put("String1");
-            assertEquals(1, q1s1.size());
-            assertEquals(1, q1s2.size());
+            assertEquals("Queue size", 1, q1s1.size());
+            assertEquals("Queue size", 1, q1s2.size());
 
             s1.begin();
 
             Object o = q1s1.take();
             assertNotNull(o);
             assertEquals("String1", o);
-            assertEquals(0, q1s1.size());
-            assertEquals(0, q1s2.size());
+            assertEquals("Queue size", 0, q1s1.size());
+            assertEquals("Queue size", 0, q1s2.size());
             q2s1.put("String2");
-            assertEquals(1, q2s1.size());
-            assertEquals(0, q2s2.size());
+            assertEquals("Queue size", 1, q2s1.size());
+            assertEquals("Queue size", 0, q2s2.size());
 
             s1.commit();
 
-            assertEquals(0, q1s1.size());
-            assertEquals(0, q1s2.size());
-            assertEquals(1, q2s1.size());
-            assertEquals(1, q2s2.size());
+            assertEquals("Queue size", 0, q1s1.size());
+            assertEquals("Queue size", 0, q1s2.size());
+            assertEquals("Queue size", 1, q2s1.size());
+            assertEquals("Queue size", 1, q2s2.size());
 
             s1.begin();
 
             o = q2s1.take();
             assertNotNull(o);
             assertEquals("String2", o);
-            assertEquals(0, q1s1.size());
-            assertEquals(0, q1s2.size());
-            assertEquals(0, q2s1.size());
-            assertEquals(0, q2s2.size());
+            assertEquals("Queue size", 0, q1s1.size());
+            assertEquals("Queue size", 0, q1s2.size());
+            assertEquals("Queue size", 0, q2s1.size());
+            assertEquals("Queue size", 0, q2s2.size());
 
             q1s1.put("String1");
 
-            assertEquals(1, q1s1.size());
-            assertEquals(0, q1s2.size());
-            assertEquals(0, q2s1.size());
-            assertEquals(0, q2s2.size());
+            assertEquals("Queue size", 1, q1s1.size());
+            assertEquals("Queue size", 0, q1s2.size());
+            assertEquals("Queue size", 0, q2s1.size());
+            assertEquals("Queue size", 0, q2s2.size());
 
             s1.rollback();
 
-            assertEquals(0, q1s1.size());
-            assertEquals(0, q1s2.size());
-            assertEquals(1, q2s1.size());
-            assertEquals(1, q2s2.size());
+            assertEquals("Queue size", 0, q1s1.size());
+            assertEquals("Queue size", 0, q1s2.size());
+            assertEquals("Queue size", 1, q2s1.size());
+            assertEquals("Queue size", 1, q2s2.size());
 
         }
         finally
@@ -396,18 +396,18 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
             QueueSession s = mgr.getQueueSession();
             Queue q = s.getQueue("queue1");
 
-            assertEquals(q.size(), 0);
+            assertEquals("Queue size", 0, q.size());
             Object o = q.poll(0);
-            assertEquals(q.size(), 0);
+            assertEquals("Queue size", 0, q.size());
             assertNull(o);
             o = q.poll(1000);
-            assertEquals(q.size(), 0);
+            assertEquals("Queue size", 0, q.size());
             assertNull(o);
             q.put("String1");
-            assertEquals(q.size(), 1);
+            assertEquals("Queue size", 1, q.size());
             o = q.poll(0);
-            assertEquals(q.size(), 0);
-            assertEquals("String1", o);
+            assertEquals("Queue size", 0, q.size());
+            assertEquals("Queue content", "String1", o);
 
             new Thread(new Runnable()
             {
@@ -427,8 +427,8 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
                 }
             }).start();
             o = q.poll(1000);
-            assertEquals(q.size(), 0);
-            assertEquals("String1", o);
+            assertEquals("Queue size", q.size(), 0);
+            assertEquals("Queue content", "String1", o);
 
         }
         finally
@@ -449,16 +449,15 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
             QueueSession s = mgr.getQueueSession();
             Queue q = s.getQueue("queue1");
 
-            assertEquals(q.size(), 0);
+            assertEquals("Queue size", 0, q.size());
             Object o = q.peek();
-            assertEquals(q.size(), 0);
+            assertEquals("Queue size", 0, q.size());
             assertNull(o);
             q.put("String1");
-            assertEquals(q.size(), 1);
+            assertEquals("Queue size", 1, q.size());
             o = q.peek();
-            assertEquals(q.size(), 1);
-            assertEquals("String1", o);
-
+            assertEquals("Queue size", 1, q.size());
+            assertEquals("Queue content", "String1", o);
         }
         finally
         {
@@ -478,11 +477,11 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
             QueueSession s = mgr.getQueueSession();
             Queue q = s.getQueue("queue1");
 
-            assertEquals(q.size(), 0);
+            assertEquals("Queue size", 0, q.size());
             assertTrue(q.offer("String1", 0L));
-            assertEquals(q.size(), 1);
+            assertEquals("Queue size", 1, q.size());
             assertFalse(q.offer("String2", 1000));
-            assertEquals(q.size(), 1);
+            assertEquals("Queue size", 1, q.size());
 
             new Thread(new Runnable()
             {
@@ -493,7 +492,7 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
                         Thread.sleep(500);
                         QueueSession s = mgr.getQueueSession();
                         Queue q = s.getQueue("queue1");
-                        assertEquals("String1", q.take());
+                        assertEquals("Queue content", "String1", q.take());
                     }
                     catch (Exception e)
                     {
@@ -502,7 +501,7 @@ public abstract class AbstractTransactionQueueManagerTestCase extends AbstractMu
                 }
             }).start();
             assertTrue(q.offer("String2", 1000));
-            assertEquals(q.size(), 1);
+            assertEquals("Queue size", 1, q.size());
         }
         finally
         {
