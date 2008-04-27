a98fdee4e89b1abd23b634a272041ecadf126e3f
dirk.olmes <dirk.olmes@bf997673-6b11-0410-b953-e057580c5b09>
2008-04-27 09:21:50 +0000

    fix license headers and svn keywords
    git-svn-id: file:///home/projects/mule/scm@11650 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.0.x/transports/cxf/src/test/java/org/mule/transport/cxf/MtomTestCase.java
+++ branches/mule-2.0.x/transports/cxf/src/test/java/org/mule/transport/cxf/MtomTestCase.java
@@ -1,9 +1,9 @@
 /*
- * $Id: XFireBasicTestCase.java 6659 2007-05-23 04:05:51Z hasari $
+ * $Id$
  * --------------------------------------------------------------------------------------
  * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
  *
- * The software in this package is published under the terms of the MuleSource MPL
+ * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
