be82a09924e7ed47b3e4d3a52251717455a7c131
dfeist <dfeist@bf997673-6b11-0410-b953-e057580c5b09>
2008-04-29 15:09:01 +0000

    MULE-3255 Inbound and response transformers configured on CXF endpoints are ignored
    git-svn-id: file:///home/projects/mule/scm@11657 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.0.x/transports/cxf/src/test/java/org/mule/transport/cxf/issues/InboundTransformerMule3255TestCase.java
+++ branches/mule-2.0.x/transports/cxf/src/test/java/org/mule/transport/cxf/issues/InboundTransformerMule3255TestCase.java
@@ -10,16 +10,9 @@
 
 package org.mule.transport.cxf.issues;
 
-import org.mule.api.MuleEventContext;
 import org.mule.api.MuleMessage;
 import org.mule.module.client.MuleClient;
 import org.mule.tck.FunctionalTestCase;
-import org.mule.tck.functional.EventCallback;
-import org.mule.tck.functional.FunctionalTestComponent2;
-import org.mule.tck.functional.StringAppendTestTransformer;
-import org.mule.util.concurrent.Latch;
-
-import edu.emory.mathcs.backport.java.util.concurrent.TimeUnit;
 
 public class InboundTransformerMule3255TestCase extends FunctionalTestCase
 {
@@ -31,22 +24,9 @@ public class InboundTransformerMule3255TestCase extends FunctionalTestCase
 
     public void testSyncResponse() throws Exception
     {
-        final Latch latch = new Latch();
-
-        EventCallback callback1 = new EventCallback()
-        {
-            public void eventReceived(final MuleEventContext context, final Object component) throws Exception
-            {
-                assertEquals("request" + StringAppendTestTransformer.DEFAULT_TEXT, context.transformMessage());
-                latch.countDown();
-            }
-        };
-
-        ((FunctionalTestComponent2) getComponent("CXFService")).setEventCallback(callback1);
         MuleClient client = new MuleClient();
-        MuleMessage message = client.send("cxf:http://localhost:4444/services/CXFService?method=onReceive", "request",
-            null);
+        MuleMessage message = client.send("cxf:http://localhost:4444/services/CXFService?method=echo", "request", null);
         assertNotNull(message);
-        assert (latch.await(5, TimeUnit.SECONDS));
+        assertEquals("request transformed", message.getPayload());
     }
 }
