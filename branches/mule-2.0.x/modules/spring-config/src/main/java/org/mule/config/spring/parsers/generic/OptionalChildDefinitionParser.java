aa63bdd097c1c1beb4e1552853b4fecb73e952d3
dirk.olmes <dirk.olmes@bf997673-6b11-0410-b953-e057580c5b09>
2008-05-08 06:46:45 +0000

    svn properties, correct license headers, jdk1.4 syntax
    MORE ATTENTION TO DETAILS, PLEASE!
    
    
    git-svn-id: file:///home/projects/mule/scm@11695 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.0.x/modules/spring-config/src/main/java/org/mule/config/spring/parsers/generic/OptionalChildDefinitionParser.java
+++ branches/mule-2.0.x/modules/spring-config/src/main/java/org/mule/config/spring/parsers/generic/OptionalChildDefinitionParser.java
@@ -3,7 +3,7 @@
  * --------------------------------------------------------------------------------------
  * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
  *
- * The software in this package is published under the terms of the MuleSource MPL
+ * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
@@ -47,7 +47,7 @@ public class OptionalChildDefinitionParser extends ChildDefinitionParser
         super(setterMethod, clazz, constraint, allowClassAttribute);
     }
 
-    @Override
+    // @Override
     protected void parseChild(Element element, ParserContext parserContext, BeanDefinitionBuilder builder)
     {
         String parentBean = getParentBeanName(element);
