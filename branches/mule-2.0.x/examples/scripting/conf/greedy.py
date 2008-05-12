33572ea5bee4a611f74dfa8867cdeed69f0e4141
tcarlson <tcarlson@bf997673-6b11-0410-b953-e057580c5b09>
2008-05-12 18:49:56 +0000

    Tabs-to-spaces (sorry, my upgrade to Eclipse 3.3 was the culprit)
    git-svn-id: file:///home/projects/mule/scm@11727 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.0.x/examples/scripting/conf/greedy.py
+++ branches/mule-2.0.x/examples/scripting/conf/greedy.py
@@ -7,23 +7,23 @@ class Change:
     def __init__(self, currency, amount):
         self.amount = amount
         if currency == "USD":
-	        self.coins = [1,5,10,25]
-	        self.coin_lookup = {25: "quarters", 10: "dimes", 5: "nickels", 1: "pennies"}
+            self.coins = [1,5,10,25]
+            self.coin_lookup = {25: "quarters", 10: "dimes", 5: "nickels", 1: "pennies"}
         elif currency == "GBP":
-	        self.coins = [1,2,5,10,20,50,100,200]
-	        self.coin_lookup = {200: "two_pounds", 100: "one_pound", 50: "fifty_pence", 20: "twenty_pence", 10: "ten_pence", 5: "five_pence", 2: "two_pence", 1: "one_penny"}
-	    #else:
-	    #	print "Currency $currency not recognized"
-	    #	exit 1
+            self.coins = [1,2,5,10,20,50,100,200]
+            self.coin_lookup = {200: "two_pounds", 100: "one_pound", 50: "fifty_pence", 20: "twenty_pence", 10: "ten_pence", 5: "five_pence", 2: "two_pence", 1: "one_penny"}
+        #else:
+        #	print "Currency $currency not recognized"
+        #	exit 1
         self.result = ""
 
     def printer(self,num,coin):
         if num:
             if coin in self.coin_lookup:
-            	if self.result == "":
-	            	self.result = '%1.0f %s' % (str(num), self.coin_lookup[coin])
-            	else:
-	            	self.result = '%s, %1.0f %s' % (self.result, str(num), self.coin_lookup[coin])
+                if self.result == "":
+                    self.result = '%1.0f %s' % (str(num), self.coin_lookup[coin])
+                else:
+                    self.result = '%s, %1.0f %s' % (self.result, str(num), self.coin_lookup[coin])
 
     def recursive_change(self, rem):
         if len(self.coins) == 0:
