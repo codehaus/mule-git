f54d45cb3cb84458273f02508cd27390bfa1325d
dandiep <dandiep@bf997673-6b11-0410-b953-e057580c5b09>
2008-06-28 12:44:48 +0000

    Create a reversible xml stream reader for where we only want to parse a portion of the document. Cache the transformer factory for a performance improvement in XMLUtils. Create a transformer to convert to XMLStreamReaders.
    git-svn-id: file:///home/projects/mule/scm@12196 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.0.x/modules/xml/src/main/java/org/mule/module/xml/util/XMLUtils.java
+++ branches/mule-2.0.x/modules/xml/src/main/java/org/mule/module/xml/util/XMLUtils.java
@@ -25,6 +25,20 @@ public class XMLUtils
 {
     public static final String TRANSFORMER_FACTORY_JDK5 = "com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl";
 
+    private static TransformerFactory transformerFactory;
+    
+    static 
+    {
+    	try
+        {
+    		transformerFactory = TransformerFactory.newInstance();
+        }
+        catch (TransformerFactoryConfigurationError e)
+        {
+            System.setProperty("javax.xml.transform.TransformerFactory", TRANSFORMER_FACTORY_JDK5);
+            transformerFactory = TransformerFactory.newInstance();
+        }
+    }
     /**
      * Converts a DOM to an XML string.
      */
@@ -39,20 +53,10 @@ public class XMLUtils
      * runtime environment.
      */
     public static Transformer getTransformer() throws TransformerConfigurationException
-    {
-        TransformerFactory tf;
-        try
+    {       
+        if (transformerFactory != null)
         {
-            tf = TransformerFactory.newInstance();
-        }
-        catch (TransformerFactoryConfigurationError e)
-        {
-            System.setProperty("javax.xml.transform.TransformerFactory", TRANSFORMER_FACTORY_JDK5);
-            tf = TransformerFactory.newInstance();
-        }
-        if (tf != null)
-        {
-            return tf.newTransformer();
+            return transformerFactory.newTransformer();
         }
         else
         {
