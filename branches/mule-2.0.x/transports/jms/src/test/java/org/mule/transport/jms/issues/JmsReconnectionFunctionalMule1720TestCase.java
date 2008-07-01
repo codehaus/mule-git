2fe13cd386e5529633ee3cf9ac09980801958fae
aguenther <aguenther@bf997673-6b11-0410-b953-e057580c5b09>
2008-07-01 20:00:18 +0000

    MULE-1720 - Added test case to verify previously committed patch (r12195). Apparently, the re-connection doesn't kick in and I had to disable the test. But I thought it's useful to have at least one and maybe someone wants to review and see whether the test case is bogus or the workaround patch still doesn't properly solve the problem.
    git-svn-id: file:///home/projects/mule/scm@12220 bf997673-6b11-0410-b953-e057580c5b09

---
+++ branches/mule-2.0.x/transports/jms/src/test/java/org/mule/transport/jms/issues/JmsReconnectionFunctionalMule1720TestCase.java
@@ -0,0 +1,54 @@
+package org.mule.transport.jms.issues;
+
+import org.mule.api.MuleEventContext;
+import org.mule.component.DefaultJavaComponent;
+import org.mule.transport.jms.JmsConnector;
+import org.mule.tck.functional.EventCallback;
+import org.mule.tck.functional.FunctionalTestComponent;
+import org.mule.transport.jms.integration.AbstractJmsFunctionalTestCase;
+import org.mule.util.concurrent.Latch;
+
+import javax.jms.JMSException;
+
+import edu.emory.mathcs.backport.java.util.concurrent.TimeUnit;
+
+public class JmsReconnectionFunctionalMule1720TestCase extends AbstractJmsFunctionalTestCase
+{
+    private long RECONNECTION_TIMEOUT = 5000;
+    private Latch componentCallbackLatch;
+
+    protected String getConfigResources()
+    {
+        return "providers/activemq/jms-reconnection-strategy.xml";
+    }
+    
+	public void testReconnectionAfterConnectionFailure() throws Exception {
+	    componentCallbackLatch = new Latch();
+	    EventCallback callback = new EventCallback()
+        {
+            public void eventReceived(MuleEventContext context, Object component) throws Exception
+            {
+                componentCallbackLatch.countDown();
+            }
+        };
+        DefaultJavaComponent defaultComponent = (DefaultJavaComponent) muleContext.getRegistry().lookupService("InputPart").getComponent();
+        FunctionalTestComponent testComponent = (FunctionalTestComponent) defaultComponent.getObjectFactory().getInstance();
+        testComponent.setEventCallback(callback);
+        
+        send(super.scenarioNoTx);
+        assertTrue(componentCallbackLatch.await(LOCK_WAIT, TimeUnit.MILLISECONDS));
+        
+        componentCallbackLatch = new Latch();
+        simulateConnectionFailure();
+        Thread.sleep(RECONNECTION_TIMEOUT);
+
+        send(super.scenarioNoTx);
+        assertTrue(componentCallbackLatch.await(LOCK_WAIT, TimeUnit.MILLISECONDS));
+	}
+	
+	private void simulateConnectionFailure() throws Exception
+	{
+        JmsConnector connector = (JmsConnector) muleContext.getRegistry().lookupConnector("jmsConnector");
+        connector.getConnection().getExceptionListener().onException(new JMSException("fake disconnect!"));
+	}
+}
