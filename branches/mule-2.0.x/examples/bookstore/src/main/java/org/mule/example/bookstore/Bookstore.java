386e918e2a6c02f7f38ba55900ac14ef16f1c8eb
dfeist <dfeist@bf997673-6b11-0410-b953-e057580c5b09>
2008-07-01 19:15:40 +0000

    Correct license headers (MULE-3503)
    git-svn-id: file:///home/projects/mule/scm@12216 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.0.x/examples/bookstore/src/main/java/org/mule/example/bookstore/Bookstore.java
+++ branches/mule-2.0.x/examples/bookstore/src/main/java/org/mule/example/bookstore/Bookstore.java
@@ -1,11 +1,11 @@
 /*
- * $Id: Bookstore.java 13295 2008-05-02 13:35:30Z marie.rizzo $
+ * $$Id$$
  * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
  *
- * (c) 2003-2008 MuleSource, Inc. This software is protected under international copyright
- * law. All use of this software is subject to MuleSource's Master Subscription Agreement
- * (or other master license agreement) separately entered into in writing between you and
- * MuleSource. If such an agreement is not in place, you may not use the software.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
  */
 
 package org.mule.example.bookstore;
