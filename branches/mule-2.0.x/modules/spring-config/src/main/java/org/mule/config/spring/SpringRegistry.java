a061c4bcaf4f3c24b3cab4e5b8ce39d408406a05
dfeist <dfeist@bf997673-6b11-0410-b953-e057580c5b09>
2008-07-10 21:16:52 +0000

    MULE-3539 Allow Spring ApplicationContext to be looked up via Mule Registry interface
    git-svn-id: file:///home/projects/mule/scm@12287 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.0.x/modules/spring-config/src/main/java/org/mule/config/spring/SpringRegistry.java
+++ branches/mule-2.0.x/modules/spring-config/src/main/java/org/mule/config/spring/SpringRegistry.java
@@ -47,6 +47,12 @@ public class SpringRegistry extends AbstractRegistry
 {
     public static final String REGISTRY_ID = "org.mule.Registry.Spring";
 
+    /**
+     * Key used to lookup Spring Application Context from SpringRegistry via Mule's
+     * Registry interface.
+     **/
+    public static final String SPRING_APPLICATION_CONTEXT = "springApplicationContext";
+    
     protected ConfigurableApplicationContext applicationContext;
 
     public SpringRegistry()
@@ -91,14 +97,21 @@ public class SpringRegistry extends AbstractRegistry
             return null;
         }
 
-        try
+        if (key.equals(SPRING_APPLICATION_CONTEXT) && applicationContext != null)
         {
-            return applicationContext.getBean(key);
+            return applicationContext;
         }
-        catch (NoSuchBeanDefinitionException e)
+        else
         {
-            logger.debug(e);
-            return null;
+            try
+            {
+                return applicationContext.getBean(key);
+            }
+            catch (NoSuchBeanDefinitionException e)
+            {
+                logger.debug(e);
+                return null;
+            }
         }
     }
 
