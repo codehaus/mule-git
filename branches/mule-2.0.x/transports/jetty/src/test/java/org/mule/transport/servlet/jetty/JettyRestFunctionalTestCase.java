4ff604b7fda96051e7191f914768e345c7cf2987
dandiep <dandiep@bf997673-6b11-0410-b953-e057580c5b09>
2008-07-11 17:54:29 +0000

    MULE-1821: no functional changes. Simply separating out jetty specific parts of the build so we can upgrade those to Jetty 6.
    git-svn-id: file:///home/projects/mule/scm@12297 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.0.x/transports/jetty/src/test/java/org/mule/transport/servlet/jetty/JettyRestFunctionalTestCase.java
+++ branches/mule-2.0.x/transports/jetty/src/test/java/org/mule/transport/servlet/jetty/JettyRestFunctionalTestCase.java
@@ -7,7 +7,7 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.transport.servlet.functional;
+package org.mule.transport.servlet.jetty;
 
 import org.mule.transport.http.functional.HttpFunctionalTestCase;
 
