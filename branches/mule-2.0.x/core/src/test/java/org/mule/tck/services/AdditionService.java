c585221e98f400825509d004ddbd8a1f2179b615
rossmason <rossmason@bf997673-6b11-0410-b953-e057580c5b09>
2008-07-13 02:16:22 +0000

    Final pattern example. Using bindings
    git-svn-id: file:///home/projects/mule/scm@12319 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.0.x/core/src/test/java/org/mule/tck/services/AdditionService.java
+++ branches/mule-2.0.x/core/src/test/java/org/mule/tck/services/AdditionService.java
@@ -7,26 +7,12 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-
 package org.mule.tck.services;
 
-import java.rmi.Remote;
-
-public class SimpleMathsComponent implements Remote, AdditionService
+/**
+ * Adds an array of numbers together
+ */
+public interface AdditionService
 {
-
-    public Integer addTen(Integer number)
-    {
-        return new Integer(number.intValue() + 10);
-    }
-
-    public int add(int[] args)
-    {
-        int result = 0;
-        for (int i = 0; i < args.length; i++)
-        {
-            result += args[i];
-        }
-        return result;
-    }
+    public int add(int[] args);
 }
