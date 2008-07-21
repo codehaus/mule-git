94841209ff874c998100ebc5eb1d2a965f250780
jwheeler <jwheeler@bf997673-6b11-0410-b953-e057580c5b09>
2008-07-21 16:37:26 +0000

    Creating a branch for the schema documentation. All future CE schema annotations should go in this branch only, to be merged into the 2.0.x dev branch after.
    git-svn-id: file:///home/projects/mule/scm@12387 bf997673-6b11-0410-b953-e057580c5b09

---
+++ branches/mule-2.0.x-schema-annotations/examples/loanbroker/esn/src/test/java/org/mule/example/loanbroker/esn/CxfLoanBrokerSynchronousFunctionalTestCase.java
@@ -0,0 +1,30 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.example.loanbroker.esn;
+
+import org.mule.example.loanbroker.tests.AbstractLoanBrokerTestCase;
+
+public class CxfLoanBrokerSynchronousFunctionalTestCase extends AbstractLoanBrokerTestCase
+{
+    
+    @Override
+    public void testSingleLoanRequest() throws Exception
+    {
+        // TODO Auto-generated method stub
+        super.testSingleLoanRequest();
+    }
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "loan-broker-sync-config.xml, loan-broker-cxf-endpoints-config.xml";
+    }
+}
