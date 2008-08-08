0ae9d60e0f3d340236904b2c169b4380162170dd
dirk.olmes <dirk.olmes@bf997673-6b11-0410-b953-e057580c5b09>
2008-08-08 14:43:34 +0000

    configure the reconnection strategy from spring, not from code
    git-svn-id: file:///home/projects/mule/scm@12476 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.0.x/transports/jms/src/test/java/org/mule/transport/jms/issues/ConnectWithThreadingTestCase.java
+++ branches/mule-2.0.x/transports/jms/src/test/java/org/mule/transport/jms/issues/ConnectWithThreadingTestCase.java
@@ -12,11 +12,8 @@ package org.mule.transport.jms.issues;
 
 import org.mule.DefaultMuleMessage;
 import org.mule.api.MuleMessage;
-import org.mule.api.transport.Connector;
 import org.mule.module.client.MuleClient;
 import org.mule.tck.FunctionalTestCase;
-import org.mule.transport.AbstractConnector;
-import org.mule.transport.SimpleRetryConnectionStrategy;
 
 public class ConnectWithThreadingTestCase extends FunctionalTestCase
 {
@@ -26,21 +23,6 @@ public class ConnectWithThreadingTestCase extends FunctionalTestCase
         return "jms_connect_with_threading.xml";
     }
     
-    protected void doSetUp() throws Exception
-    {
-        super.doSetUp();
-
-        SimpleRetryConnectionStrategy connectionStrategy = new SimpleRetryConnectionStrategy();
-        connectionStrategy.setRetryCount(-1);
-        connectionStrategy.setRetryFrequency(1000);
-        connectionStrategy.setDoThreading(true);
-        
-        // we have to install the reconnection strategy here as there's currently no way to 
-        // do it via config in 2.0
-        Connector connector = muleContext.getRegistry().lookupConnector("jmsConnector");
-        ((AbstractConnector) connector).setConnectionStrategy(connectionStrategy);
-    }
-
     public void testConnectWithThreading() throws Exception 
     { 
         MuleClient client = new MuleClient(); 
