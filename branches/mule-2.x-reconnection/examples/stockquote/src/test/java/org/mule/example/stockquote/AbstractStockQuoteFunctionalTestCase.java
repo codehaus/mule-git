6a63de35037edee7805d54b4f73879241fe7e48f
tcarlson <tcarlson@bf997673-6b11-0410-b953-e057580c5b09>
2008-08-22 03:30:19 +0000

    Create feature branch for work on reconnection strategies
    git-svn-id: file:///home/projects/mule/scm@12545 bf997673-6b11-0410-b953-e057580c5b09

---
+++ branches/mule-2.x-reconnection/examples/stockquote/src/test/java/org/mule/example/stockquote/AbstractStockQuoteFunctionalTestCase.java
@@ -0,0 +1,56 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.example.stockquote;
+
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
+import org.mule.util.StringUtils;
+
+import java.util.Locale;
+
+public abstract class AbstractStockQuoteFunctionalTestCase extends FunctionalTestCase
+{
+
+    public void testStockQuoteExample() throws Exception
+    {
+        MuleClient client = new MuleClient();
+        MuleMessage response = client.send("vm://stockquote", "CSCO", null);
+    
+        if (null == response)
+        { 
+            fail("No response message.");
+        }
+        else
+        {
+            if (null == response.getExceptionPayload())
+            {
+                String text = response.getPayloadAsString();
+                assertNotNull("Null response", text);
+                assertTrue("Stock quote should contain \"CISCO\": " + text, StringUtils.contains(text, "CISCO"));
+                if (Locale.getDefault().getISO3Language().equalsIgnoreCase("eng"))
+                {
+                    // the stockquote message is localized ...
+                    assertTrue("Stock quote should start with \"StockQuote[\":" + text, text.startsWith("StockQuote["));
+                }
+                logger.debug("**********");
+                logger.debug(response.getPayload());
+                logger.debug(response.getPayloadAsString());
+                logger.debug("**********");
+            }
+            else
+            {
+                fail("Exception occurred: " + response.getExceptionPayload());
+            }
+        }
+    }
+
+}
