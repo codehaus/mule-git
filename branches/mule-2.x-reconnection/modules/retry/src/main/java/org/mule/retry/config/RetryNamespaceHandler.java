5b14b9b537f5116efcc61b527ef96dbc4b8398b6
tcarlson <tcarlson@bf997673-6b11-0410-b953-e057580c5b09>
2008-08-29 12:10:46 +0000

    Start on EE-743 Schema/namespacehandler etc. for retry policies
    git-svn-id: file:///home/projects/mule/scm@12590 bf997673-6b11-0410-b953-e057580c5b09

---
+++ branches/mule-2.x-reconnection/modules/retry/src/main/java/org/mule/retry/config/RetryNamespaceHandler.java
@@ -0,0 +1,20 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ *
+ * (c) 2003-2008 MuleSource, Inc. This software is protected under international copyright
+ * law. All use of this software is subject to MuleSource's Master Subscription Agreement
+ * (or other master license agreement) separately entered into in writing between you and
+ * MuleSource. If such an agreement is not in place, you may not use the software.
+ */
+package org.mule.retry.config;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+public class RetryNamespaceHandler extends NamespaceHandlerSupport
+{
+    public void init()
+    {
+        // TODO
+    }
+}
---
+++ branches/mule-2.x-reconnection/modules/retry/src/test/java/org/mule/retry/config/RetryNamespaceHandlerTestCase.java
@@ -0,0 +1,27 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ *
+ * (c) 2003-2008 MuleSource, Inc. This software is protected under international copyright
+ * law. All use of this software is subject to MuleSource's Master Subscription Agreement
+ * (or other master license agreement) separately entered into in writing between you and
+ * MuleSource. If such an agreement is not in place, you may not use the software.
+ */
+
+package org.mule.retry.config;
+
+import org.mule.tck.FunctionalTestCase;
+
+public class RetryNamespaceHandlerTestCase extends FunctionalTestCase
+{
+    protected String getConfigResources()
+    {
+        return "retry-namespace-config.xml";
+    }
+
+    public void testConfig() throws Exception
+    {
+        // TODO
+    }
+
+}
