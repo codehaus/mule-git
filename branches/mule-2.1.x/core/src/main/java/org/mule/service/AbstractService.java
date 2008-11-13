4713d1932709032aed1cd2113ccf692041a7931d
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2008-11-13 15:35:28 +0000

    Merged revisions 13295 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.x
    ........
      r13295 | aperepel | 2008-11-12 10:20:27 -0500 (Wed, 12 Nov 2008) | 1 line
      
      MULE-3941 pass-through-router still processes message when Component with String return type returns null
    ........
    
    
    git-svn-id: file:///home/projects/mule/scm@13311 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.1.x/core/src/main/java/org/mule/service/AbstractService.java
+++ branches/mule-2.1.x/core/src/main/java/org/mule/service/AbstractService.java
@@ -44,6 +44,7 @@ import org.mule.routing.outbound.DefaultOutboundRouterCollection;
 import org.mule.routing.outbound.OutboundPassThroughRouter;
 import org.mule.routing.response.DefaultResponseRouterCollection;
 import org.mule.transport.AbstractConnector;
+import org.mule.transport.NullPayload;
 import org.mule.util.concurrent.WaitableBoolean;
 
 import java.beans.ExceptionListener;
@@ -892,7 +893,7 @@ public abstract class AbstractService implements Service
         {
             logger.debug("MuleEvent stop further processing has been set, no outbound routing will be performed.");
         }
-        if (result != null && !event.isStopFurtherProcessing())
+        if (result != null && !event.isStopFurtherProcessing() && !(result.getPayload() instanceof NullPayload))
         {
             if (getOutboundRouter().hasEndpoints())
             {
