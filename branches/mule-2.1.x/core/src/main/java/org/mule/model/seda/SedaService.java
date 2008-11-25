65bb507612c49d6b61aaa0c5902c4848c1d0580e
dfeist <dfeist@bf997673-6b11-0410-b953-e057580c5b09>
2008-11-25 15:11:06 +0000

    Rollback r13417
    git-svn-id: file:///home/projects/mule/scm@13418 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.1.x/core/src/main/java/org/mule/model/seda/SedaService.java
+++ branches/mule-2.1.x/core/src/main/java/org/mule/model/seda/SedaService.java
@@ -103,7 +103,7 @@ public class SedaService extends AbstractService implements Work, WorkListener
         // Create thread pool
         // (Add one to maximum number of active threads to account for the service
         // work item that is running continuously and polling the SEDA queue.)
-        threadingProfile = new ChainedThreadingProfile(this.threadingProfile);
+        ChainedThreadingProfile threadingProfile = new ChainedThreadingProfile(this.threadingProfile);
         threadingProfile.setMaxThreadsActive(threadingProfile.getMaxThreadsActive() + 1);
         workManager = threadingProfile.createWorkManager(getName());
 
@@ -335,15 +335,7 @@ public class SedaService extends AbstractService implements Work, WorkListener
                         logger.debug("Service: " + name + " dequeued event on: "
                                         + event.getEndpoint().getEndpointURI());
                     }
-                    Work work = new ComponentStageWorker(event);
-                    if (threadingProfile.isDoThreading())
-                    {
-                        workManager.scheduleWork(work, WorkManager.INDEFINITE, null, this);
-                    }
-                    else
-                    {
-                        work.run();
-                    }
+                    workManager.scheduleWork(new ComponentStageWorker(event), WorkManager.INDEFINITE, null, this);
                 }
             }
             catch (Exception e)
