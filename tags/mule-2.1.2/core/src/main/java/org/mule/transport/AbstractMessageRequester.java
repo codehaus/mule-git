3524657d1f86b8986a301838585aa057f7c19b55
dfeist <dfeist@bf997673-6b11-0410-b953-e057580c5b09>
2008-12-05 00:09:53 +0000

    Tag 2.1.2 release
    git-svn-id: file:///home/projects/mule/scm@13559 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tags/mule-2.1.2/core/src/main/java/org/mule/transport/AbstractMessageRequester.java
@@ -0,0 +1,130 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport;
+
+import org.mule.api.MuleException;
+import org.mule.api.MuleMessage;
+import org.mule.api.context.WorkManager;
+import org.mule.api.endpoint.InboundEndpoint;
+import org.mule.api.lifecycle.InitialisationException;
+import org.mule.api.transport.MessageRequester;
+import org.mule.api.transport.ReceiveException;
+import org.mule.context.notification.EndpointMessageNotification;
+
+/**
+ * The Message Requester is used to explicitly request messages from a message channel or
+ * resource rather than subscribing to inbound events or polling for messages.
+ * This is often used programatically but will not be used for inbound endpoints
+ * configured on services.
+ */
+public abstract class AbstractMessageRequester extends AbstractConnectable implements MessageRequester
+{
+    public AbstractMessageRequester(InboundEndpoint endpoint)
+    {
+        super(endpoint);
+    }
+
+    //@Override
+    public final void initialise() throws InitialisationException
+    {
+        super.initialise();
+        doInitialise();
+    }
+
+    //@Override
+    public final synchronized void dispose()
+    {
+        super.dispose();
+        try
+        {
+            disposing.set(true);
+            doDispose();
+        }
+        finally
+        {
+            disposed.set(true);
+        }
+    }
+
+    /**
+     * Make a specific request to the underlying transport
+     *
+     * @param timeout the maximum time the operation should block before returning.
+     *            The call should return immediately if there is data available. If
+     *            no data becomes available before the timeout elapses, null will be
+     *            returned
+     * @return the result of the request wrapped in a MuleMessage object. Null will be
+     *         returned if no data was available
+     * @throws Exception if the call to the underlying protocol causes an exception
+     */
+    public final MuleMessage request(long timeout) throws Exception
+    {
+        try
+        {
+            // Make sure we are connected
+            connect();
+            MuleMessage result = null;
+            try
+            {
+                result = doRequest(timeout);
+            }
+            finally
+            {
+                if (useStrictConnectDisconnect) disconnect();
+            }
+
+            if (result != null && connector.isEnableMessageEvents())
+            {
+                connector.fireNotification(new EndpointMessageNotification(result, endpoint, null,
+                    EndpointMessageNotification.MESSAGE_REQUESTED));
+            }
+            return result;
+        }
+        catch (ReceiveException e)
+        {
+            disposeAndLogException();
+            throw e;
+        }
+        catch (Exception e)
+        {
+            disposeAndLogException();
+            throw new ReceiveException(endpoint, timeout, e);
+        }
+    }
+
+    //@Override
+    protected WorkManager getWorkManager()
+    {
+        try
+        {
+            return connector.getRequesterWorkManager();
+        }
+        catch (MuleException e)
+        {
+            handleException(e);
+            return null;
+        }
+    }
+    
+    /**
+     * Make a specific request to the underlying transport
+     *
+     * @param timeout the maximum time the operation should block before returning.
+     *            The call should return immediately if there is data available. If
+     *            no data becomes available before the timeout elapses, null will be
+     *            returned
+     * @return the result of the request wrapped in a MuleMessage object. Null will be
+     *         returned if no data was avaialable
+     * @throws Exception if the call to the underlying protocal cuases an exception
+     */
+    protected abstract MuleMessage doRequest(long timeout) throws Exception;
+
+}
