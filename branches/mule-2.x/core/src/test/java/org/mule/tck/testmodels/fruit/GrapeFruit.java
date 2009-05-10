0978532aeb8374a9171d8a6695c3e8af92aa0604
rossmason <rossmason@bf997673-6b11-0410-b953-e057580c5b09>
2009-05-10 09:07:19 +0000

    Handle is* as well as get* bean methods
    git-svn-id: file:///home/projects/mule/scm@14685 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.x/core/src/test/java/org/mule/tck/testmodels/fruit/GrapeFruit.java
+++ branches/mule-2.x/core/src/test/java/org/mule/tck/testmodels/fruit/GrapeFruit.java
@@ -17,17 +17,19 @@ public class GrapeFruit implements Fruit
     private Integer segments = new Integer(10);
     private Double radius = new Double(4.34);
     private String brand = "Pirulo";
+    private boolean red = false;
     private boolean bitten = false;
 
     public GrapeFruit()
     {
     }
 
-    public GrapeFruit(Integer segments, Double radius, String brand)
+    public GrapeFruit(Integer segments, Double radius, String brand, boolean red)
     {
         this.segments = segments;
         this.radius = radius;
         this.brand = brand;
+        this.red = red;
     }
 
     /**
@@ -78,6 +80,15 @@ public class GrapeFruit implements Fruit
         radius = double1;
     }
 
+    public boolean isRed()
+    {
+        return red;
+    }
+
+    public void setRed(boolean red)
+    {
+        this.red = red;
+    }
 
     public void bite()
     {
@@ -103,7 +114,7 @@ public class GrapeFruit implements Fruit
 
         GrapeFruit that = (GrapeFruit) o;
 
-        if (bitten != that.bitten)
+        if (red != that.red)
         {
             return false;
         }
@@ -129,6 +140,7 @@ public class GrapeFruit implements Fruit
         int result = segments != null ? segments.hashCode() : 0;
         result = 31 * result + (radius != null ? radius.hashCode() : 0);
         result = 31 * result + (brand != null ? brand.hashCode() : 0);
+        result = 31 * result + (red ? 1 : 0);
         result = 31 * result + (bitten ? 1 : 0);
         return result;
     }
