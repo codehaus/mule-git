98d4360bea0e1ce605fd8753ba8523202ba917ae
dzapata <dzapata@bf997673-6b11-0410-b953-e057580c5b09>
2009-06-19 17:11:55 +0000

    creating 2.2.1-HF2 branch from 2.2.1 tag @ 14761
    git-svn-id: file:///home/projects/mule/scm@15031 bf997673-6b11-0410-b953-e057580c5b09

---
+++ tags/mule-2.2.1-HF2/tests/integration/src/test/java/org/mule/test/integration/transaction/XAResourceManagerTestCase.java
@@ -0,0 +1,99 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.test.integration.transaction;
+
+import org.mule.module.jboss.transaction.JBossArjunaTransactionManagerFactory;
+import org.mule.tck.AbstractMuleTestCase;
+import org.mule.util.xa.AbstractTransactionContext;
+import org.mule.util.xa.AbstractXAResourceManager;
+import org.mule.util.xa.DefaultXASession;
+import org.mule.util.xa.ResourceManagerException;
+
+import javax.transaction.Transaction;
+import javax.transaction.TransactionManager;
+import javax.transaction.xa.XAResource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class XAResourceManagerTestCase extends AbstractMuleTestCase
+{
+    private TransactionManager tm;
+
+    protected void doSetUp() throws Exception
+    {
+        tm = new JBossArjunaTransactionManagerFactory().create();
+    }
+
+    protected void doTearDown() throws Exception
+    {
+        tm = null;
+    }
+
+    public void testTxBehaviour() throws Exception
+    {
+        TestXAResourceManager rm = new TestXAResourceManager();
+        rm.start();
+        DefaultXASession s = rm.createSession();
+
+        tm.begin();
+        Transaction tx = tm.getTransaction();
+        tx.enlistResource(s);
+
+        tx.delistResource(s, XAResource.TMSUCCESS);
+        tx.commit();
+
+    }
+
+    protected static class TestXAResourceManager extends AbstractXAResourceManager
+    {
+
+        private static Log logger = LogFactory.getLog(TestXAResourceManager.class);
+
+        public DefaultXASession createSession()
+        {
+            return new DefaultXASession(this);
+        }
+
+        protected Log getLogger()
+        {
+            return logger;
+        }
+
+        protected AbstractTransactionContext createTransactionContext(Object session)
+        {
+            return new AbstractTransactionContext() 
+            {
+            };
+        }
+
+        protected void doBegin(AbstractTransactionContext context)
+        {
+            // template method
+        }
+
+        protected int doPrepare(AbstractTransactionContext context)
+        {
+            // template method
+            return 0;
+        }
+
+        protected void doCommit(AbstractTransactionContext context) throws ResourceManagerException
+        {
+            // template method
+        }
+
+        protected void doRollback(AbstractTransactionContext context) throws ResourceManagerException
+        {
+            // template method
+        }
+    }
+}
