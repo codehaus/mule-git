a08fe17bce4d175b11d00dd01ac253770002dd12
dirk.olmes <dirk.olmes@bf997673-6b11-0410-b953-e057580c5b09>
2009-07-08 09:10:13 +0000

    added proper license header
    git-svn-id: file:///home/projects/mule/scm@15191 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.1.x/modules/management/src/test/java/org/mule/management/JmxStatisticsAsyncTestCase.java
+++ branches/mule-2.1.x/modules/management/src/test/java/org/mule/management/JmxStatisticsAsyncTestCase.java
@@ -1,8 +1,18 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.management;
 
 import org.mule.management.stats.ServiceStatistics;
-import org.mule.tck.FunctionalTestCase;
 import org.mule.module.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
 
 public class JmxStatisticsAsyncTestCase extends FunctionalTestCase
 {
