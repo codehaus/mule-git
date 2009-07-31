8055b473f656a2ee8993dea07f1d8f2b3a79752e
dfeist <dfeist@bf997673-6b11-0410-b953-e057580c5b09>
2009-07-31 15:38:04 +0000

    MULE-4456 Failing test case for this issue
    git-svn-id: file:///home/projects/mule/scm@15318 bf997673-6b11-0410-b953-e057580c5b09

---
+++ branches/mule-2.2.x/transports/http/src/test/java/org/mule/transport/http/issues/HttpMessageReceiverMule4456TestCase.java
@@ -0,0 +1,73 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport.http.issues;
+
+import org.mule.api.MuleEventContext;
+import org.mule.api.MuleMessage;
+import org.mule.module.client.MuleClient;
+import org.mule.tck.FunctionalTestCase;
+import org.mule.tck.functional.EventCallback;
+import org.mule.tck.functional.FunctionalTestComponent;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpVersion;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.params.HttpClientParams;
+
+public class HttpMessageReceiverMule4456TestCase extends FunctionalTestCase
+{
+    private static final String URL = "http://localhost:8000";
+    private static final String MESSAGE = "test message";
+
+    private HttpClient http11Client;
+    private MuleClient muleClient;
+
+    @Override
+    protected boolean isGracefulShutdown()
+    {
+        return true;
+    }
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        super.doSetUp();
+        HttpClientParams params = new HttpClientParams();
+        params.setVersion(HttpVersion.HTTP_1_1);
+        http11Client = new HttpClient(params);
+        muleClient = new MuleClient();
+    }
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "http-receiver-mule4456-config.xml";
+    }
+
+    public void testAsyncPost() throws Exception
+    {
+        ((FunctionalTestComponent) getComponent("AsyncService")).setEventCallback(new EventCallback()
+        {
+
+            public void eventReceived(MuleEventContext context, Object component) throws Exception
+            {
+                Thread.sleep(200);
+                context.getMessageAsString();
+            }
+        });
+        PostMethod request = new PostMethod(URL);
+        request.setRequestBody(MESSAGE);
+        http11Client.executeMethod(request);
+        MuleMessage message = muleClient.request("vm:///tmp/mule/out", 1000);
+        assertNotNull(message);
+        assertEquals(MESSAGE + "2", message.getPayloadAsString());
+    }
+}
