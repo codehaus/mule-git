fc2eefea67c65ba94375fcab65a307b2b973d3f6
dirk.olmes <dirk.olmes@bf997673-6b11-0410-b953-e057580c5b09>
2009-08-05 11:06:06 +0000

    Merged revisions 15359 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.2.x
    ........
      r15359 | dirk.olmes | 2009-08-05 12:48:35 +0200 (Wed, 05 Aug 2009) | 1 line
      
      MULE-4459 (Once a HttpMessageAdaptor with an InputStream payload is serialized it's payload has an incorrect value (empty string))
    ........
    
    
    git-svn-id: file:///home/projects/mule/scm@15360 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.2.x-MULE-4460/transports/http/src/test/java/org/mule/transport/http/HttpMessageAdapterTestCase.java
+++ branches/mule-2.2.x-MULE-4460/transports/http/src/test/java/org/mule/transport/http/HttpMessageAdapterTestCase.java
@@ -10,9 +10,15 @@
 
 package org.mule.transport.http;
 
+import org.mule.DefaultMuleMessage;
 import org.mule.api.MessagingException;
+import org.mule.api.MuleMessage;
 import org.mule.api.transport.MessageAdapter;
 import org.mule.transport.AbstractMessageAdapterTestCase;
+
+import java.io.ByteArrayInputStream;
+
+import org.apache.commons.lang.SerializationUtils;
 
 public class HttpMessageAdapterTestCase extends AbstractMessageAdapterTestCase
 {
@@ -28,5 +34,43 @@ public class HttpMessageAdapterTestCase extends AbstractMessageAdapterTestCase
     {
         return new HttpMessageAdapter(payload);
     }
-    
+ 
+    /**
+     * Ensure that if the HttpMessageAdaptor is serialized the payload can still
+     * be read from the original instance that was used for serialization. Also
+     * ensure that when deserializing the payload is the same.
+     */
+    public void testStringPayloadSerialization() throws Exception 
+    {
+        String payload = "TEST";
+        MuleMessage message = new DefaultMuleMessage(new HttpMessageAdapter(payload));
+        
+        byte[] serialized = SerializationUtils.serialize(message);
+        
+        MuleMessage deserializedMessage = (MuleMessage) SerializationUtils.deserialize(serialized);
+        
+        assertEquals(payload, deserializedMessage.getPayloadAsString());
+        assertEquals(payload, message.getPayloadAsString());
+    }
+
+    /**
+     * Ensure that if the HttpMessageAdaptor is serialized the stream payload
+     * can still be read from the original instance that was used for
+     * serialization. Also ensure that when deserializing the payload is the same.
+     * This reproduces MULE-4459.
+     */
+    public void testStreamPayloadSerialization() throws Exception 
+    {
+        String payload = "TEST";
+        MuleMessage message = new DefaultMuleMessage(new HttpMessageAdapter(
+            new ByteArrayInputStream(payload.getBytes())));
+        
+        byte[] serialized = SerializationUtils.serialize(message);
+
+        MuleMessage deserializedMessage = (MuleMessage) SerializationUtils.deserialize(serialized);
+        
+        assertEquals(payload, deserializedMessage.getPayloadAsString());
+        assertEquals(payload, message.getPayloadAsString());
+    }
+
 }
