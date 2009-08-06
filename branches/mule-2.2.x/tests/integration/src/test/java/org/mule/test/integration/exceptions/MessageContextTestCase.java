408877b06f2083cbb0543e49a246a2f94bdf90f4
dirk.olmes <dirk.olmes@bf997673-6b11-0410-b953-e057580c5b09>
2009-08-06 01:53:32 +0000

    checkstyle, license header
    git-svn-id: file:///home/projects/mule/scm@15368 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.2.x/tests/integration/src/test/java/org/mule/test/integration/exceptions/MessageContextTestCase.java
+++ branches/mule-2.2.x/tests/integration/src/test/java/org/mule/test/integration/exceptions/MessageContextTestCase.java
@@ -1,37 +1,42 @@
-package org.mule.test.integration.exceptions;
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 
-import java.util.Map;
+package org.mule.test.integration.exceptions;
 
 import org.mule.DefaultMuleMessage;
 import org.mule.api.MuleMessage;
 import org.mule.module.client.MuleClient;
 import org.mule.tck.FunctionalTestCase;
 
-public class MessageContextTestCase extends FunctionalTestCase {
+import java.util.Map;
+
+public class MessageContextTestCase extends FunctionalTestCase 
+{
 
 	String request = "Hello World";
 
-	protected String getConfigResources() {
+	protected String getConfigResources() 
+	{
 		return "org/mule/test/integration/exceptions/message-context-test.xml";
 	}
 
 	/**
 	 * Test for MULE-4361
-	 * @throws Exception
 	 */
-	public void testAlternateExceptionStrategy() throws Exception {
-
-		try {
-			MuleClient client1 = new MuleClient();
-			DefaultMuleMessage msg1 = new DefaultMuleMessage(request,
-					(Map) null);
-			MuleMessage response1 = client1.send("testin", msg1, 200000);
-			assertNotNull(response1);
-			Thread.sleep(10000); // Wait for test to finish
-		} catch (Throwable t) {
-			System.out.println(t.getMessage());
-			t.printStackTrace();
-		}
+	public void testAlternateExceptionStrategy() throws Exception 
+	{
+		MuleClient client1 = new MuleClient();
+		DefaultMuleMessage msg1 = new DefaultMuleMessage(request, (Map) null);
+		MuleMessage response1 = client1.send("testin", msg1, 200000);
+		assertNotNull(response1);
+		Thread.sleep(10000); // Wait for test to finish
 	}
 
 }
