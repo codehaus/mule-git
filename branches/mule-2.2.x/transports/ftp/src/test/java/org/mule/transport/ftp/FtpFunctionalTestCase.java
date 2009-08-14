1aadc245df00c0fbc5a59f818be437a926e01c05
dirk.olmes <dirk.olmes@bf997673-6b11-0410-b953-e057580c5b09>
2009-08-14 20:07:03 +0000

    Merged revisions 15457 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.1.x
    ........
      r15457 | dirk.olmes | 2009-08-14 22:03:03 +0200 (Fri, 14 Aug 2009) | 2 lines
      
      refactor FtpMessageRequester for easier code and subclassability
    ........
    
    
    git-svn-id: file:///home/projects/mule/scm@15458 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.2.x/transports/ftp/src/test/java/org/mule/transport/ftp/FtpFunctionalTestCase.java
+++ branches/mule-2.2.x/transports/ftp/src/test/java/org/mule/transport/ftp/FtpFunctionalTestCase.java
@@ -37,6 +37,14 @@ public class FtpFunctionalTestCase extends AbstractFtpServerTestCase
         return "ftp-functional-test.xml";
     }
         
+    /**
+     * Used by subclasses (in EE)
+     */
+    public int getPort()
+    {
+        return PORT;
+    }
+    
     public void testSendAndRequest() throws Exception
     {
         final CountDownLatch latch = new CountDownLatch(1);
