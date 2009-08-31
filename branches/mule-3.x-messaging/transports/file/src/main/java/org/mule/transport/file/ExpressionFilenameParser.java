a88f4e4dddb23557273699e98484943f410b043e
dirk.olmes <dirk.olmes@bf997673-6b11-0410-b953-e057580c5b09>
2009-08-31 07:04:20 +0000

    Merged revisions 15536-15538 via svnmerge from https://svn.codehaus.org/mule/branches/mule-3.x
    ........
      r15536 | dirk.olmes | 2009-08-27 12:17:51 +0200 (Thu, 27 Aug 2009) | 2 lines
      
      MULE-4479 (SimpleFilenameParser is deprecated, replace all uses with ExpressionFilenameParser)
    ........
      r15537 | dirk.olmes | 2009-08-27 12:58:02 +0200 (Thu, 27 Aug 2009) | 2 lines
      
      MULE-4479 (SimpleFilenameParser is deprecated, replace all uses with ExpressionFilenameParser): update Javadocs
    ........
      r15538 | dirk.olmes | 2009-08-27 14:14:24 +0200 (Thu, 27 Aug 2009) | 3 lines
      
      fix some leftovers from MULE-4479 (SimpleFilenameParser is deprecated, replace all uses with ExpressionFilenameParser)
    ........
    
    
    git-svn-id: file:///home/projects/mule/scm@15545 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-3.x-messaging/transports/file/src/main/java/org/mule/transport/file/ExpressionFilenameParser.java
+++ branches/mule-3.x-messaging/transports/file/src/main/java/org/mule/transport/file/ExpressionFilenameParser.java
@@ -21,28 +21,29 @@ import java.text.MessageFormat;
 
 /**
  * <code>ExpressionFilenameParser</code> can use any expression language supported by Mule
- * to construct a file name for the current message.  Expressions can be xpath, xquery, ognl, mvel, header, function
- * and more. For more information see http://muledocs.org/v2/Expressions.
+ * to construct a file name for the current message.  Expressions can be xpath, xquery, ognl, mvel, 
+ * header, function and more. For more information see http://muledocs.org/v2/Expressions.
  * <p/>
- * For example an xpath expression can be defined to pull a message id out of an xml message and use that as the file name -
+ * For example an xpath expression can be defined to pull a message id out of an xml message and 
+ * use that as the file name -
  * <code>
  * #[xpath:/message/header/@id]
  * </code>
  * <p/>
- * This parser superseeds the {@link org.mule.transport.file.SimpleFilenameParser} which has been kept in Mule 2 for
- * compatibility. The following demonstrates how to achieve the same results when using the <code>ExpressionFilenameParser</code>
- * over the {@link org.mule.transport.file.SimpleFilenameParser}.
+ * This parser superseeds the (now removed) <code>org.mule.transport.file.SimpleFilenameParser</code>
+ * which has been kept in Mule 2 for compatibility. The following demonstrates how to achieve the 
+ * same results when using the <code>ExpressionFilenameParser</code> over the 
+ * <code>SimpleFilenameParser</code>
  * <ul>
  * <li>#[DATE] : #[function:datestamp]</li>
  * <li>#[DATE:yy-MM-dd] : #[function:datestamp(yy-MM-dd)]</li>
  * <li>#[SYSTIME] : #[function:systime]</li>
  * <li>#[UUID] : #[function:uuid]</li>
  * <li>#[ORIGINALNAME] : #[header:originalFilename]</li>
- * <li>#[COUNT] : #[function:counter] - note that this is a global counter. If you want a local counter per file connector then you should use the {@link org.mule.transport.file.SimpleFilenameParser}.</li>
- * <li>#[&lt;Message Property Name>] : #[header:&lt;Message Property Name>]</li>
+ * <li>#[COUNT] : #[function:count] - note that this is a global counter.</li>
+ * <li>#[&lt;Message Property Name&gt;] : #[header:&lt;Message Property Name&gt;]</li>
  * </ul>
  */
-
 public class ExpressionFilenameParser implements FilenameParser, MuleContextAware
 {
     public static final String DEFAULT_DATE_FORMAT = "dd-MM-yy_HH-mm-ss.SSS";
