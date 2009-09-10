b0e6c056b31bfd210ffd1434495f83aaf99baac1
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2009-09-10 16:10:38 +0000

    Bad, bad source file, you broke the deployment build on license header check. Tabs -> spaces as well, damn you eclipse people! :)
    git-svn-id: file:///home/projects/mule/scm@15674 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.2.x/transports/http/src/test/java/org/mule/transport/http/functional/PartialReadComponent.java
+++ branches/mule-2.2.x/transports/http/src/test/java/org/mule/transport/http/functional/PartialReadComponent.java
@@ -1,3 +1,13 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.transport.http.functional;
 
 import org.mule.api.MuleEventContext;
@@ -10,11 +20,11 @@ import java.io.InputStream;
  */
 public class PartialReadComponent implements Callable {
 
-	public Object onCall(MuleEventContext eventContext) throws Exception {
-		InputStream stream = (InputStream) eventContext.getMessage().getPayload(InputStream.class);
-		
-		stream.read();
-		return "Hello";
-	}
+    public Object onCall(MuleEventContext eventContext) throws Exception {
+        InputStream stream = (InputStream) eventContext.getMessage().getPayload(InputStream.class);
+        
+        stream.read();
+        return "Hello";
+    }
 
 }
