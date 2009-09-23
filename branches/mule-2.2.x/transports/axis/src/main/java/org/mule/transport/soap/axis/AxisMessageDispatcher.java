0805aecb664bfa0c3ecd9a3b62bbf33c0dea8efd
dfeist <dfeist@bf997673-6b11-0410-b953-e057580c5b09>
2009-09-23 00:35:47 +0000

    MULE-4520 Dispatchers doSend() methods should return message with NullPayload rather than null
    git-svn-id: file:///home/projects/mule/scm@15756 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.2.x/transports/axis/src/main/java/org/mule/transport/soap/axis/AxisMessageDispatcher.java
+++ branches/mule-2.2.x/transports/axis/src/main/java/org/mule/transport/soap/axis/AxisMessageDispatcher.java
@@ -141,7 +141,7 @@ public class AxisMessageDispatcher extends AbstractMessageDispatcher
         result = call.invoke(args);
         if (result == null)
         {
-            return null;
+            return new DefaultMuleMessage(NullPayload.getInstance());
         }
         else
         {
