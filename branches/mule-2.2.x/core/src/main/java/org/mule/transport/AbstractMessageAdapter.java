c3a8c89d35c86aa3e02bd11c49cac40926f25752
tcarlson <tcarlson@bf997673-6b11-0410-b953-e057580c5b09>
2009-10-26 20:01:55 +0000

    MULE-4431 Session scoped properties get lost
    git-svn-id: file:///home/projects/mule/scm@15879 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.2.x/core/src/main/java/org/mule/transport/AbstractMessageAdapter.java
+++ branches/mule-2.2.x/core/src/main/java/org/mule/transport/AbstractMessageAdapter.java
@@ -136,7 +136,7 @@ public abstract class AbstractMessageAdapter implements MessageAdapter, ThreadSa
     }
 
     /**
-     * Returns a copy of the {@link org.mule.transport.MessagePropertiesContext} object.  This is usful when copying messages
+     * Returns a copy of the {@link org.mule.transport.MessagePropertiesContext} object.  This is useful when copying messages
      * rather than using the Messaging API directly. This provides a faster method of copying message properties.
      * @return a copy of the properties on this message
      */
@@ -173,18 +173,6 @@ public abstract class AbstractMessageAdapter implements MessageAdapter, ThreadSa
     public void addProperties(Map props)
     {
         addProperties(props, properties.getDefaultScope());
-        assertAccess(WRITE);
-        if (props != null)
-        {
-            synchronized (props)
-            {
-                for (Iterator iter = props.entrySet().iterator(); iter.hasNext();)
-                {
-                    Map.Entry entry = (Map.Entry) iter.next();
-                    setProperty((String) entry.getKey(), entry.getValue());
-                }
-            }
-        }
     }
 
     /** {@inheritDoc} */
@@ -228,6 +216,13 @@ public abstract class AbstractMessageAdapter implements MessageAdapter, ThreadSa
     {
         assertAccess(WRITE);
         return properties.removeProperty(key);
+    }
+
+    /** {@inheritDoc} */
+    public Object removeProperty(String key, PropertyScope scope)
+    {
+        assertAccess(WRITE);
+        return properties.removeProperty(key, scope);
     }
 
     /** {@inheritDoc} */
@@ -580,11 +575,11 @@ public abstract class AbstractMessageAdapter implements MessageAdapter, ThreadSa
         //
         // Having said that, you can disable these exceptions by defining
         // MuleProperties.MULE_THREAD_UNSAFE_MESSAGES_PROPERTY (mule.disable.threadsafemessages)
-        // (ie by adding -Dmule.disable.threadsafemessages=true to the java command line).
+        // (i.e., by adding -Dmule.disable.threadsafemessages=true to the java command line).
         //
         // To remove the underlying cause, however, you probably need to do one of:
         //
-        // - make sure that the message adapter you are using correclty implements the
+        // - make sure that the message adapter you are using correctly implements the
         // ThreadSafeAccess interface
         //
         // - make sure that dispatcher and receiver classes copy ThreadSafeAccess instances when
