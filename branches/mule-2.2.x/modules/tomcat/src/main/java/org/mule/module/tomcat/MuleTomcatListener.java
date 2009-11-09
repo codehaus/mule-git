9425c55785aa977d827cba8f0bd6acf422588de5
aperepel <aperepel@bf997673-6b11-0410-b953-e057580c5b09>
2009-11-09 15:36:40 +0000

    Added an initial version of tomcat service integration (not included in the main build lifecycle yet)
    git-svn-id: file:///home/projects/mule/scm@16003 bf997673-6b11-0410-b953-e057580c5b09

---
+++ branches/mule-2.2.x/modules/tomcat/src/main/java/org/mule/module/tomcat/MuleTomcatListener.java
@@ -0,0 +1,63 @@
+package org.mule.module.tomcat;
+
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleEvent;
+import org.apache.catalina.LifecycleListener;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.mule.api.MuleContext;
+import org.mule.api.MuleException;
+import org.mule.context.DefaultMuleContextFactory;
+
+/**
+ *
+ */
+public class MuleTomcatListener implements LifecycleListener {
+
+    private static Log log = LogFactory.getLog(MuleTomcatListener.class);
+
+    protected MuleContext muleContext;
+
+    public void lifecycleEvent(LifecycleEvent event) {
+        if (Lifecycle.BEFORE_START_EVENT.equals(event.getType())) {
+            if (log.isDebugEnabled()) {
+                log.debug("BEFORE_START_EVENT");
+            }
+            doStart();
+            return;
+        }
+
+        if (Lifecycle.BEFORE_STOP_EVENT.equals(event.getType())) {
+            if (log.isDebugEnabled()) {
+                log.debug("BEFORE_STOP_EVENT");
+            }
+            doStop();
+            return;
+        }
+
+        if (log.isDebugEnabled()) {
+            log.debug("not our event: " + event.getType());
+        }
+    }
+
+    protected void doStart() {
+        log.info("Starting Mule");
+        DefaultMuleContextFactory muleContextFactory = new DefaultMuleContextFactory();
+        try {
+            muleContext = muleContextFactory.createMuleContext();
+            muleContext.start();
+        } catch (Exception e) {
+            log.error("Failed to start Mule", e);
+        }
+    }
+
+    protected void doStop() {
+        log.info("Stopping Mule");
+        try {
+            muleContext.stop();
+        } catch (MuleException e) {
+            log.error("Failed to stop Mule", e);
+        }
+        muleContext.dispose();
+    }
+}
