7c9362bc4b94c94736f11fa0e92008d90f6bb563
dzapata <dzapata@bf997673-6b11-0410-b953-e057580c5b09>
2009-11-10 17:10:34 +0000

    create 2.2.3 branch from 2.2.x
    git-svn-id: file:///home/projects/mule/scm@16025 bf997673-6b11-0410-b953-e057580c5b09

---
+++ branches/mule-2.2.3/modules/spring-config/src/main/java/org/mule/config/spring/parsers/specific/TransactionConfigDefinitionParser.java
@@ -0,0 +1,24 @@
+/*
+ * $Id:TransactionConfigDefinitionParser.java 5187 2007-02-16 18:00:42Z rossmason $
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.config.spring.parsers.specific;
+
+import org.mule.config.spring.parsers.generic.ChildDefinitionParser;
+import org.mule.transaction.MuleTransactionConfig;
+
+public class TransactionConfigDefinitionParser extends ChildDefinitionParser
+{
+
+    public TransactionConfigDefinitionParser()
+    {
+        super("transactionConfig", MuleTransactionConfig.class);
+        addMapping("action", "NONE=0,ALWAYS_BEGIN=1,BEGIN_OR_JOIN=2,ALWAYS_JOIN=3,JOIN_IF_POSSIBLE=4");
+    }
+
+}
