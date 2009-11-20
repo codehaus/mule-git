1861abc3b0a8e051915fa8f490538328efdc6a6d
dfeist <dfeist@bf997673-6b11-0410-b953-e057580c5b09>
2009-11-20 12:57:05 +0000

    Merged revisions 16080-16081 via svnmerge from https://svn.codehaus.org/mule/branches/mule-2.1.x
    ........
      r16080 | dfeist | 2009-11-20 10:44:42 -0200 (Fri, 20 Nov 2009) | 1 line
      
      MULE-4619 ChainingRouter sets MULE_REMOTE_SYNC_PROPERTY true on message when dispatching to last endpoint
    ........
      r16081 | dfeist | 2009-11-20 10:52:53 -0200 (Fri, 20 Nov 2009) | 1 line
      
      MULE-4619 ChainingRouter sets MULE_REMOTE_SYNC_PROPERTY true on message when dispatching to last endpoint
    ........
    
    
    git-svn-id: file:///home/projects/mule/scm@16082 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.2.x/tests/integration/src/test/java/org/mule/test/integration/routing/outbound/ChainingRouterRemoteSyncTestCase.java
+++ branches/mule-2.2.x/tests/integration/src/test/java/org/mule/test/integration/routing/outbound/ChainingRouterRemoteSyncTestCase.java
@@ -12,6 +12,7 @@ package org.mule.test.integration.routing.outbound;
 
 import org.mule.DefaultMuleMessage;
 import org.mule.api.MuleMessage;
+import org.mule.api.config.MuleProperties;
 import org.mule.module.client.MuleClient;
 import org.mule.tck.FunctionalTestCase;
 
@@ -32,6 +33,24 @@ public class ChainingRouterRemoteSyncTestCase extends FunctionalTestCase
 
         assertNull("Shouldn't have any exceptions", result.getExceptionPayload());
         assertEquals("test [REMOTESYNC RESPONSE] [REMOTESYNC RESPONSE 2]", result.getPayloadAsString());
+    }
+
+    /**
+     * MULE-4619 ChainingRouter sets MULE_REMOTE_SYNC_PROPERTY true on message when dispatching to last endpoint
+     * @throws Exception
+     */
+    public void testRemoteSyncLastEndpointDispatch() throws Exception
+    {
+
+        MuleClient muleClient = new MuleClient();
+        MuleMessage result = muleClient.send("vm://in2", new DefaultMuleMessage("test"));
+
+        assertNull("Shouldn't have any exceptions", result.getExceptionPayload());
+        assertEquals("test", result.getPayloadAsString());
+
+        MuleMessage jmsMessage = muleClient.request("jms://out2", FunctionalTestCase.RECEIVE_TIMEOUT);
+        assertEquals("test [REMOTESYNC RESPONSE] [REMOTESYNC RESPONSE 2]", jmsMessage.getPayloadAsString());
+        assertFalse(jmsMessage.getBooleanProperty(MuleProperties.MULE_REMOTE_SYNC_PROPERTY, false));
     }
 
 }
