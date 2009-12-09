22577f8918af1c76f55d18c86816bd5ccc3f5aa5
dirk.olmes <dirk.olmes@bf997673-6b11-0410-b953-e057580c5b09>
2009-12-09 16:10:48 +0000

    exclude more tests failing on IBM JDK
    git-svn-id: file:///home/projects/mule/scm@16207 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.2.x/transports/email/src/test/java/org/mule/transport/email/connectors/SmtpsConnectorTestCase.java
+++ branches/mule-2.2.x/transports/email/src/test/java/org/mule/transport/email/connectors/SmtpsConnectorTestCase.java
@@ -12,18 +12,25 @@ package org.mule.transport.email.connectors;
 
 import org.mule.api.transport.Connector;
 import org.mule.transport.email.SmtpsConnector;
+import org.mule.util.SystemUtils;
 
 import com.icegreen.greenmail.util.ServerSetup;
 
 public class SmtpsConnectorTestCase extends SmtpConnectorTestCase
 {
+    @Override
+    protected boolean isDisabledInThisEnvironment()
+    {
+        // MULE-4662
+        return SystemUtils.isIbmJDK();
+    }
 
     public SmtpsConnectorTestCase()
     {
         super(ServerSetup.PROTOCOL_SMTPS, 50008);
     }
 
-    // @Override
+    @Override
     public Connector createConnector() throws Exception
     {
         SmtpsConnector connector = new SmtpsConnector();
@@ -32,5 +39,4 @@ public class SmtpsConnectorTestCase extends SmtpConnectorTestCase
         connector.setTrustStore("greenmail-truststore");
         return connector;
     }
-
 }
