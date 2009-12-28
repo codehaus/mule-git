cacc95af6525a9ce2a7a146ece46551fd40b2bcf
dirk.olmes <dirk.olmes@bf997673-6b11-0410-b953-e057580c5b09>
2009-12-28 13:32:40 +0000

    checkstyle
    git-svn-id: file:///home/projects/mule/scm@16327 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.2.x/transports/cxf/src/test/java/org/mule/transport/cxf/issues/LargeProxyTestCase.java
+++ branches/mule-2.2.x/transports/cxf/src/test/java/org/mule/transport/cxf/issues/LargeProxyTestCase.java
@@ -19,32 +19,32 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * Tests large requests sent to the proxy and back.
- *
- * @author lenhag
- * @version $Id$
  */
-public class LargeProxyTestCase extends FunctionalTestCase {
-
-  protected String getConfigResources() {
-    return "largeproxytest-config.xml";
-  }
-
-  public void testLargeMessageWithEchoProxy() throws Exception {
-    int length = 5000;
-    final MuleClient client = new MuleClient();
-
-    StringBuffer b = new StringBuffer();
-    int counter = 1;
-    while (b.length() < length) {
-      // Using a counter to make it easier to see the size
-      b.append(counter).append(" ");
-//      b.append((char) (Math.random() * 26 + 'a'));
-      counter++;
+public class LargeProxyTestCase extends FunctionalTestCase 
+{
+    protected String getConfigResources() 
+    {
+        return "largeproxytest-config.xml";
     }
-    final String largeString = b.toString().trim();
 
-    final String msg =
-        "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" " +
+    public void testLargeMessageWithEchoProxy() throws Exception 
+    {
+        int length = 5000;
+        final MuleClient client = new MuleClient();
+
+        StringBuffer b = new StringBuffer();
+        int counter = 1;
+        while (b.length() < length) 
+        {
+            // Using a counter to make it easier to see the size
+            b.append(counter).append(" ");
+            //      b.append((char) (Math.random() * 26 + 'a'));
+            counter++;
+        }
+        final String largeString = b.toString().trim();
+
+        final String msg =
+            "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" " +
             "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" " +
             "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">" +
             "<soap:Body>" +
@@ -54,34 +54,32 @@ public class LargeProxyTestCase extends FunctionalTestCase {
             "</soap:Body>" +
             "</soap:Envelope>";
 
-    final CountDownLatch latch = new CountDownLatch(100);
+        final CountDownLatch latch = new CountDownLatch(100);
 
-    Runnable runnable = new Runnable() {
-
-        public void run()
+        Runnable runnable = new Runnable()
         {
-            for (int i = 0; i < 20; i++)
+            public void run()
             {
-                try {
-                    MuleMessage result = client.send("http://localhost:63082/services/EchoProxy", msg, null);
-                    String payloadAsStr = result.getPayloadAsString();
-                    assertTrue("The payload length should never be 0", payloadAsStr.length() != 0);
-                    assertTrue(payloadAsStr.indexOf(largeString) != -1);
-                    latch.countDown();
-                } catch (Exception e) {
-                    e.printStackTrace();
+                for (int i = 0; i < 20; i++)
+                {
+                    try {
+                        MuleMessage result = client.send("http://localhost:63082/services/EchoProxy", msg, null);
+                        String payloadAsStr = result.getPayloadAsString();
+                        assertTrue("The payload length should never be 0", payloadAsStr.length() != 0);
+                        assertTrue(payloadAsStr.indexOf(largeString) != -1);
+                        latch.countDown();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }
+        };
+
+        for (int j = 0; j < 5; j++) 
+        {
+            new Thread(runnable).start();
         }
-        
-    };
-    
-    for (int j = 0; j < 5; j++) {
-        new Thread(runnable).start();
+
+        latch.await(60000, TimeUnit.SECONDS);
     }
-    
-    latch.await(60000, TimeUnit.SECONDS);
-  }
-
-
 }
