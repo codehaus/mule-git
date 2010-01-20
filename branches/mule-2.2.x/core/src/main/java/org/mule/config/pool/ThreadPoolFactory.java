13d9198691d2591e591b3ac6b1a7b7bb20484337
dirk.olmes <dirk.olmes@bf997673-6b11-0410-b953-e057580c5b09>
2010-01-20 12:17:30 +0000

    add proper license headers
    git-svn-id: file:///home/projects/mule/scm@16530 bf997673-6b11-0410-b953-e057580c5b09

--- branches/mule-2.2.x/core/src/main/java/org/mule/config/pool/ThreadPoolFactory.java
+++ branches/mule-2.2.x/core/src/main/java/org/mule/config/pool/ThreadPoolFactory.java
@@ -1,3 +1,13 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.config.pool;
 
 import org.mule.api.MuleRuntimeException;
@@ -10,6 +20,7 @@ import java.util.Iterator;
 import javax.imageio.spi.ServiceRegistry;
 
 import edu.emory.mathcs.backport.java.util.concurrent.ThreadPoolExecutor;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
