b96cfd56b25de244330efc798c8f4245de467d91
dzapata <dzapata@bf997673-6b11-0410-b953-e057580c5b09>
2010-04-21 23:54:44 +0000

    add more extensive local transaction testing
    git-svn-id: file:///home/projects/mule/scm@17073 bf997673-6b11-0410-b953-e057580c5b09

---
+++ branches/mule-2.2.x/transports/jms/src/test/java/org/mule/transport/jms/integration/JmsSingleTransactionSingleServiceAlwaysBeginConfigurationTestCase.java
@@ -0,0 +1,23 @@
+/*
+ * $Id: JmsSingleTransactionAlwaysBeginConfigurationTestCase.java 14304 2009-03-15 11:19:11Z dfeist $
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport.jms.integration;
+
+/**
+ * Test all combinations of (inbound) ALWAYS_BEGIN.  They should all pass.
+ */
+public class JmsSingleTransactionSingleServiceAlwaysBeginConfigurationTestCase extends
+    AbstractJmsSingleTransactionSingleServiceTestCase
+{
+    protected String getConfigResources()
+    {
+        return "integration/jms-single-tx-single-service-always-begin.xml";
+    }
+}
---
+++ branches/mule-2.2.x/transports/jms/src/test/java/org/mule/transport/jms/integration/JmsSingleTransactionSingleServiceAlwaysJoinConfigurationTestCase.java
@@ -0,0 +1,89 @@
+/*
+ * $Id: JmsSingleTransactionAlwaysBeginConfigurationTestCase.java 14304 2009-03-15 11:19:11Z dfeist $
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.transport.jms.integration;
+
+import org.junit.Test;
+
+/**
+ * Test all combinations of (inbound) ALWAYS_JOIN.  They should all fail.
+ */
+public class JmsSingleTransactionSingleServiceAlwaysJoinConfigurationTestCase extends
+    AbstractJmsSingleTransactionSingleServiceTestCase
+{
+    protected String getConfigResources()
+    {
+        return "integration/jms-single-tx-single-service-always-join.xml";
+    }
+    @Test
+    public void testNone() throws Exception
+    {
+        scenarioCommit.setInputDestinationName(JMS_QUEUE_INPUT_CONF_A);
+        scenarioRollback.setInputDestinationName(JMS_QUEUE_INPUT_CONF_A);
+        scenarioNotReceive.setInputDestinationName(JMS_QUEUE_INPUT_CONF_A);
+        scenarioCommit.setOutputDestinationName(JMS_QUEUE_OUTPUT_CONF_A);
+        scenarioRollback.setOutputDestinationName(JMS_QUEUE_OUTPUT_CONF_A);
+        scenarioNotReceive.setOutputDestinationName(JMS_QUEUE_OUTPUT_CONF_A);
+
+        runTransactionFail("testNone");
+    }
+
+    @Test
+    public void testAlwaysBegin() throws Exception
+    {
+        scenarioCommit.setInputDestinationName(JMS_QUEUE_INPUT_CONF_B);
+        scenarioRollback.setInputDestinationName(JMS_QUEUE_INPUT_CONF_B);
+        scenarioNotReceive.setInputDestinationName(JMS_QUEUE_INPUT_CONF_B);
+        scenarioCommit.setOutputDestinationName(JMS_QUEUE_OUTPUT_CONF_B);
+        scenarioRollback.setOutputDestinationName(JMS_QUEUE_OUTPUT_CONF_B);
+        scenarioNotReceive.setOutputDestinationName(JMS_QUEUE_OUTPUT_CONF_B);
+
+        runTransactionFail("testAlwaysBegin");
+    }
+
+    @Test
+    public void testBeginOrJoin() throws Exception
+    {
+        scenarioCommit.setInputDestinationName(JMS_QUEUE_INPUT_CONF_C);
+        scenarioRollback.setInputDestinationName(JMS_QUEUE_INPUT_CONF_C);
+        scenarioNotReceive.setInputDestinationName(JMS_QUEUE_INPUT_CONF_C);
+        scenarioCommit.setOutputDestinationName(JMS_QUEUE_OUTPUT_CONF_C);
+        scenarioRollback.setOutputDestinationName(JMS_QUEUE_OUTPUT_CONF_C);
+        scenarioNotReceive.setOutputDestinationName(JMS_QUEUE_OUTPUT_CONF_C);
+
+        runTransactionFail("testBeginOrJoin");
+    }
+
+    @Test
+    public void testAlwaysJoin() throws Exception
+    {
+        scenarioCommit.setInputDestinationName(JMS_QUEUE_INPUT_CONF_D);
+        scenarioRollback.setInputDestinationName(JMS_QUEUE_INPUT_CONF_D);
+        scenarioNotReceive.setInputDestinationName(JMS_QUEUE_INPUT_CONF_D);
+        scenarioCommit.setOutputDestinationName(JMS_QUEUE_OUTPUT_CONF_D);
+        scenarioRollback.setOutputDestinationName(JMS_QUEUE_OUTPUT_CONF_D);
+        scenarioNotReceive.setOutputDestinationName(JMS_QUEUE_OUTPUT_CONF_D);
+
+        runTransactionFail("testAlwaysJoin");
+    }
+
+    @Test
+    public void testJoinIfPossible() throws Exception
+    {
+        scenarioCommit.setInputDestinationName(JMS_QUEUE_INPUT_CONF_E);
+        scenarioRollback.setInputDestinationName(JMS_QUEUE_INPUT_CONF_E);
+        scenarioNotReceive.setInputDestinationName(JMS_QUEUE_INPUT_CONF_E);
+        scenarioCommit.setOutputDestinationName(JMS_QUEUE_OUTPUT_CONF_E);
+        scenarioRollback.setOutputDestinationName(JMS_QUEUE_OUTPUT_CONF_E);
+        scenarioNotReceive.setOutputDestinationName(JMS_QUEUE_OUTPUT_CONF_E);
+
+        runTransactionFail("testJoinIfPossible");
+    }
+}
